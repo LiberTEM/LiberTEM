8f807f9f9414e87a1c19817a99a5a5fb37248a20
Alexander Clausen <alex@gc-web.de>
2018-05-22 19:08:44 +0200

    make hdfs3 and h5py optional dependencies
    you can install those for example via ``pip install libertem[hdfs,hdf5]``

--- setup.py
+++ setup.py
@@ -8,9 +8,12 @@ setup(
     include_package_data=True,
     install_requires=[
         "numpy",
-        "hdfs3",
         "distributed",
     ],
+    extras_require={
+        'hdfs': 'hfds3',
+        'hdf5': 'h5py',
+    },
     package_dir={"": "src"},
     packages=[
         "libertem",

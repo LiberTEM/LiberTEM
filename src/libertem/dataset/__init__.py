18f3bceda3304441e38f9687b7889494c395235b
Alexander Clausen <alex@gc-web.de>
2018-06-28 16:55:49 +0200

    libertem.dataset: add load function to lazily load datasets
    This means we don't have to depend on hdf3 on import time anymore

---
+++ src/libertem/dataset/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+
+filetypes = {
+    "hdfs": "libertem.dataset.hdfs.BinaryHDFSDataSet",
+    "hdf5": "libertem.dataset.hdf5.H5DataSet",
+}
+
+
+def load(filetype, *args, **kwargs):
+    try:
+        ft = filetypes[filetype.lower()]
+    except KeyError:
+        raise ValueError("unknown filetype: %s" % filetype)
+    parts = ft.split(".")
+    module = ".".join(parts[:-1])
+    cls = parts[-1]
+    module = importlib.import_module(module)
+    cls = getattr(module, cls)
+    return cls(*args, **kwargs)

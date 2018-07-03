def4bd12a154253c7df2e0a298894754e2350bb4
Alexander Clausen <alex@gc-web.de>
2018-07-03 11:23:56 +0200

    client: tweak mask styles

--- client/src/widgets/styles.ts
+++ client/src/widgets/styles.ts
@@ -1,6 +1,9 @@
 import { CSSProperties } from "react";
 
 export const defaultMaskStyles: CSSProperties = {
-    fillOpacity: 0.4,
+    fillOpacity: 0.3,
     fill: "red",
+    strokeOpacity: 0.7,
+    stroke: "red",
+    strokeWidth: 0.4,
 }

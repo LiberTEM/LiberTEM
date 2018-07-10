c00556ee174aaaf32d67b0f97564e1da4a39ad7a
Alexander Clausen <alex@gc-web.de>
2018-07-10 19:44:32 +0200

    client: allow the UI to take up more space
    more tweaking to be done here, but this should be good enough for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import ChannelStatus from './channel/components/ChannelStatus';
 import DatasetList from './dataset/components/DatasetList';
 import ErrorList from './errors/ErrorList';
@@ -8,13 +8,13 @@ import ErrorList from './errors/ErrorList';
 class App extends React.Component {
     public render() {
         return (
-            <Container style={{ marginTop: "5em", marginBottom: "5em", }}>
+            <div style={{ margin: "5em 1em 5em 1em" }}>
                 <Header as='h1'>LiberTEM</Header>
                 <ErrorList />
                 <ChannelStatus>
                     <DatasetList />
                 </ChannelStatus>
-            </Container>
+            </div>
         );
     }
 }

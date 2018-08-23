880cc693325e9063d691c812f790fe843dc0b791
Alexander Clausen <alex@gc-web.de>
2018-08-23 18:58:12 +0200

    InlineJobExecutor: docstring

--- src/libertem/executor/inline.py
+++ src/libertem/executor/inline.py
@@ -2,6 +2,9 @@ from .base import JobExecutor
 
 
 class InlineJobExecutor(JobExecutor):
+    """
+    naive JobExecutor that just iterates over partitions and processes them one after another
+    """
     def run_job(self, job):
         for task in job.get_tasks():
             yield task()

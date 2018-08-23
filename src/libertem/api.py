6808f91fb76fa09c29006dade5b394f9cc106c0e
Alexander Clausen <alex@gc-web.de>
2018-08-23 18:54:47 +0200

    libertem.api: add docstrings

--- src/libertem/api.py
+++ src/libertem/api.py
@@ -8,19 +8,72 @@ from libertem.viz import visualize_simple
 
 class Context:
     def __init__(self):
+        """
+        Create a new context. In the background, this creates a suitable
+        executor and spins up a local Dask cluster.
+        """
         self.executor = self._create_local_executor()
 
-    def load(self, *args, **kwargs):
+    def load(self, filetype, *args, **kwargs):
+        """
+        Load a `DataSet`. As it doesn't load the whole data into RAM at once,
+        you can load and process datasets that are bigger than your available RAM.
+        Using fast storage (i.e. SSD) is advisable.
+
+        Parameters
+        ----------
+        filetype : str
+            see libertem.io.dataset.filetypes for supported types, example: 'hdf5'
+
+
+        Note
+        ----
+
+        Additional parameters are passed to the concrete DataSet implementation
+
+        Returns
+        -------
+        DataSet
+            a subclass of DataSet
+        """
         # delegate to libertem.io.dataset.load:
-        return load(*args, **kwargs)
+        return load(filetype, *args, **kwargs)
 
     def create_mask_job(self, factories, dataset):
+        """
+        Create a mask application job. Each factory function should, when called,
+        return a numpy array with the same shape as frames in the dataset (so dataset.shape[2:]).
+
+        Parameters
+        ----------
+        factories : list of mask factory functions
+            functions that take no arguments and create masks
+        dataset : DataSet
+            dataset to work on
+
+        Examples
+        --------
+        >>> job = ctx.create_mask_job(
+        ... factories=[lambda: np.ones(dataset.shape[2:])],
+        ... dataset=dataset)
+        >>> result = ctx.run(job)
+        """
         return ApplyMasksJob(
             dataset=dataset,
             mask_factories=factories,
         )
 
     def run(self, job, out=None):
+        """
+        Run the given `Job` and return the result data.
+
+        Parameters
+        ----------
+        job : Job
+            the job to run
+        out : ndarray
+            ndarray to store the result, if None it is created for you
+        """
         if out is None:
             out = job.get_result_buffer()
         else:
@@ -33,6 +86,9 @@ class Context:
         return out
 
     def apply_colormap(self, result_frames):
+        """
+        Normalize and apply a colormap to result_frames and return resulting RGB data
+        """
         return [visualize_simple(result_frame)
                 for result_frame in result_frames]
 

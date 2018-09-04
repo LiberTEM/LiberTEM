4ccb814e8459a746d124d6e20d5b5de1412578eb
Alexander Clausen <alex@gc-web.de>
2018-09-04 18:25:40 +0200

    server: disable --host parameter for now, refs #67
    Without authentication, --host is dangerous and should not be used

--- src/libertem/web/cli.py
+++ src/libertem/web/cli.py
@@ -6,8 +6,11 @@ from .server import run
 @click.command()
 @click.option('--port', help='port on which the server should listen on',
               default=9000, type=int)
-@click.option('--host', help='host on which the server should listen on',
-              default="localhost", type=str)
-def main(host, port):
+# FIXME: the host parameter is currently disabled, as it poses a security risk
+# as long as there is no authentication
+# see also: https://github.com/LiberTEM/LiberTEM/issues/67
+# @click.option('--host', help='host on which the server should listen on',
+#               default="localhost", type=str)
+def main(port, host="localhost"):
     console_tweaks()
     run(host, port)

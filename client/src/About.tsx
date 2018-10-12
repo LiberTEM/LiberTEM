a22d18d71db9e7f53644b10c57eb77bfe3b1d0b7
Alexander Clausen <alex@gc-web.de>
2018-10-12 13:08:44 +0200

    client: add about screen

---
+++ client/src/About.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { Header, List } from "semantic-ui-react";
+import { RootReducer } from "./store";
+
+const mapStateToProps = (state: RootReducer) => {
+    return {
+        version: state.config.version,
+    }
+}
+
+type MergedProps = ReturnType<typeof mapStateToProps>;
+
+const About: React.SFC<MergedProps> = ({ version }) => {
+    return (
+        <>
+            <Header as="h3">This is LiberTEM version {version}</Header>
+            <List>
+                <List.Item>
+                    <List.Icon name="github" />
+                    <List.Content>
+                        Find us on <a href="https://github.com/LiberTEM/LiberTEM">GitHub</a>
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="bug" />
+                    <List.Content>
+                        Found a bug? Got a feature request? Please <a href="https://github.com/LiberTEM/LiberTEM/issues/new">open an issue!</a>
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="legal" />
+                    <List.Content>
+                        LiberTEM is licensed under the <a href="https://github.com/LiberTEM/LiberTEM/blob/master/LICENSE">GNU General Public License v3.0</a>
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="book" />
+                    <List.Content>
+                        Read <a href="https://libertem.github.io/LiberTEM/">the documentation</a>
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="gitter" />
+                    <List.Content>
+                        Join our <a href="https://gitter.im/LiberTEM/Lobby">chat on gitter!</a>
+                    </List.Content>
+                </List.Item>
+            </List>
+        </>
+    );
+}
+
+export default connect(mapStateToProps)(About);
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Icon, Modal, Popup } from 'semantic-ui-react';
+import About from './About';
 import ChannelStatus from './channel/components/ChannelStatus';
 import DatasetList from './dataset/components/DatasetList';
 import ErrorList from './errors/components/ErrorList';
@@ -9,7 +10,20 @@ class App extends React.Component {
     public render() {
         return (
             <Container style={{ margin: "5em 1em 5em 1em" }}>
-                <Header as='h1'>LiberTEM</Header>
+                <Header as='h1'>
+                    LiberTEM
+                    {' '}
+                    <Modal trigger={
+                        <Header.Content>
+                            <Icon name="info circle" size="small" link={true} />
+                        </Header.Content>
+                    }>
+                        <Popup.Header>About LiberTEM</Popup.Header>
+                        <Popup.Content>
+                            <About />
+                        </Popup.Content>
+                    </Modal>
+                </Header>
                 <ErrorList />
                 <ChannelStatus>
                     <DatasetList />

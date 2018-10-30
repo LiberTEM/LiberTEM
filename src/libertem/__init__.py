c3ae21f220ceb89bf18a26df24410a8b452f3f72
Alexander Clausen <alex@gc-web.de>
2018-10-30 11:50:01 +0100

    bump_version scripts and related changes
     + use canonical version format: 0.1.0.dev0
     + put version into its own file, so we can just overwrite it
       and "safely" exec it in the bump script (no side effects besides
       the assignment of __version__)

--- src/libertem/__init__.py
+++ src/libertem/__init__.py
@@ -1,7 +1,14 @@
-__version__ = "0.1.dev0"
+# keep version and revision in separate files such that we can
+# generate them without any regex magic
+
+from .__version__ import version as __version__
 
 try:
     from ._baked_revision import revision
 except ImportError:
     from .versioning import get_git_rev
     revision = get_git_rev()
+
+__all__ = [
+    "revision", "__version__"
+]

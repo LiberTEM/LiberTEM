ed987972d2d103178a50a07a864b6d9dceb46207
Dieter Weber <d.weber@materials-scientist.com>
2018-11-06 17:18:24 +0100

    Zenodo badge updated and included in README; favicon
    Updated Zenodo DOI to 0.1.0 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,8 @@ pygments_style = 'sphinx'
 #
 html_theme = 'alabaster'
 
+html_favicon = '../../corporatedesign/logo/favicon.ico'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.

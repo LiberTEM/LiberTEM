199a5fb6256f81281684a4443994c2c157621950
Dieter Weber <d.weber@materials-scientist.com>
2018-11-20 15:00:31 +0100

    Moved the pick coordinate display to the right
    Users had difficulties finding the coordinate display under
    the frame window #164. This commit moves the coordinates under the
    scan window on the right side where they are hopefully easier to spot.

--- client/src/job/components/ResultList.tsx
+++ client/src/job/components/ResultList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { Dropdown, DropdownProps } from "semantic-ui-react";
+import { AnalysisTypes } from "src/messages";
 import { RootReducer } from "../../store";
 import { JobState } from "../types";
 import Result from "./Result";
@@ -19,12 +20,14 @@ const mapStateToProps = (state: RootReducer, ownProps: ExternalResultListProps) 
     const jobId = analysis.jobs.RESULT;
     const job = jobId ? state.jobs.byId[jobId] : undefined;
     const ds = job ? state.datasets.byId[job.dataset] : undefined;
+    const pickCoords = analysis.frameDetails.type === AnalysisTypes.SUM_FRAMES ? null : <>Pick: x={analysis.frameDetails.parameters.x}, y={analysis.frameDetails.parameters.y} &emsp;</>;
 
     return {
         currentJob: job,
         jobsById: state.jobs.byId,
         analysis,
         dataset: ds,
+        pickCoords,
     };
 };
 
@@ -58,30 +61,30 @@ class ResultList extends React.Component<MergedProps, ResultListState> {
     }
 
     public render() {
-        const { analysis, dataset, width, height } = this.props;
+        const { analysis, dataset, width, height, pickCoords } = this.props;
         let msg;
         let img = (
             <svg style={{ display: "block", border: "1px solid black", width: "100%", height: "auto" }} width={width} height={height} viewBox={`0 0 ${width} ${height}`} key={-1} />
         );
         const job = this.getJob();
         if (!job || !dataset) {
-            msg = <p>&nbsp;</p>;
+            msg = <>&nbsp;</>;
         } else {
             img = (
                 <Result analysis={analysis} job={job} dataset={dataset} width={width} height={height} idx={this.state.selectedImg} />
             );
             if (job.startTimestamp && job.endTimestamp) {
                 const dt = (job.endTimestamp - job.startTimestamp) / 1000;
-                msg = <p>Analysis done in {dt.toFixed(3)} seconds</p>;
+                msg = <>Analysis done in {dt.toFixed(3)} seconds</>;
             } else {
-                msg = <p>Analysis running...</p>;
+                msg = <>Analysis running...</>;
             }
         }
         return (
             <div>
                 {img}
                 <ResultImageSelector job={job} handleChange={this.selectImage} selectedImg={this.state.selectedImg} />
-                {msg}
+                <p>{pickCoords} {msg}</p>
             </div>
         );
     }

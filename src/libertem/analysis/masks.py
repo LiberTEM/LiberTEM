9b8c406123942e176a61fe4dcd114fc05158e3ac
Dieter Weber <d.weber@materials-scientist.com>
2018-11-23 22:50:24 +0100

    More fixes for failing tests
    tox currently doesn't work on Windows, TODO
    Therefore debugging commit by commit

--- src/libertem/analysis/masks.py
+++ src/libertem/analysis/masks.py
@@ -7,7 +7,7 @@ from libertem.job.masks import ApplyMasksJob
 class BaseMasksAnalysis(BaseAnalysis):
     """
     base class for any masks-based analysis; you only need to implement
-    ``get_results`` and ``get_mask_factories``
+    ``get_results`` ``get_use_sparse`` and ``get_mask_factories``
     """
 
     @property
@@ -16,16 +16,23 @@ class BaseMasksAnalysis(BaseAnalysis):
 
     def get_job(self):
         mask_factories = self.get_mask_factories()
-        job = ApplyMasksJob(dataset=self.dataset, mask_factories=mask_factories, use_sparse=self.parameters['use_sparse'])
+        use_sparse = self.get_use_sparse()
+        job = ApplyMasksJob(dataset=self.dataset, mask_factories=mask_factories, use_sparse=use_sparse)
         return job
 
     def get_mask_factories(self):
+        raise NotImplementedError()
+
+    def get_use_sparse(self):
         raise NotImplementedError()
 
 
 class MasksAnalysis(BaseMasksAnalysis):
     def get_mask_factories(self):
         return self.parameters['factories']
+
+    def get_use_sparse(self):
+        return self.parameters['use_sparse']
 
     def get_results(self, job_results):
         return AnalysisResultSet([

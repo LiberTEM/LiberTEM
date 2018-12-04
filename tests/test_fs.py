2c7b9e233ba863a3f82f2387b2582d3719c1e403
Alexander Clausen <alex@gc-web.de>
2018-12-04 16:51:19 +0100

    add tests for filesystem util _get_alt_path
    also try to disable appveyor for continuous tag again

---
+++ tests/test_fs.py
@@ -0,0 +1,26 @@
+import os
+import uuid
+
+import pytest
+
+from libertem.web.server import _get_alt_path
+
+
+@pytest.mark.skipif(os.name != 'nt',
+                    reason="only runs on windows")
+def test_doesnt_exist_windows():
+    rnd = str(uuid.uuid4())
+    path = r"C:\\" + rnd + r"\really\this\does\not\exist\\"
+
+    alt = str(_get_alt_path(path))
+    assert alt == "C:\\"
+
+
+@pytest.mark.skipif(os.name == 'nt',
+                    reason="doesnt run on windows")
+def test_doesnt_exist_posix():
+    rnd = str(uuid.uuid4())
+    path = "/%s/really/this/doesnt/exist/" % rnd
+
+    alt = str(_get_alt_path(path))
+    assert alt == "/"

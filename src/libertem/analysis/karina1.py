2aa4d547001b70195fd4aa89dc8d7fbc7a075039
Alexander Clausen <alex@gc-web.de>
2019-01-15 14:38:51 +0100

    use pyfftw if available

--- src/libertem/analysis/karina1.py
+++ src/libertem/analysis/karina1.py
@@ -1,6 +1,3 @@
-import functools
-import operator
-
 import numpy as np
 import pandas as pd
 from skimage.feature import peak_local_max
@@ -8,6 +5,15 @@ from skimage.draw import circle_perimeter
 
 from .base import BaseAnalysis
 from libertem.masks import radial_gradient, _make_circular_mask
+
+try:
+    import pyfftw
+    fft = pyfftw.interfaces.numpy_fft
+    pyfftw.interfaces.cache.enable()
+    zeros = pyfftw.zeros_aligned
+except ImportError:
+    fft = np.fft
+    zeros = np.zeros
 
 
 class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
@@ -38,7 +44,7 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
         # TODO: executor may be async, what do? it also may be sync... (how does dask do it?)
         # TODO: find a "lightweight" way to do init_result/compute/reduce_result?
         dataset = self.dataset
-        sum_result_total = np.zeros(dataset.shape.sig, dtype="float32")
+        sum_result_total = zeros(dataset.shape.sig, dtype="float32")
         for sum_result in executor.map_partitions(dataset=dataset, fn=self.pass_1):
             sum_result_total += sum_result
 
@@ -49,8 +55,10 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
 
         num_disks = self.parameters['num_disks']
 
-        all_centers = np.zeros(tuple(dataset.raw_shape.nav) + (num_disks, 2), dtype="u2")
-        all_peak_values = np.zeros(tuple(dataset.raw_shape.nav) + (num_disks,), dtype="float32")
+        all_centers = zeros(tuple(dataset.raw_shape.nav) + (num_disks, 2),
+                            dtype="u2")
+        all_peak_values = zeros(tuple(dataset.raw_shape.nav) + (num_disks,),
+                                dtype="float32")
 
         for centers, peak_values, partition in executor.map_partitions(
                 dataset=dataset,
@@ -65,12 +73,12 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
     def pass_1(self, partition):
         tiles = partition.get_tiles()  # full_frames=True)
 
-        sum_result = np.zeros(partition.meta.shape.sig, dtype="float32")
+        sum_result = zeros(partition.meta.shape.sig, dtype="float32")
 
         # TODO:
         # bf_result = np.zeros(partition.meta.shape.nav, dtype="float32")
 
-        buf = np.zeros(partition.meta.shape.sig, dtype="float32")
+        buf = zeros(partition.meta.shape.sig, dtype="float32")
 
         for tile in tiles:
             data = tile.flat_nav
@@ -94,9 +102,9 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
         radius = parameters['radius']
         num_disks = parameters['num_disks']
         spec_mask = self.get_template(sig_shape=framesize, radius=radius)
-        spec_sum = np.fft.rfft2(sum_result)
+        spec_sum = fft.rfft2(sum_result)
         corrspec = spec_mask * spec_sum
-        corr = np.fft.fftshift(np.fft.irfft2(corrspec))
+        corr = fft.fftshift(fft.irfft2(corrspec))
         peaks = peak_local_max(corr, num_peaks=num_disks)
         self._debug_template = spec_mask
         return peaks
@@ -111,9 +119,10 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
         template = self.get_template(sig_shape=(2 * crop_size, 2 * crop_size), radius=radius)
         centers = np.zeros(tuple(partition.slice.shape.nav) + (len(peaks), 2),
                            dtype="u2")
-        peak_values = np.zeros(tuple(partition.slice.shape.nav) + (len(peaks),),
-                               dtype="float32")
-        buf = np.zeros((2 * crop_size, 2 * crop_size), dtype="float32")
+        peak_values = zeros(tuple(partition.slice.shape.nav) + (len(peaks),),
+                            dtype="float32")
+        buf = zeros((2 * crop_size, 2 * crop_size), dtype="float32")
+
         for tile in tiles:
             data = tile.flat_nav
             tile_slice_in_partition = tile.tile_slice.shift(partition.slice)
@@ -142,6 +151,13 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
                     centers[result_idx] = abs_center
                     peak_values[result_idx] = peak_value
         return centers, peak_values, partition
+
+    def do_correlation(self, template, crop_part):
+        spec_part = fft.rfft2(crop_part)
+        corrspec = template * spec_part
+        corr = fft.fftshift(fft.irfft2(corrspec))
+        center = np.unravel_index(np.argmax(corr), corr.shape)
+        return center, corr[center]
 
     def get_crop_size(self, radius, padding):
         return int(radius + radius * padding)
@@ -193,12 +209,5 @@ class Karina1PhaseCorrelationAnalysis(BaseAnalysis):
         else:
             raise ValueError("unknown mask type: %s" % mask_type)
 
-        spec_mask = np.fft.rfft2(mask)
+        spec_mask = fft.rfft2(mask)
         return spec_mask
-
-    def do_correlation(self, template, crop_part):
-        spec_part = np.fft.rfft2(crop_part)
-        corrspec = template * spec_part
-        corr = np.fft.fftshift(np.fft.irfft2(corrspec))
-        center = np.unravel_index(np.argmax(corr), corr.shape)
-        return center, corr[center]

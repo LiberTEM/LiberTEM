43efb403d8062ceeeb009fafc5f6be373d399937
Alexander Clausen <alex@gc-web.de>
2019-03-19 17:40:56 +0100

    add simple udf test

---
+++ tests/udf/test_simple_udf.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from libertem.udf import map_frames
+from libertem.common.buffers import BufferWrapper
+
+from utils import MemoryDataSet, _mk_random
+
+
+def test_sum_frames(lt_ctx):
+    data = _mk_random(size=(16, 16, 16, 16), dtype="float32")
+    dataset = MemoryDataSet(data=data, tileshape=(1, 1, 16, 16),
+                            partition_shape=(4, 4, 16, 16), sig_dims=2)
+
+    def my_init(partition):
+        return {}
+
+    def my_buffers():
+        return {
+            'pixelsum': BufferWrapper(
+                kind="nav", dtype="float32"
+            )
+        }
+
+    def my_frame_fn(frame, pixelsum):
+        pixelsum[:] = np.sum(frame)
+
+    res = map_frames(
+        ctx=lt_ctx,
+        dataset=dataset,
+        make_result_buffers=my_buffers,
+        init_fn=my_init,
+        frame_fn=my_frame_fn,
+    )
+    assert 'pixelsum' in res
+    print(data.shape, res['pixelsum'].data.shape)
+    assert np.allclose(res['pixelsum'].data, np.sum(data, axis=(2, 3)))
+
+
+def test_3d_ds(lt_ctx):
+    data = _mk_random(size=(16 * 16, 16, 16), dtype="float32")
+    dataset = MemoryDataSet(data=data, tileshape=(1, 16, 16),
+                            partition_shape=(4, 16, 16), sig_dims=2)
+
+    def my_init(partition):
+        return {}
+
+    def my_buffers():
+        return {
+            'pixelsum': BufferWrapper(
+                kind="nav", dtype="float32"
+            )
+        }
+
+    def my_frame_fn(frame, pixelsum):
+        pixelsum[:] = np.sum(frame)
+
+    res = map_frames(
+        ctx=lt_ctx,
+        dataset=dataset,
+        make_result_buffers=my_buffers,
+        init_fn=my_init,
+        frame_fn=my_frame_fn,
+    )
+    assert 'pixelsum' in res
+    print(data.shape, res['pixelsum'].data.shape)
+    assert np.allclose(res['pixelsum'].data, np.sum(data, axis=(1, 2)))

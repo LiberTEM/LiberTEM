0e754b90f7ae08d54a3cebda72da886adbce1f86
Karina KR. Ruzaeva <ruzaeva@iffw2k.kfa-juelich.de>
2019-03-27 17:01:36 +0100

    fixed bugs, added tests

--- src/libertem/udf/FEM.py
+++ src/libertem/udf/FEM.py
@@ -1,45 +1,31 @@
 import functools
 
 import numpy as np
-from skimage.feature import peak_local_max
-import scipy.ndimage as nd
 
-from libertem.udf import check_cast
 from libertem.common.buffers import BufferWrapper
-from libertem.masks import radial_gradient, background_substraction
-from libertem.job.sum import SumFramesJob
+from libertem.masks import _make_circular_mask
+
 
 def make_result_buffers():
     return {
-        'example_intensity': ResultBuffer(
+        'intensity': BufferWrapper(
             kind="nav", dtype="float32"
         ),
     }
 
-def check_cast(fromvar, tovar):
-    if not np.can_cast(fromvar.dtype, tovar.dtype, casting='safe'):
-        # FIXME exception or warning?
-        raise TypeError("Unsafe automatic casting from %s to %s" % (fromvar.dtype, tovar.dtype))
 
-
-def pass_2_merge(partition_result_buffers, example_intensity):
-    c = partition_result_buffers['example_intensity'].data
-    check_cast(c, example_intensity)
-    example_intensity[:] = c
-
-
-def _make_circular_mask(centerX, centerY, imageSizeX, imageSizeY, radius):
-
-    x, y = np.ogrid[-centerY:imageSizeY-centerY, -centerX:imageSizeX-centerX]
-    mask = x*x + y*y <= radius*radius
-    return(mask)
-
-
-
-def init_pass_2(partition, center, rad_in, rad_out):
-    mask_out=1*_make_circular_mask(center[1]*0.5, center[0]*0.5, partition.shape.sig[1], partition.shape.sig[0], rad_out)
-    mask_in=1*_make_circular_mask(center[1]*0.5, center[0]*0.5,partition.shape.sig[1], partition.shape.sig[0],rad_in)
-    mask=mask_out-mask_in
+def init(partition, center, rad_in, rad_out):
+    mask_out = 1*_make_circular_mask(
+        center[1], center[0],
+        partition.shape.sig[1], partition.shape.sig[0],
+        rad_out
+    )
+    mask_in = 1*_make_circular_mask(
+        center[1], center[0],
+        partition.shape.sig[1], partition.shape.sig[0],
+        rad_in
+    )
+    mask = mask_out - mask_in
 
     kwargs = {
         'mask': mask,
@@ -47,24 +33,17 @@ def init_pass_2(partition, center, rad_in, rad_out):
     return kwargs
 
 
-
-def pass_2(frame, mask, example_intensity):
-    example_intensity[:]=np.std(frame[mask==1])
+def masked_std(frame, mask, intensity):
+    intensity[:] = np.std(frame[mask == 1])
     return
-   
-    
 
 
-
-def run_analysis(ctx, dataset, center, rad_in, rad_out):
-    
-    pass_2_results = map_frames(
-        ctx=ctx,
+def run_fem(ctx, dataset, center, rad_in, rad_out):
+    pass_results = ctx.run_udf(
         dataset=dataset,
-        make_result_buffers=make_result_buffers,
-        merge=pass_2_merge,
-        init_fn=functools.partial(init_pass_2, center=center, rad_in=rad_in, rad_out=rad_out),
-        frame_fn=pass_2,
+        make_buffers=make_result_buffers,
+        init=functools.partial(init, center=center, rad_in=rad_in, rad_out=rad_out),
+        fn=masked_std,
     )
 
-    return (pass_2_results)
+    return (pass_results)

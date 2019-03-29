427f2f5ed2ce4fa22225f77e2412046efbeced8b
Jae <seya1214@gmail.com>
2019-03-29 14:55:55 +0100

    tests

--- tests/udf/test_simple_udf.py
+++ tests/udf/test_simple_udf.py
@@ -1,11 +1,12 @@
-import collections 
+import collections
 
 import numpy as np
 
 from libertem.common.buffers import BufferWrapper
 from libertem.api import Context
-from libertem.udf.stddev import minibatch, merge, part
+from libertem.udf.stddev import merge
 from utils import MemoryDataSet, _mk_random
+
 
 def test_sum_frames(lt_ctx):
     data = _mk_random(size=(16, 16, 16, 16), dtype="float32")
@@ -56,69 +57,73 @@ def test_3d_ds(lt_ctx):
     print(data.shape, res['pixelsum'].data.shape)
     assert np.allclose(res['pixelsum'].data, np.sum(data, axis=(1, 2)))
 
-def test_minibatch(batchsize = 32):
-    """
-    Test minibatch function from libertem/udf/stddev.py 
-    """
-    batch = [np.random.rand(2, 3) for i in range(batchsize)]
-    compute_batch = minibatch(batch)
+def test_minibatch(lt_ctx):
+    data = _mk_random(size=(16, 16, 16, 16), dtype="float32")
+    dataset = MemoryDataSet(data=data, tileshape=(1, 1, 16, 16),
+                            partition_shape=(4, 4, 16, 16), sig_dims=2)
 
-    sum_var, sum_im, N = compute_batch.sum_var, compute_batch.sum_im, compute_batch.N
-
-    assert N == len(batch) # check number of observations
-    assert np.allclose([sum_im], [sum(batch)]) # check sum of image pixels 
-
-    # check sum of variances 
-    var = np.var(batch, axis=0) * batchsize
-    assert np.allclose([var], [sum_var]) 
-
-def test_merge(batchsize = 64):
-    """
-    Test merge function from libertem/udf/stddev.py
-    """
-    batch = [np.random.rand(2, 3) for i in range(batchsize)]
-
-    obs0 = np.random.randint(5, 45)
-    obs1 = batchsize - obs0
-
-    batch0 = batch[:obs0]
-    batch1 = batch[obs0:]
-
-    sum_im0 = sum(batch0)
-    sum_im1 = sum(batch1)
-
-    sum_var0 = np.var(batch0, axis=0) * obs0
-    sum_var1 = np.var(batch1, axis=0) * obs1
+    def my_buffers():
+        return {
+            'batch' : BufferWrapper(
+                kind = 'sig', extra_shape = (3,), dtype = 'float32'
+                )
+        }
 
     VariancePart = collections.namedtuple('VariancePart', ['sum_var', 'sum_im', 'N'])
 
-    p0 = VariancePart(sum_var = sum_var0, sum_im = sum_im0, N = obs0)
-    p1 = VariancePart(sum_var = sum_var1, sum_im = sum_im1, N = obs1)
+    def my_frame_fn(frame, batch):
 
-    compute_merge = merge(p0, p1)
-    sum_var, sum_im, N = compute_merge.sum_var, compute_merge.sum_im, compute_merge.N
+        var = np.square(frame - np.mean(frame))
 
-    assert N == obs0 + obs1 
+        if batch[:, :, 2][0][0] == 0:
+            batch[:, :, 0] = var
 
-    # check sum of pixels 
-    sum_ims = sum(batch)
-    assert np.allclose([sum_ims], [sum_im])
+        else:
+            
+            p0 = VariancePart(sum_var = batch[:, :, 0], sum_im = batch[:, :, 1], N = batch[:, :, 2][0][0])
+            p1 = VariancePart(sum_var = var, sum_im = frame, N = 1)
+            compute_merge = merge(p0, p1)
 
-    # check sum of variances 
-    var = np.var(batch, axis = 0) * batchsize
-    assert np.allclose([var], [sum_var])
+            sum_var, sum_im, N = compute_merge.sum_var, compute_merge.sum_im, compute_merge.N
 
-def test_part():
-    """
-    Test part function from libertem/udf/stddev.py
-    """
-    num_ims = 1024
-    data = [np.random.rand(16, 16, 16, 16) for i in range(num_ims)]
+            batch[:, :, 0] = sum_var
 
-    compute_part = part(data)
-    sum_var, sum_im, N = compute_part.sum_var, compute_part.sum_im, compute_part.N
+        batch[:, :, 1] += frame
+        batch[:, :, 2] += 1
 
-    assert N == num_ims
-    assert np.allclose([sum_var], [np.var(data, axis = 0) * num_ims])
-    assert np.allclose([sum_im], [sum(data)])
+
+    def stddev_merge(dest, src):
+
+        p0 = VariancePart(sum_var = dest['batch'][:, :, 0], sum_im = dest['batch'][:, :, 1], N = dest['batch'][:, :, 2][0][0])
+        p1 = VariancePart(sum_var = src['batch'][:, :, 0], sum_im = src['batch'][:, :, 1], N = src['batch'][:, :, 2][0][0])
+        compute_merge = merge(p0, p1)
+
+        sum_var, sum_im, N = compute_merge.sum_var, compute_merge.sum_im, compute_merge.N
+        dest['batch'][:, :, 0] = sum_var
+        dest['batch'][:, :, 1] = sum_im
+        dest['batch'][:, :, 2] = N
+
+    res = lt_ctx.run_udf(
+        dataset=dataset,
+        fn=my_frame_fn,
+        make_buffers=my_buffers,
+        merge=stddev_merge,
+    )
+
+    assert 'batch' in res
+
+    N = data.shape[2] * data.shape[3]
+    assert res['batch'].data[:, :, 2][0][0] == N # check the total number of frames 
+
+    assert np.allclose(res['batch'].data[:, :, 1], np.sum(data, axis = (0, 1))) # check sum of frames
+
+    # TO DO : at the moment, the below gives Assertion error
+    ims = [data[:, :, i, j] for i in range(data.shape[2]) for j in range(data.shape[3])]
+    mean = sum(ims)/N
+    sum_var = sum([np.square(i-mean) for i in ims])
+    assert np.allclose(sum_var, res['batch'].data[:, :, 0]) # check sum of variances 
+
+if __name__ == '__main__':
+    with Context() as ctx:
+        test_minibatch(ctx) 
 

888a267d6ec21bd185792d77f1699cfc2739e327
Rahul Chandra <1rahulchandra1@gmail.com>
2019-03-30 17:45:26 +0530

    Trying to add a new mask(square)

--- src/libertem/analysis/square.py
+++ src/libertem/analysis/square.py
@@ -3,31 +3,29 @@ from libertem.viz import visualize_simple
 from .base import AnalysisResult, AnalysisResultSet
 from .masks import BaseMasksAnalysis
 
-
-class RingMaskAnalysis(BaseMasksAnalysis):
-    def get_results(self, job_results):
+class SquareMaskAnalysis(BaseMasksAnalysis):
+    def get_results(self,job_results):
+        data=job_results[0]
         shape = tuple(self.dataset.shape.nav)
-        data = job_results[0].reshape(shape)
 
-        
-        if data.dtype.kind == 'c':
+        if data.dtype.kind==='c':
             return AnalysisResultSet(
                 self.get_complex_results(
                     data,
                     key_prefix='intensity',
                     title='intensity',
-                    desc="intensity of the integration over the selected ring",
+                    desc='intensity over the selected the selected square',
                 )
             )
         return AnalysisResultSet([
             AnalysisResult(
-                raw_data=data,
+                raw_data=data.reshape(shape),
                 visualized=visualize_simple(data),
                 key="intensity",
                 title="intensity",
-                desc="intensity of the integration over the selected ring"),
+                desc="intensity of the integeration over the selected disk"),
         ])
-
+    
     def get_mask_factories(self):
         if self.dataset.shape.sig.dims != 2:
             raise ValueError("can only handle 2D signals currently")
@@ -35,32 +33,30 @@ class RingMaskAnalysis(BaseMasksAnalysis):
 
         cx = self.parameters['cx']
         cy = self.parameters['cy']
-        ri = self.parameters['ri']
-        ro = self.parameters['ro']
+        side= self.parameters['side']
 
-        def _ring_inner():
-            return masks.ring(
+        def square_mask():
+            return mask.square(
+
                 centerX=cx,
                 centerY=cy,
                 imageSizeX=detector_x,
                 imageSizeY=detector_y,
-                radius=ro,
-                radius_inner=ri
+                sideValue=side,
             )
+        
+        return [
+            square_mask,
+        ]
 
-        return [_ring_inner]
-
-    def get_parameters(self, parameters):
-        (detector_y, detector_x) = self.dataset.shape.sig
+    def get_parameters(self,parameters):
+        (detector_y,detector_x)=self.dataset.shape.sig
 
         cx = parameters.get('cx', detector_x / 2)
         cy = parameters.get('cy', detector_y / 2)
-        ro = parameters.get('ro', min(detector_y, detector_x) / 2)
-        ri = parameters.get('ri', ro * 0.8)
-
-        return {
-            'cx': cx,
-            'cy': cy,
-            'ri': ri,
-            'ro': ro,
+        side=parameters.get('side',min(detector_y,detector_x)/2*0.3)
+        return{
+            'cx':cx,
+            'cy':cy,
+            'side':side,
         }

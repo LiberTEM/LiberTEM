348c1c3103ec749538cf001f25621a3554ab400e
Jae <seya1214@gmail.com>
2019-04-02 01:34:13 +0200

    need more fixes

--- src/libertem/udf/stddev.py
+++ src/libertem/udf/stddev.py
@@ -1,54 +1,92 @@
-# import functools
 import collections
 
 from libertem.common.buffers import BufferWrapper
 
-# from libertem.udf import ResultBuffer, map_frames
 
 VariancePart = collections.namedtuple('VariancePart', ['sum_var', 'sum_im', 'N'])
 
 
-def my_buffer_batch():
+# def my_buffer_batch():
+#     return {
+#         'batch': BufferWrapper(
+#             kind='sig', extra_shape=(3,), dtype='float32'
+#             )
+#     }
+    
+
+def batch_buffer():
     return {
-        'batch': BufferWrapper(
-            kind='sig', extra_shape=(3,), dtype='float32'
+        'stddev': BufferWrapper(
+            kind='sig', dtype='float32'
+            ),
+        'num_frame': BufferWrapper(
+            kind='nav', dtype='float32'
+            ),
+        'sum_frame': BufferWrapper(
+            kind='sig', dtype='float32'
             )
     }
 
 
-def my_frame_fn_batch(frame, batch):
-    if batch[:, :, 2][0][0] == 0:
-        batch[:, :, 0] = 0
+def compute_batch(frame, stddev, sum_frame, num_frame): 
+    if num_frame == 0:
+        stddev = 0
 
     else:
-        p0 = VariancePart(sum_var=batch[:, :, 0], sum_im=batch[:, :, 1], N=batch[:, :, 2][0][0])
+        p0 = VariancePart(sum_var=stddev, sum_im=sum_frame, N=num_frame)
         p1 = VariancePart(sum_var=0, sum_im=frame, N=1)
         compute_merge = merge(p0, p1)
 
         sum_var = compute_merge.sum_var
+        stddev = sum_var
 
-        batch[:, :, 0] = sum_var
-
-    batch[:, :, 1] += frame
-    batch[:, :, 2] += 1
+    sum_frame += frame
+    num_frame += 1  
 
 
-def stddev_merge(dest, src):
-    sum_var1 = dest['batch'][:, :, 0]
-    sum_var2 = src['batch'][:, :, 0]
-    sum_im1 = dest['batch'][:, :, 1]
-    sum_im2 = src['batch'][:, :, 1]
-    obs1 = dest['batch'][:, :, 2][0][0]
-    obs2 = src['batch'][:, :, 2][0][0]
+# def my_frame_fn_batch(frame, batch):
+#     if batch[:, :, 2][0][0] == 0:
+#         batch[:, :, 0] = 0
 
-    p0 = VariancePart(sum_var=sum_var1, sum_im=sum_im1, N=obs1)
-    p1 = VariancePart(sum_var=sum_var2, sum_im=sum_im2, N=obs2)
+#     else:
+#         p0 = VariancePart(sum_var=batch[:, :, 0], sum_im=batch[:, :, 1], N=batch[:, :, 2][0][0])
+#         p1 = VariancePart(sum_var=0, sum_im=frame, N=1)
+#         compute_merge = merge(p0, p1)
+
+#         sum_var = compute_merge.sum_var
+
+#         batch[:, :, 0] = sum_var
+
+#     batch[:, :, 1] += frame
+#     batch[:, :, 2] += 1
+
+
+def batch_merge(dest, src):
+    p0 = VariancePart(sum_var=dest['stddev'], sum_im=dest['sum_frame'], N=dest['num_frame'])
+    p1 = VariancePart(sum_var=src['stddev'], sum_im=src['sum_frame'], N=src['num_frame'])
     compute_merge = merge(p0, p1)
 
-    sum_var, sum_im, N = compute_merge.sum_var, compute_merge.sum_im, compute_merge.N
-    dest['batch'][:, :, 0] = sum_var
-    dest['batch'][:, :, 1] = sum_im
-    dest['batch'][:, :, 2] = N
+    dest['stddev'] = compute_merge.sum_var
+    dest['sum_frame'] = compute_merge.sum_frame
+    dest['num_frame'] = compute_merge.num_frame
+
+
+# def stddev_merge(dest, src):
+#     sum_var1 = dest['batch'][:, :, 0]
+#     sum_var2 = src['batch'][:, :, 0]
+#     sum_im1 = dest['batch'][:, :, 1]
+#     sum_im2 = src['batch'][:, :, 1]
+#     obs1 = dest['batch'][:, :, 2][0][0]
+#     obs2 = src['batch'][:, :, 2][0][0]
+
+#     p0 = VariancePart(sum_var=sum_var1, sum_im=sum_im1, N=obs1)
+#     p1 = VariancePart(sum_var=sum_var2, sum_im=sum_im2, N=obs2)
+#     compute_merge = merge(p0, p1)
+
+#     sum_var, sum_im, N = compute_merge.sum_var, compute_merge.sum_im, compute_merge.N
+#     dest['batch'][:, :, 0] = sum_var
+#     dest['batch'][:, :, 1] = sum_im
+#     dest['batch'][:, :, 2] = N
 
 
 def merge(p0, p1):
@@ -56,6 +94,22 @@ def merge(p0, p1):
     Given two sets of partitions, with sum of frames
     and sum of variances, compute joint sum of frames
     and sum of variances using one pass algorithm
+
+    Parameters 
+    ----------
+    p0 : collections.namedtuple object 
+        Contains information about the first partition, including 
+        sum of variances, sum of pixels, and number of frames used
+
+    p1 : collections.namedtuple object
+        Contains information about the second partition, including 
+        sum of variances, sum of pixels, and number of frames used
+
+    Returns 
+    -------
+    collections.namedtuple object
+        Contains information about the merged partitions, including
+        sum of variances, sum of pixels, and number of frames used
     """
     if p0.N == 0:
         return VariancePart(sum_var=p1.sum_var, sum_im=p1.sum_im, N=p1.N)
@@ -79,5 +133,27 @@ def merge(p0, p1):
     return VariancePart(sum_var=sum_var_AB, sum_im=sum_im_AB, N=N)
 
 
-def run_analysis(ctx, dataset, parameters):
-    pass
+def run_analysis(ctx, dataset):
+    """
+    Extended description of the function
+
+    Parameters  
+    ----------
+    ctx : Context class
+    dataset : STEM data (filetype : raw, hd5f)
+
+    Returns
+    -------
+    bts 
+        A buffer that contains sum of variances, sum of pixels, and 
+        number of frames used to compute these statistic
+
+    """
+    pass_results = ctx.run_udf(
+        dataset=dataset,
+        fn=compute_batch, 
+        make_buffers=batch_buffer,
+        merge=batch_merge,
+    )
+
+    return (pass_results)
--- tests/udf/test_simple_udf.py
+++ tests/udf/test_simple_udf.py
@@ -1,10 +1,11 @@
 import collections
+import functools 
 
 import numpy as np
 
 from libertem.common.buffers import BufferWrapper
 from libertem.api import Context
-from libertem.udf.stddev import my_frame_fn_batch, my_buffer_batch, stddev_merge, merge
+from libertem.udf.stddev import merge, batch_merge, compute_batch, batch_buffer
 from utils import MemoryDataSet, _mk_random
 
 def test_sum_frames(lt_ctx):
@@ -62,19 +63,33 @@ def test_minibatch(lt_ctx):
     dataset = MemoryDataSet(data=data, tileshape=(1, 1, 16, 16),
                             partition_shape=(4, 4, 16, 16), sig_dims=2)
 
-    res = lt_ctx.run_udf(
+    # res = lt_ctx.run_udf(
+    #     dataset=dataset,
+    #     fn=my_frame_fn_batch,
+    #     make_buffers=my_buffer_batch,
+    #     merge=stddev_merge,
+    # )
+
+    # assert 'batch' in res
+
+    # N = data.shape[2] * data.shape[3]
+    # assert res['batch'].data[:, :, 2][0][0] == N # check the total number of frames 
+
+    # assert np.allclose(res['batch'].data[:, :, 1], np.sum(data, axis=(0, 1))) # check sum of frames
+
+    # sum_var = np.var(data, axis=(0, 1))
+    # assert np.allclose(sum_var, res['batch'].data[:, :, 0]/N) # check sum of variances
+
+    bts = lt_ctx.run_udf(
         dataset=dataset,
-        fn=my_frame_fn_batch,
-        make_buffers=my_buffer_batch,
-        merge=stddev_merge,
+        fn=compute_batch, 
+        make_buffers=functools.partial(
+            batch_buffer,
+        ),
+        merge=batch_merge,
     )
 
-    assert 'batch' in res
+    # assert 'batch_buffer' in bts
+    # assert 'sum_frame' in bts
+    # assert 'num_frame' in bts
 
-    N = data.shape[2] * data.shape[3]
-    assert res['batch'].data[:, :, 2][0][0] == N # check the total number of frames 
-
-    assert np.allclose(res['batch'].data[:, :, 1], np.sum(data, axis = (0, 1))) # check sum of frames
-
-    sum_var = np.var(data, axis=(0, 1))
-    assert np.allclose(sum_var, res['batch'].data[:, :, 0]/N) # check sum of variances

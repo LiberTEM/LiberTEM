d57f1c603fff9fcd20bd32570ce49420b515cb1d
Karina Ruzaeva <kruzaeva@fz-juelich.de>
2019-04-04 16:10:29 +0200

    Documentation for FEM UDF was added, closes #317

--- src/libertem/udf/FEM.py
+++ src/libertem/udf/FEM.py
@@ -39,6 +39,34 @@ def masked_std(frame, mask, intensity):
 
 
 def run_fem(ctx, dataset, center, rad_in, rad_out):
+    """
+    Return a standard deviation(SD) value for each frame of pixels which belong to ring mask.
+    Parameters
+    ----------
+    ctx: Context
+        Context class that contains methods for loading datasets,
+        creating jobs on them and running them
+
+    dataset: DataSet
+        A dataset with 1- or 2-D scan dimensions and 2-D frame dimensions
+
+    center: tuple
+        (x,y) - coordinates of a center of a ring for a masking region of interest to calculate SD
+
+    rad_in: int
+        Inner radius of a ring mask
+
+    rad_out: int
+        Outer radius of a ring mask
+
+    Returns
+    -------
+    pass_results: dict
+        Returns a standard deviation(SD) value for each frame of pixels which belong to ring mask.
+        To return 2-D array use pass_results['intensity'].data
+
+    """
+
     pass_results = ctx.run_udf(
         dataset=dataset,
         make_buffers=make_result_buffers,

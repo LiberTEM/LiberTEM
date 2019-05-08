0d8a688e393b29d29b9a11771f01a6493e783ffd
Dieter Weber <d.weber@fz-juelich.de>
2019-05-08 17:03:20 +0200

    Reduce copied code with frame visualization function

--- src/libertem/udf/blobfinder.py
+++ src/libertem/udf/blobfinder.py
@@ -3,6 +3,7 @@ import functools
 import numpy as np
 from skimage.feature import peak_local_max
 import scipy.ndimage as nd
+import matplotlib.pyplot as plt
 
 from libertem.udf import check_cast
 from libertem.common.buffers import BufferWrapper
@@ -433,3 +434,74 @@ def run_refine(ctx, dataset, zero, a, b, corr_params, match_params, indices=None
         ),
     )
     return (result, indices)
+
+
+# Visualize the refinement of a specific frame in matplotlib axes
+def visualize_frame(ctx, ds, result, indices, r, y, x, axes, colors=None, stretch=10):
+    # Get the frame from the dataset
+    get_sample_frame = ctx.create_pick_analysis(dataset=ds, y=y, x=x)
+    sample_frame = ctx.run(get_sample_frame)
+
+    d = sample_frame[0].raw_data
+
+    axes.imshow(np.log(d - np.min(d) + 1))
+
+    refined = result['refineds'].data[y, x]
+    elevations = result['peak_elevations'].data[y, x]
+    selector = result['selector'].data[y, x]
+
+    max_elevation = np.max(elevations)
+
+    # Calclate the best fit positions to compare with the
+    # individual peak positions.
+    # A difference between best fit and individual peaks highlights outliers.
+    calculated = grm.calc_coords(
+        zero=result['zero'].data[y, x],
+        a=result['a'].data[y, x],
+        b=result['b'].data[y, x],
+        indices=indices
+    )
+
+    paint_markers(
+        axes=axes,
+        r=r,
+        refined=refined,
+        normalized_elevations=elevations/max_elevation,
+        calculated=calculated,
+        selector=selector,
+        zero=result['zero'].data[y, x],
+        a=result['a'].data[y, x],
+        b=result['b'].data[y, x],
+        colors=colors,
+        stretch=stretch,
+    )
+
+
+def paint_markers(axes, r, refined, normalized_elevations, calculated, selector, zero, a, b,
+        colors=None, stretch=10):
+    if colors is None:
+        colors = {
+            'marker': 'w',
+            'arrow': 'r',
+            'missing': 'r',
+            'a': 'b',
+            'b': 'g',
+        }
+
+    plt.arrow(*np.flip(zero), *(np.flip(a)), color=colors['a'])
+    plt.arrow(*np.flip(zero), *(np.flip(b)), color=colors['b'])
+
+    # Plot markers for the individual peak positions.
+    # The alpha channel represents the peak elevation, which is used as a weight in the fit.
+    for i in range(len(refined)):
+        p = np.flip(refined[i])
+        a = max(0, normalized_elevations[i])
+        p0 = np.flip(calculated[i])
+        if selector[i]:
+            axes.add_artist(plt.Circle(p, r, color=colors['marker'], fill=False, alpha=a))
+            axes.add_artist(plt.Circle(p0, 1, color=colors['arrow'], fill=True, alpha=a))
+            axes.arrow(*p0, *(p-p0)*stretch, color=colors['arrow'], alpha=a)
+        else:
+            (yy, xx) = calculated[i]
+            xy = (xx - r, yy - r)
+            axes.add_artist(plt.Rectangle(xy, 2*r, 2*r, color=colors['missing'], fill=False))

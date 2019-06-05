c6b8ba8a5fa7983c82a559506f835e7f3ccd2de9
Karina KR. Ruzaeva <ruzaeva@iffw2k.kfa-juelich.de>
2019-06-05 10:52:07 +0200

    UDF for feature vector making + test

---
+++ src/libertem/udf/feature_vector_maker.py
@@ -0,0 +1,88 @@
+import functools
+
+from libertem.common.buffers import BufferWrapper
+from libertem.masks import _make_circular_mask
+from libertem.udf.stddev import run_stddev
+from skimage.feature import peak_local_max
+
+
+def make_result_buffers(coordinates):
+    return {
+        'feature_vec': BufferWrapper(
+            kind="nav", extra_shape=(coordinates.shape[0],), dtype="float32"
+        ),
+    }
+
+
+def vec_form(frame, coordinates, savg, delta, feature_vec):
+    ref = savg[coordinates[:, 0], coordinates[:, 1]]
+    for j in range(0, coordinates.shape[0]):
+        if frame[coordinates[j, 0], coordinates[j, 1]]-ref[j] > delta:
+            feature_vec[j] = 1
+    return
+
+
+def make_feature_vec(ctx, dataset, num, delta, center=None, rad_in=None, rad_out=None):
+    """
+    Return a value after integration of Fourier spectrum for each frame over ring.
+    Parameters
+    ----------
+    ctx: Context
+        Context class that contains methods for loading datasets,
+        creating jobs on them and running them
+
+    dataset: DataSet
+        A dataset with 1- or 2-D scan dimensions and 2-D frame dimensions
+
+    num: int
+        Number of possible peak positions to detect (better put higher value,
+         the output is limited to number of peaks the algorithm could find)
+
+    delta: int
+        Intensity difference between current frame and reference image for decision
+        making for feature vector value
+
+    rad_in: int, optional
+        Inner radius in pixels of a ring to mask region of interest of SD image to delete outliers
+        for peak finding
+
+    rad_out: int, optional
+        Outer radius in pixels of a ring to mask region of interest of SD image to delete outliers
+        for peak finding
+
+    real_center: tuple, optional
+        (y,x) - pixels, coordinates of a ring to mask region of interest of SD image 
+        to delete outliers for peak finding
+
+    Returns
+    -------
+    pass_results: dict
+        Returns a feature vector for each frame. 
+        "1" - denotes presence of peak for current frame for given possible peak position,
+        "0" - absence of peak for current frame for given possible peak position,
+        To return 2-D array use pass_results['feature_vec'].data
+
+    coordinates: numpy array of int
+        Returns array of coordinates of possible peaks positions
+
+    """
+    res_stat = run_stddev(ctx, dataset)
+    savg = res_stat['mean']
+    sstd = res_stat['std']
+    sshape = sstd.shape
+    if not (center is None or rad_in is None or rad_out is None):
+        mask_out = 1*_make_circular_mask(center[1], center[0], sshape[1], sshape[0], rad_out)
+        mask_in = 1*_make_circular_mask(center[1], center[0], sshape[1], sshape[0], rad_in)
+        mask = mask_out - mask_in
+        masked_sstd = sstd*mask
+    else:
+        masked_sstd = sstd
+    coordinates = peak_local_max(masked_sstd, num_peaks=num, min_distance=0)
+    vec_form_ = functools.partial(vec_form, coordinates=coordinates, delta=delta, savg=savg)
+    pass_results = ctx.run_udf(
+        dataset=dataset,
+        make_buffers=functools.partial(make_result_buffers, coordinates=coordinates),
+        fn=vec_form_,
+    )
+
+    return (pass_results, coordinates)

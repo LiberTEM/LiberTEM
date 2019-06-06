3cd951437698b5c7348121b7d6dbf04d34a7bfe5
Dieter Weber <d.weber@fz-juelich.de>
2019-06-06 21:50:39 +0200

    Avoid building a large dense mask stack with sparse matrices
    That can eat up a lot of memory

--- src/libertem/analysis/radialfourier.py
+++ src/libertem/analysis/radialfourier.py
@@ -116,11 +116,19 @@ class RadialFourierAnalysis(BaseMasksAnalysis):
                 imageSizeX=detector_x,
                 imageSizeY=detector_y
             )
-            rings = rings.reshape((rings.shape[0], 1, *rings.shape[1:])).astype(np.complex64)
-            ring_stack = [rings] * len(orders)
-            ring_stack = sparse.concatenate(ring_stack, axis=1)
+
+            bin_width = ro - ri
+            bin_area = np.pi * ro**2 - np.pi * (ro - bin_width)**2
             modulator = np.exp(phi * orders[:, np.newaxis, np.newaxis] * 1j)
-            ring_stack *= modulator
+            # Actually dense
+            if bin_area / (detector_x * detector_y) > 0.05:
+                ring_stack = rings.todense()
+                ring_stack = ring_stack[:, np.newaxis, ...] * modulator
+            else:
+                rings = rings.reshape((rings.shape[0], 1, *rings.shape[1:])).astype(np.complex64)
+                ring_stack = [rings] * len(orders)
+                ring_stack = sparse.concatenate(ring_stack, axis=1)        
+                ring_stack *= modulator
             return ring_stack.reshape((-1, detector_y, detector_x))
 
         return stack

963ce8a0310dbb6a5559f629c9cf8cb655ebd03c
Jae <seya1214@gmail.com>
2019-06-12 12:57:25 +0200

    fixed

--- docs/source/udf/buffer_types.py
+++ docs/source/udf/buffer_types.py
@@ -1,22 +1,30 @@
 from libertem.udf import UDF
 
-class test_buffer(UDF):
+class TestBuffer(UDF):
 
-	def get_results_buffer(self):
+	def get_result_buffers(self):
 		"""
 		Initialize a buffer of different types
 
+		Suppose our dataset has dimension (14, 14, 32, 32),
+		where the first two dimensions represent the navigation
+		dimension and the last two dimensions represent the signal dimension.
+
 		The first buffer "single_buffer" specifies a buffer of
-		dimension (16, 16) with entries of type float32
+		dimension (16, 16) with entries of type float32.
 
 		The second buffer "sig_buffer" specifies a buffer whose
-		first component dimension is increased by 2 from that of
-		signal dimension
+		dimension is initialized to be the same as the signal dimension.
+		With 'extra_shape = (2,)', two additional dimensions are added
+		to the first dimension of the signal dimension, so the resulting
+		dimension for this buffer will be (34, 32)
 
 		The third buffer "nav_buffer" specifies a buffer whose
-		first component dimension is increased by 1 and the second
-		component dimension is increased by 2 from that of navigation
-		dimension
+		dimension is initialized to be the same as the navigation dimension.
+		With 'extra_shape = (1, 2)', one additional dimension is added to the
+		first dimension of the navigation dimension and two additional
+		dimensions are added to the second dimension of the navigation dimension,
+		so the resulting dimension for this buffer will be (15, 16)
 		"""
 		return {
 			"single_buffer": self.buffer(
@@ -30,6 +38,7 @@ class test_buffer(UDF):
 				extra_shape=(2,), 
 				dtype="float32",
 				),
+			
 			"nav_buffer": self.buffer(
 				kind="nav",
 				extra_shape=(1, 2),

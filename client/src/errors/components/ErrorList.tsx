afdedaeb586e210eceaa566af10a47b7f501383d
Alexander Clausen <alex@gc-web.de>
2020-01-08 15:43:36 +0100

    Allow dismissing error messages via escape key, closes #437

--- client/src/errors/components/ErrorList.tsx
+++ client/src/errors/components/ErrorList.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { Modal } from "semantic-ui-react";
 import { RootReducer } from "../../store";
 import Error from "./Error";
+import { Actions } from "../actions";
 
 const mapStateToProps = (state: RootReducer) => {
     return {
@@ -19,6 +20,22 @@ const ErrorList: React.SFC<MergedProps> = ({ errors, clusterConnected, channelCo
     const numShown = 3;
     const latestErrors = errors.ids.slice(Math.max(0, errors.ids.length - numShown));
     const showModal = errors.ids.length > 0 && clusterConnected && channelConnected;
+
+    const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        const handleEsc = (ev: KeyboardEvent) => {
+            if(ev.code === "Escape" || ev.keyCode === 27) {
+                dispatch(Actions.dismissAll());
+            }
+        }
+        document.addEventListener("keyup", handleEsc);
+
+        return () => {
+            document.removeEventListener("keyup", handleEsc);
+        };
+    });
+
     return (
         <Modal open={showModal}>
             {latestErrors.map(error => <Error error={errors.byId[error]} key={error} />)}
--- src/libertem/web/client/service-worker.js
+++ src/libertem/web/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/precache-manifest.8ec0d0467175fd9e99dc19da8e35519e.js"
+  "/static/precache-manifest.586c88a725d227d7940bdfe88f0c2024.js"
 );
 
 self.addEventListener('message', (event) => {

fa98a4ba09eb5b1876dd08d302a6d960c4380302
Dieter Weber <d.weber@fz-juelich.de>
2020-02-05 18:39:55 +0100

    Port rawfft to UDF; add tests
    * Use UDF back-end and disable JOB back-end
    * Test for actual FFT

--- src/libertem/analysis/rawfft.py
+++ src/libertem/analysis/rawfft.py
@@ -1,35 +1,29 @@
 import numpy as np
-from libertem.viz import visualize_simple
 from libertem.masks import _make_circular_mask
-from .base import AnalysisResult, AnalysisResultSet
 from .raw import PickFrameAnalysis
 
 
 class PickFFTFrameAnalysis(PickFrameAnalysis):
     def get_results(self, job_results):
-        data, coords = self.get_results_base(job_results)
+        # Make sure we don't use legacy code from superclasses
+        raise NotImplementedError
+
+    def get_job(self):
+        # Make sure we don't use legacy code from superclasses
+        raise NotImplementedError
+
+    def get_udf_results(self, udf_results, roi):
+        data = udf_results['intensity'].data[0]
         real_rad = self.parameters.get("real_rad")
         real_center = (self.parameters.get("real_centery"), self.parameters.get("real_centerx"))
         if data.dtype.kind == 'c':
-            return AnalysisResultSet(
-                self.get_complex_results(
-                    job_results,
-                    key_prefix="intensity",
-                    title="intensity",
-                    desc="the frame at %s" % (coords,),
-                )
-            )
+            return self.get_generic_results(data)
         if not (real_center is None or real_rad is None):
-            sigshape = job_results.shape
+            sigshape = data.shape
             real_mask = 1-1*_make_circular_mask(
-                    real_center[1], real_center[0], sigshape[1], sigshape[0], real_rad
-                )
-            visualized = visualize_simple(np.fft.fftshift(abs(np.fft.fft2(data*real_mask))),
-                logarithmic=True)
+                real_center[1], real_center[0], sigshape[1], sigshape[0], real_rad
+            )
+            fft_data = np.fft.fftshift(abs(np.fft.fft2(data*real_mask)))
         else:
-            visualized = visualize_simple(np.fft.fftshift(abs(np.fft.fft2(data))), logarithmic=True)
-        return AnalysisResultSet([
-            AnalysisResult(raw_data=data, visualized=visualized,
-                           key="intensity", title="intensity",
-                           desc="the frame at %s" % (coords,)),
-        ])
+            fft_data = np.fft.fftshift(abs(np.fft.fft2(data)))
+        return self.get_generic_results(fft_data)
--- tests/test_analysis_rawfft.py
+++ tests/test_analysis_rawfft.py
@@ -2,20 +2,26 @@ import numpy as np
 
 from libertem.analysis.rawfft import PickFFTFrameAnalysis
 from libertem.io.dataset.memory import MemoryDataSet
+from libertem.masks import _make_circular_mask
+
+from utils import _mk_random
 
 
 def test_pick_fft_defaults(lt_ctx):
-    data = np.zeros([3*3, 8, 8]).astype(np.float32)
+    data = _mk_random([3*3, 8, 8], dtype=np.float32)
     dataset = MemoryDataSet(data=data, tileshape=(1, 8, 8),
                             num_partitions=2, sig_dims=2)
     analysis = PickFFTFrameAnalysis(dataset=dataset, parameters={
         'x': 1,
     })
     res = lt_ctx.run(analysis)
+    fft_data = np.fft.fftshift(abs(np.fft.fft2(data[1])))
+
+    assert np.allclose(res.intensity.raw_data, fft_data)
 
 
 def test_pick_fft_masked(lt_ctx):
-    data = np.zeros([3*3, 8, 8]).astype(np.float32)
+    data = _mk_random([3*3, 8, 8], dtype=np.float32)
     dataset = MemoryDataSet(data=data, tileshape=(1, 8, 8),
                             num_partitions=2, sig_dims=2)
     analysis = PickFFTFrameAnalysis(dataset=dataset, parameters={
@@ -24,4 +30,10 @@ def test_pick_fft_masked(lt_ctx):
         'real_centerx': 1,
         'real_centery': 1,
     })
+    real_mask = np.invert(_make_circular_mask(
+        centerX=1, centerY=1, imageSizeX=8, imageSizeY=8, radius=1
+    ))
+    fft_data = np.fft.fftshift(abs(np.fft.fft2(data[1]*real_mask)))
     res = lt_ctx.run(analysis)
+
+    assert np.allclose(res.intensity.raw_data, fft_data)

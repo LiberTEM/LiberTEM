b20b21dd455ae14d6f202259b995231e3f889e69
Anand Baburajan <anandbaburajan@gmail.com>
2020-02-22 23:48:41 +0530

    Rebuilt production version of client

--- src/libertem/web/client/service-worker.js
+++ src/libertem/web/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/precache-manifest.34e139816c81c91a0f0e8bed6b78d5a5.js"
+  "/static/precache-manifest.e7e54f17fd662aa035dafb5befadb013.js"
 );
 
 self.addEventListener('message', (event) => {

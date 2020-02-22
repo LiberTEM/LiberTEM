920ac9fb01cfe9746a9418536918810fe6437d04
Dieter Weber <d.weber@fz-juelich.de>
2020-02-22 15:42:47 +0100

    Numba-based one-pass StdDevUDF
    This version is slightly faster for the benign case of signal buffers
    that fit comfortably into the CPU caches.
    
    For the malign case of signal buffers that don't fit the caches well,
    it is 5x-7x faster since it eliminates large intermediate buffers and
    calculates each pixel in one pass.
    
    * Added test for numerical stability.
    * Added citation

--- src/libertem/udf/stddev.py
+++ src/libertem/udf/stddev.py
@@ -1,118 +1,115 @@
-import collections
-
 import numpy as np
 import numba
 
 from libertem.udf import UDF
 
 
-VariancePart = collections.namedtuple('VariancePart', ['var', 'sum_im', 'N'])
+@numba.njit(fastmath=True)
+def _merge(N, N0, sum_im0, var0, N1, sum_im1, var1):
+    '''
+    Basic function to perform numerically stable merge :cite:`Schubert2018`
+    '''
+    # compute mean for each partitions
+    mean_A = sum_im0 / N0
+    mean_B = sum_im1 / N1
+
+    # compute mean for joint samples
+    delta = mean_B - mean_A
+    mean = mean_A + (N1 * delta) / N
+
+    # compute sum of images for joint samples
+    sum_im_AB = sum_im0 + sum_im1
+
+    # compute sum of variances for joint samples
+    delta_P = mean_B - mean
+    var_AB = var0 + var1 + (N1 * delta * delta_P)
+    return sum_im_AB, var_AB
 
 
-@numba.njit
-def merge(N0, sum_im0, var0, N1, sum_im1, var1):
+@numba.njit(fastmath=True)
+def merge(dest_N, dest_sum, dest_varsum, src_N, src_sum, src_varsum):
     """
     Given two sets of partitions, with sum of frames
-    and sum of variances, compute joint sum of frames
-    and sum of variances using one pass algorithm
+    and sum of variances, aggregate joint sum of frames
+    and sum of variances in destination partition using one pass
+    algorithm :cite:`Schubert2018`
 
     Parameters
     ----------
-    N0, sum_im0, var0
-        Contains information about the first partition:
-        Number of frames used, sum of pixels, sum of variances
+    dest_N : int
+        Number of frames aggregated in dest_sum and dest_varsum
+        The case :code:`dest_N == 0` is handled correctly.
+    dest_sum, dest_varsum
+        Aggregation buffers, will be updated: sum of pixels, sum of variances
+    src_N, src_sum, src_varsum
+        Information about the second partition
 
-    N1, sum_im1, var1
-        Contains information about the second partition:
-        
 
     Returns
     -------
-    N, sum_im, var:
-        Number of frames used, sum of pixels, sum of variances of the merged partitions
+    N:
+        New number of frames aggregated in aggregation buffer
     """
-    if N0 == 0:
-        return N1, sum_im1, var1
-    if N1 == 0:
-        return N0, sum_im0, var0
-    N = N0 + N1
+    if dest_N == 0:
+        dest_sum[:] = src_sum
+        dest_varsum[:] = src_varsum
+        return src_N
+    else:
+        N = dest_N + src_N
 
-    shape = sum_im0.shape
-
-    sum_im0_ = sum_im0.flatten()
-    sum_im1_ = sum_im1.flatten()
-    sum_im_AB = np.zeros_like(sum_im0_)
-    var0_ = var0.flatten()
-    var1_ = var1.flatten()
-    var_AB = np.zeros_like(var0_)
-
-    le = len(sum_im0_)
-    BLOCKSIZE = 16
-    n_blocks = le // BLOCKSIZE
-
-    def _merge(sum_im0, var0, sum_im1, var1):
-        # compute mean for each partitions
-        mean_A = sum_im0 / N0
-        mean_B = sum_im1 / N1
-
-        # compute mean for joint samples
-        delta = mean_B - mean_A
-        mean = mean_A + (N1 * delta) / N
-
-        # compute sum of images for joint samples
-        sum_im_AB = sum_im0 + sum_im1
-
-        # compute sum of variances for joint samples
-        delta_P = mean_B - mean
-        var_AB = var0 + var1 + (N1 * delta * delta_P)
-        return sum_im_AB, var_AB
-
-    for block in range(n_blocks):
-        for i in range(block*BLOCKSIZE, (block+1)*BLOCKSIZE):
-            sum_im_AB[i], var_AB[i] = _merge(
-                sum_im0_[i], var0_[i], sum_im1_[i], var1_[i]
+        le = len(dest_sum)
+        for i in range(le):
+            dest_sum[i], dest_varsum[i] = _merge(
+                N,
+                dest_N, dest_sum[i], dest_varsum[i],
+                src_N, src_sum[i], src_varsum[i]
             )
-    for i in range(n_blocks*BLOCKSIZE, le):
-        sum_im_AB[i], var_AB[i] = _merge(
-            sum_im0_[i], var0_[i], sum_im1_[i], var1_[i]
+    return N
+
+
+@numba.njit(fastmath=True)
+def process_tile(tile, N0, sum_inout, var_inout):
+    '''
+    Compute sum and variance of :code:`tile` along navigation axis
+    and merge into aggregation buffers. Numerical "workhorse" for
+    :meth:`StdDevUDF.process_tile`.
+
+    Parameters
+    ----------
+    tile : numpy.ndarray
+        Tile with flattened signal dimension
+    N0 : int
+        Number of frames already aggegated in partition buffer
+        Cannot be 0 -- the initial case is handled in :meth:`StdDevUDF.process_tile`
+        For unknown reasons, handling the case N0 == 0 in this function degraded performance
+        significantly. Re-check with a newer compiler version!
+    sum_inout, var_inout : numpy.ndarray
+        Aggregation buffers with flattened signal dimension. The tile
+        will be merged into these buffers (call by reference)
+
+    Returns
+    -------
+    N : int
+        New number of frames in aggregation buffer
+    '''
+    N1 = tile.shape[0]
+    le = tile.shape[1]
+    N = N0 + N1
+    for i in range(le):
+        s = 0
+        for j in range(N1):
+            s += tile[j, i]
+        mean = s / N1
+        varsum = 0
+        for j in range(N1):
+            varsum += (tile[j, i] - mean)**2
+
+        sum_inout[i], var_inout[i] = _merge(
+            N,
+            N0, sum_inout[i], var_inout[i],
+            N1, s, varsum
         )
-
-    return N, sum_im_AB.reshape(shape), var_AB.reshape(shape)
-
-
-@numba.njit
-def tile_sum_var(tile):
-    BLOCKSIZE = 16
-    shape = tile.shape[1:]
-    N = tile.shape[0]
-    # Flatten signal dimension
-    tile_ = tile.reshape((N, -1))
-    le = tile_.shape[1]
-    s = np.zeros(le, dtype=tile_.dtype)
-    v = np.zeros(le, dtype=tile_.dtype)
-
-    n_blocks = le // BLOCKSIZE
-
-    means = np.zeros(BLOCKSIZE, dtype=tile_.dtype)
-
-    for block in range(n_blocks):
-        offset = block*BLOCKSIZE
-        for j in range(N):
-            for i in range(offset, offset + BLOCKSIZE):
-                s[i] += tile_[j, i]
-        for i in range(BLOCKSIZE):
-            means[i] = s[i + offset] / N
-        for j in range(N):
-            for i in range(offset, offset + BLOCKSIZE):
-                v[i] += (tile_[j, i] - means[i - offset])**2
-    for i in range(n_blocks*BLOCKSIZE, le):
-        for j in range(N):
-            s[i] += tile_[j, i]
-        mean = s[i] / N
-        for j in range(N):
-            v[i] += (tile_[j, i] - mean)**2
-
-    return s.reshape(shape), v.reshape(shape)
+    return N
 
 
 # Helper function to make sure the frame count
@@ -130,9 +127,8 @@ class StdDevUDF(UDF):
     """
     Compute sum of variances and sum of pixels from the given dataset
 
-    One-pass algorithm used in this code is taken from the following paper:
-    "Numerically Stable Parallel Computation of (Co-) Variance"
-    DOI : https://doi.org/10.1145/3221269.3223036
+    The one-pass algorithm used in this code is taken from the following paper:
+    :cite:`Schubert2018`
 
     Examples
     --------
@@ -157,7 +153,7 @@ class StdDevUDF(UDF):
 
         Returns
         -------
-        A dictionary that maps 'var', 'std', 'mean', 'num_frame', 'sum_frame' to
+        A dictionary that maps 'var',  'num_frame', 'sum_frame' to
         the corresponding BufferWrapper objects
         """
         return {
@@ -192,20 +188,31 @@ class StdDevUDF(UDF):
             Partial results that contains sum of variances, sum of frames, and the
             number of frames used over current iteration of partition
         """
-        N0 = _validate_n(dest['num_frame'][0])
-        sum_im0 = dest['sum_frame'][:]
-        var0 = dest['var'][:]
+        dest_N = _validate_n(dest['num_frame'][0])
+        src_N = _validate_n(src['num_frame'][0])
 
-        N1 = _validate_n(src['num_frame'][0])
-        sum_im1 = src['sum_frame'][:]
-        var1 = src['var'][:]
+        oldshape = dest['sum_frame'][:].shape
 
-        N, sum_im, var = merge(N0, sum_im0, var0, N1, sum_im1, var1)
+        dest['sum_frame'][:].shape = (-1,)
+        dest['var'][:].shape = (-1, )
+        src['sum_frame'][:].shape = (-1,)
+        src['var'][:].shape = (-1, )
 
-        dest['var'][:] = var
-        dest['sum_frame'][:] = sum_im
+        N = merge(
+            dest_N=dest_N,
+            dest_sum=dest['sum_frame'][:],
+            dest_varsum=dest['var'][:],
+            src_N=src_N,
+            src_sum=src['sum_frame'][:],
+            src_varsum=src['var'][:],
+        )
         for key in src['num_frame'][0]:
             dest['num_frame'][0][key] = N
+
+        dest['sum_frame'][:].shape = oldshape
+        dest['var'][:].shape = oldshape
+        src['sum_frame'][:].shape = oldshape
+        src['var'][:].shape = oldshape
 
     def process_tile(self, tile):
         """
@@ -223,22 +230,30 @@ class StdDevUDF(UDF):
         if key not in self.results.num_frame[0]:
             self.results.num_frame[0][key] = 0
 
-        tile_sum, tile_var = tile_sum_var(tile)
-
         N0 = self.results.num_frame[0][key]
-        sum_im0 = self.results.sum_frame
-        var0 = self.results.var
-
         N1 = tile.shape[0]
-        sum_im1 = tile_sum
-        var1 = tile_var
 
-        N, sum_im, var = merge(N0, sum_im0, var0, N1, sum_im1, var1)
+        oldshape = self.results.sum_frame.shape
 
-        self.results.var[:] = var
+        tile.shape = (N1, -1)
 
-        self.results.sum_frame[:] = sum_im
-        self.results.num_frame[0][key] = N
+        self.results.sum_frame.shape = (-1, )
+        self.results.var.shape = (-1, )
+
+        if N0 == 0:
+            self.results.sum_frame[:] = tile.sum(axis=0)
+            self.results.var[:] = np.var(tile, axis=0, ddof=N1-1)
+            self.results.num_frame[0][key] = N1
+        else:
+            self.results.num_frame[0][key] = process_tile(
+                tile=tile,
+                N0=N0,
+                sum_inout=self.results.sum_frame,
+                var_inout=self.results.var
+            )
+        self.results.sum_frame.shape = oldshape
+        self.results.var.shape = oldshape
+        tile.shape = (N1, *oldshape)
 
 
 def consolidate_result(udf_result):

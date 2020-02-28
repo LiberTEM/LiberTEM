6673aa3180104a13d8032666c7e7ff2fa7375a71
Dieter Weber <d.weber@fz-juelich.de>
2020-02-28 14:06:27 +0100

    Ensure coverage of blocked loop
    The small previous shape only covered the tail handling.

--- tests/udf/test_stddev.py
+++ tests/udf/test_stddev.py
@@ -22,10 +22,10 @@ def test_stddev(lt_ctx, use_roi):
     lt_ctx
         Context class for loading dataset and creating jobs on them
     """
-    data = _mk_random(size=(16, 17, 18, 19), dtype="float32")
+    data = _mk_random(size=(30, 2, 1025), dtype="float32")
     # FIXME the tiling in signal dimension can only be tested once MemoryDataSet
     # actually supports it
-    dataset = MemoryDataSet(data=data, tileshape=(3, 2, 16),
+    dataset = MemoryDataSet(data=data, tileshape=(3, 1, 1025),
                             num_partitions=8, sig_dims=2)
     if use_roi:
         roi = np.random.choice([True, False], size=dataset.shape.nav)

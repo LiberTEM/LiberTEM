0977eb1022b4ec4a78afad411101697bf804cc22
Alexander Clausen <alex@gc-web.de>
2020-03-19 15:54:48 +0100

    Fix __all__, sphinx wants str[]

--- src/libertem/io/dataset/base/__init__.py
+++ src/libertem/io/dataset/base/__init__.py
@@ -8,9 +8,9 @@ from .part3d import Partition3D, File3D, FileSet3D
 from .utils import FileTree
 
 __all__ = [
-    DataSetException, DataSetMeta, PartitionStructure,
-    _roi_to_nd_indices, _roi_to_indices,
-    DataSet, WritableDataSet, Partition, WritablePartition,
-    DataTile, Partition3D, File3D, FileSet3D,
-    FileTree,
+    'DataSetException', 'DataSetMeta', 'PartitionStructure',
+    '_roi_to_nd_indices', '_roi_to_indices',
+    'DataSet', 'WritableDataSet', 'Partition', 'WritablePartition',
+    'DataTile', 'Partition3D', 'File3D', 'FileSet3D',
+    'FileTree',
 ]

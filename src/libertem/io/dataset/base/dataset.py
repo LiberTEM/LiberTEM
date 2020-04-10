da1dac6d78df99ec5b70fa30e8802d5c114d4d62
Anand Baburajan <anandbaburajan@gmail.com>
2020-04-10 17:24:49 +0530

    Updated detect and dropdown

--- src/libertem/io/dataset/base/dataset.py
+++ src/libertem/io/dataset/base/dataset.py
@@ -66,7 +66,7 @@ class DataSet(object):
         raise NotImplementedError()
 
     @classmethod
-    def detect_params(cls, path, executor, info=False):
+    def detect_params(cls, path, executor):
         """
         Guess if path can be opened using this DataSet implementation and
         detect parameters.

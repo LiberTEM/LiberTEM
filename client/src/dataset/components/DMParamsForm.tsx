49a01242e064a899490f1506ada00d8e988ffba2
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2020-04-13 23:21:52 +0530

    Updated dmParamsForm to support scan_size

--- client/src/dataset/components/DMParamsForm.tsx
+++ client/src/dataset/components/DMParamsForm.tsx
@@ -3,14 +3,17 @@ import * as React from "react";
 import { Button, Form } from "semantic-ui-react";
 import { Omit } from "../../helpers/types";
 import { DatasetParamsDM, DatasetTypes } from "../../messages";
-import { getInitialName, withValidation } from "../helpers";
+import { getInitial, getInitialName, parseNumList, withValidation } from "../helpers";
 import { OpenFormProps } from "../types";
 
 // some fields have different types in the form vs. in messages
 type DatasetParamsDMForForm = Omit<DatasetParamsDM,
   "path"
   | "type"
->;
+  | "scan_size"
+> & {
+  scan_size: string,
+};
 
 type MergedProps = FormikProps<DatasetParamsDMForForm> & OpenFormProps<DatasetParamsDM>;
 
@@ -33,6 +36,11 @@ const DMParamsForm: React.SFC<MergedProps> = ({
         <ErrorMessage name="name" />
         <Field name="name" id="id_name" />
       </Form.Field>
+      <Form.Field>
+        <label htmlFor="id_scan_size">Scan Size:</label>
+        <ErrorMessage name="scan_size" />
+        <Field name="scan_size" id="id_scan_size" />
+      </Form.Field>
       <Button primary={true} type="submit" disabled={isSubmitting}>Load Dataset</Button>
       <Button type="button" onClick={onCancel}>Cancel</Button>
       <Button type="button" onClick={handleReset}>Reset</Button>
@@ -43,12 +51,14 @@ const DMParamsForm: React.SFC<MergedProps> = ({
 export default withValidation<DatasetParamsDM, DatasetParamsDMForForm>({
   mapPropsToValues: ({ path, initial }) => ({
     name: getInitialName("name", path, initial),
+    scan_size: getInitial("scan_size", "", initial).toString(),
   }),
   formToJson: (values, path) => {
     return {
       path,
       type: DatasetTypes.DM,
       name: values.name,
+      scan_size: parseNumList(values.scan_size),
     };
   },
   type: DatasetTypes.DM,

d5dba109ff28351d1a22a8ea4b2217fcf9ab8c5e
abijith-bahuleyan <abijithbahuleyan2@gmail.com>
2020-04-24 20:21:01 +0530

    rebuilt client

--- src/libertem/web/client/service-worker.js
+++ src/libertem/web/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./static/precache-manifest.7a38a35139b6d280651f4964abb7dbb5.js"
+  "./static/precache-manifest.63edb0838a4c7ee7ffd128b17a4d573f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./static/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

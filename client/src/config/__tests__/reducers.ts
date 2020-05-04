b98d57dc695bba638bb2025a9c77664c8e4a027f
Alexander Clausen <alex@gc-web.de>
2020-05-05 00:26:09 +0200

    Remove initial state duplication

--- client/src/config/__tests__/reducers.ts
+++ client/src/config/__tests__/reducers.ts
@@ -1,30 +1,15 @@
-import { ClusterTypes } from "../../messages";
 import { Actions } from "../actions";
-import { configReducer, ConfigState } from "../reducers";
+import { configReducer, initialConfigState } from "../reducers";
 
 const somePath = "/some/path/"
 
-const initialState: ConfigState = {
-    separator: '/', version: '', revision: '', cwd: '/',
-    lastOpened: {},
-    fileHistory: [],
-    resultFileFormats: {},
-    localCores: 0,
-    haveConfig: true,
-    lastConnection: {
-        type: ClusterTypes.LOCAL,
-        address: "tcp://localhost:8786",
-    },
-    starred: [],
-};
-
 describe('browser reducer', () => {
     it('should allow to add a favorite', () => {
-        const newState = configReducer(initialState, Actions.toggleStar(somePath));
+        const newState = configReducer(initialConfigState, Actions.toggleStar(somePath));
         expect(newState.starred).toEqual(["/some/path/"]);
     });
     it('should allow to remove a favorite', () => {
-        const intermediateState = configReducer(initialState, Actions.toggleStar(somePath));
+        const intermediateState = configReducer(initialConfigState, Actions.toggleStar(somePath));
         const newState = configReducer(intermediateState, Actions.toggleStar(somePath));
         expect(newState.starred).toEqual([]);
     });

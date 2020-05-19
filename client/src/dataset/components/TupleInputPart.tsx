75e54c26ad4f489cd1c5b1e558c32ac05e67f9ad
Anand Baburajan <anandbaburajan@gmail.com>
2020-05-19 18:09:16 +0530

    RAW's detector_size

---
+++ client/src/dataset/components/TupleInputPart.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+interface TupleInputPartProps {
+    name: string,
+    id: string,
+    tupleKey: number,
+    value: number,
+    tupleInputChangeHandle: (idx: number, value: string) => void,
+    commaPressHandle: (idx: number) => void,
+    tupleRef: (ref:HTMLInputElement) => void,
+}
+
+const TupleInputPart: React.FC<TupleInputPartProps> = ({ name, id, tupleKey, tupleInputChangeHandle, value, commaPressHandle, tupleRef }) => {
+
+   const onPartChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      tupleInputChangeHandle(tupleKey, e.target.value);
+   }
+
+   const onCommaPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+     if(e.keyCode === 188) {
+       commaPressHandle(tupleKey);
+     }
+   }
+
+   return <input type="number" name={name} id={id} key={tupleKey} onChange={onPartChange} value={value} onKeyDown={onCommaPress} ref={tupleRef} />;
+}
+
+export default TupleInputPart;

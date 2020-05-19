1d9eaf61409bfe7333a8e4e0630a8124efaf8724
Anand Baburajan <anandbaburajan@gmail.com>
2020-05-19 20:50:03 +0530

    Fix scan_size to 2D

--- client/src/dataset/components/MIBParamsForm.tsx
+++ client/src/dataset/components/MIBParamsForm.tsx
@@ -45,7 +45,7 @@ const MIBFileParamsForm: React.SFC<MergedProps> = ({
             <Form.Field>
                 <label htmlFor="id_scan_size_0">Scan Size:</label>
                 <ErrorMessage name="scan_size" />
-                <TupleInput value={values.scan_size} minLen={2} maxLen={4} fieldName="scan_size" setFieldValue={setFieldValue} />
+                <TupleInput value={values.scan_size} minLen={2} maxLen={2} fieldName="scan_size" setFieldValue={setFieldValue} />
             </Form.Field>
             <Button primary={true} type="submit" disabled={isSubmitting || isValidating}>Load Dataset</Button>
             <Button type="button" onClick={onCancel}>Cancel</Button>

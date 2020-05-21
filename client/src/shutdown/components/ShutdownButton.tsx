b19fd7e327d13a332b391b5bc4bf465a30095846
abijith-bahuleyan <abijithbahuleyan2@gmail.com>
2020-05-21 21:44:49 +0530

    improved modal

--- client/src/shutdown/components/ShutdownButton.tsx
+++ client/src/shutdown/components/ShutdownButton.tsx
@@ -1,19 +1,29 @@
 import * as React from "react";
+import { connect } from "react-redux";
 import { Button, Header, Icon, Modal } from "semantic-ui-react";
 import { handleSubmit } from "../api";
+import { RootReducer } from "../../store";
 
-class ShutdownButton extends React.Component {
+const mapStateToProps = (state: RootReducer) => {
+    return {
+        channel: state.channelStatus.status,
+    };
+};
+
+type MergedProps = ReturnType<typeof mapStateToProps>;
+
+class ShutdownButton extends React.Component<MergedProps> {
     public state = {
-        modelOpen: false,
+        modal: false,
         shutdown: false,
     };
 
-    public handleOpen = () => {
-        this.setState({ modelOpen: true });
+    public modalOpen = () => {
+        this.setState({ modal: true });
     };
 
-    public handleClose = () => {
-        this.setState({ modelOpen: false });
+    public modalClose = () => {
+        this.setState({ modal: false });
     };
 
     public handleShutdown = () => {
@@ -21,20 +31,29 @@ class ShutdownButton extends React.Component {
         handleSubmit();
     };
 
+    //rename renderForm
+    public handleModal() {
+        const { shutdown } = this.state;
+        const { channel } = this.props;
+
+        if (channel === "waiting" && shutdown) {
+            this.setState({ shutdown: false });
+            this.modalClose();
+        }
+    }
+
     public render() {
+        {
+            this.handleModal();
+        }
         return (
-            <Modal
-                trigger={<Button content="Shutdown" icon="shutdown" onClick={this.handleOpen} labelPosition="left" floated="right" />}
-                open={this.state.modelOpen}
-                onClose={this.handleClose}
-                size="mini"
-            >
+            <Modal trigger={<Button content="Shutdown" icon="shutdown" onClick={this.modalOpen} labelPosition="left" floated="right" />} open={this.state.modal} onClose={this.modalClose} size="mini">
                 <Header icon="shutdown" content="Confirm shutdown" />
                 <Modal.Content>
                     <p>Do you want to shutdown ?</p>
                 </Modal.Content>
                 <Modal.Actions>
-                    <Button onClick={this.handleClose}>
+                    <Button onClick={this.modalClose} disabled={this.state.shutdown}>
                         <Icon name="remove" /> Cancel
                     </Button>
                     <Button primary={true} loading={this.state.shutdown} disabled={this.state.shutdown} onClick={this.handleShutdown}>
@@ -46,4 +65,4 @@ class ShutdownButton extends React.Component {
     }
 }
 
-export default ShutdownButton;
+export default connect(mapStateToProps)(ShutdownButton);

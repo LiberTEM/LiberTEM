58eb7af6ab5cd22f9e8325636feb1618b162bcf1
Alexander Clausen <alex@gc-web.de>
2020-06-08 20:30:20 +0200

    Add support for excluded_pixels in CorrectionSet

--- src/libertem/corrections/corrset.py
+++ src/libertem/corrections/corrset.py
@@ -1,11 +1,12 @@
 import numpy as np
+import sparse
 
 from libertem.common import Slice
 from libertem.corrections.detector import correct
 
 
 class CorrectionSet:
-    def __init__(self, dark=None, gain=None):
+    def __init__(self, dark=None, gain=None, excluded_pixels=None):
         """
         A set of corrections to apply.
 
@@ -14,15 +15,27 @@ class CorrectionSet:
         Parameters
         ----------
         dark : np.ndarray
-            A 2D array containing a dark frame to substract from all frames,
+            A ND array containing a dark frame to substract from all frames,
             its shape needs to match the signal shape of the dataset.
 
         gain : np.ndarray
-            A 2D array containing a gain map to multiply with each frame,
+            A ND array containing a gain map to multiply with each frame,
             its shape needs to match the signal shape of the dataset.
+
+        excluded_pixels : sparse.COO
+            A "sparse pydata" COO array containing only entries for pixels
+            that should be excluded. The shape needs to match the signal
+            shape of the dataset. Can also be anything that is directly
+            compatible with the :code:`sparse.COO` constructor, for example a
+            "roi-like" numpy array. A :code:`sparse.COO` array can be
+            directly constructed from a coordinate array, using
+            :code:`sparse.COO(coords=coords, data=1, shape=ds.shape.sig)`
         """
         self._dark = dark
         self._gain = gain
+        if excluded_pixels is not None:
+            excluded_pixels = sparse.COO(excluded_pixels)
+        self._excluded_pixels = excluded_pixels
 
     def get_dark_frame(self):
         return self._dark
@@ -30,10 +43,16 @@ class CorrectionSet:
     def get_gain_map(self):
         return self._gain
 
+    def get_excluded_pixels(self):
+        return self._excluded_pixels
+
     def have_corrections(self):
-        dark_frame = self.get_dark_frame()
-        gain_map = self.get_gain_map()
-        return dark_frame is not None or gain_map is not None
+        corrs = [
+            self.get_dark_frame(),
+            self.get_gain_map(),
+            self.get_excluded_pixels(),
+        ]
+        return any(c is not None for c in corrs)
 
     def apply(self, data: np.ndarray, tile_slice: Slice):
         """
@@ -42,18 +61,25 @@ class CorrectionSet:
         """
         dark_frame = self.get_dark_frame()
         gain_map = self.get_gain_map()
+        excluded_pixels = self.get_excluded_pixels()
 
         if not self.have_corrections():
             return
 
+        sig_slice = tile_slice.get(sig_only=True)
+
         if dark_frame is not None:
-            dark_frame = dark_frame[tile_slice.get(sig_only=True)]
+            dark_frame = dark_frame[sig_slice]
         if gain_map is not None:
-            gain_map = gain_map[tile_slice.get(sig_only=True)]
+            gain_map = gain_map[sig_slice]
+        if excluded_pixels is not None:
+            excluded_pixels = excluded_pixels[sig_slice]
+            excluded_pixels = excluded_pixels.coords
         correct(
             buffer=data,
             dark_image=dark_frame,
             gain_map=gain_map,
-            excluded_pixels=None,
+            excluded_pixels=excluded_pixels,
             inplace=True,
+            sig_shape=tuple(tile_slice.shape.sig),
         )
--- tests/corrections/test_corrset.py
+++ tests/corrections/test_corrset.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pytest
+import sparse
 
 from libertem.corrections import CorrectionSet
 from libertem.udf.sum import SumUDF
 
 
-@pytest.mark.with_numba
 @pytest.mark.parametrize("gain,dark", [
     (np.zeros((128, 128)), np.ones((128, 128))),
     (np.zeros((128, 128)), None),
@@ -22,7 +22,6 @@ def test_correction_set_zero_gain(lt_ctx, default_raw, gain, dark):
     assert np.allclose(res['intensity'], 0)
 
 
-@pytest.mark.with_numba
 @pytest.mark.parametrize("gain,dark", [
     (np.ones((128, 128)), np.ones((128, 128))),
     (None, np.ones((128, 128))),
@@ -37,3 +36,32 @@ def test_correction_set_dark_one(lt_ctx, default_raw, default_raw_data, gain, da
         corrections=corr
     )
     assert np.allclose(res['intensity'], np.sum(default_raw_data - 1, axis=(0, 1)))
+
+
+def test_patch_pixels(lt_ctx, default_raw, default_raw_data):
+    udf = SumUDF()
+
+    # test with empty excluded_pixels array
+    corr = CorrectionSet(excluded_pixels=np.array([
+        (), ()
+    ]).astype(np.int64), gain=np.ones((128, 128)))
+    res = lt_ctx.run_udf(
+        dataset=default_raw,
+        udf=udf,
+        corrections=corr
+    )
+    assert np.allclose(res['intensity'], np.sum(default_raw_data, axis=(0, 1)))
+
+
+def test_patch_pixels_only_excluded_pixels(lt_ctx, default_raw, default_raw_data):
+    udf = SumUDF()
+    excluded_pixels = sparse.COO(
+        np.zeros((128, 128))
+    )
+    corr = CorrectionSet(excluded_pixels=excluded_pixels)
+    res = lt_ctx.run_udf(
+        dataset=default_raw,
+        udf=udf,
+        corrections=corr
+    )
+    assert np.allclose(res['intensity'], np.sum(default_raw_data, axis=(0, 1)))

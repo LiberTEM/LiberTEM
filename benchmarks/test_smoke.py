1b27f67e13b72172a0e8eb2f7f03861229bae552
Dieter Weber <d.weber@fz-juelich.de>
2020-06-25 17:03:51 +0200

    Sneak in a forgotten benchmark

---
+++ benchmarks/test_smoke.py
@@ -0,0 +1,24 @@
+import pytest
+import numba
+
+first = False
+
+@numba.njit
+def hello():
+    return "world"
+
+
+@pytest.mark.compilation
+@pytest.mark.benchmark(
+    group="compilation"
+)
+def test_numba_compilation(benchmark):
+    benchmark.extra_info["mark"] = "compilation"
+    benchmark.pedantic(hello, warmup_rounds=0, rounds=2, iterations=1)
+
+
+@pytest.mark.benchmark(
+    group="smoke"
+)
+def test_numba_performance(benchmark):
+    benchmark(hello)

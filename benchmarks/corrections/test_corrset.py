27e1b0288cd0fcaaba3776220b1f12dbc99f46aa
Dieter Weber <d.weber@fz-juelich.de>
2020-07-01 17:59:40 +0200

    WIP continue refactoring
    * More on code structure
    * More benchmarks
    
    TODO optimize the "diagonal case" for the direct method
    
    Co-authored-by: Alexander Clausen <a.clausen@fz-juelich.de>

--- benchmarks/corrections/test_corrset.py
+++ benchmarks/corrections/test_corrset.py
@@ -1,24 +1,39 @@
 import numpy as np
 import sparse
 
+import pytest
+
 from libertem.corrections import CorrectionSet
 
 
-def test_tileshape_adjustment_bench(benchmark):
+@pytest.mark.parametrize(
+    "base_shape", ((1, 1), (2, 2))
+)
+@pytest.mark.parametrize(
+    "excluded_coords", (
+        # These magic numbers are "worst case" to produce collisions
+        # 2*3*4*5*6*7
+        np.array([
+            (720, 210, 306),
+            (120, 210, 210)
+        ]),
+        # Diagonal that prevents any tiling
+        np.array([
+            range(1024),
+            range(1024),
+        ])
+
+    )
+)
+def test_tileshape_adjustment_bench(benchmark, base_shape, excluded_coords):
     sig_shape = (1014, 1024)
-    tile_shape = (1, 1)
-    base_shape = (1, 1)
-    # These magic numbers are "worst case" to produce collisions
-    # 2*3*4*5*6*7
-    excluded_coords = np.array([
-        (720, 210, 306),
-        (120, 210, 210)
-    ])
+    tile_shape = base_shape
     excluded_pixels = sparse.COO(coords=excluded_coords, shape=sig_shape, data=True)
     corr = CorrectionSet(excluded_pixels=excluded_pixels)
     adjusted = benchmark(
         corr.adjust_tileshape,
         tile_shape=tile_shape, sig_shape=sig_shape, base_shape=base_shape
     )
-    print(adjusted)
-    assert adjusted != (1014, 1024)
+    print("Base shape", base_shape)
+    print("Excluded coords", excluded_coords)
+    print("Adjusted", adjusted)

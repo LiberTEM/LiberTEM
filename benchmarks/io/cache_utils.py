fd6380726ac5dd49c83c88f821f3a473f58025cc
Dieter Weber <d.weber@fz-juelich.de>
2020-10-14 23:24:13 +0200

    Support Windows
    * Drop caches on Windows, confirmed to be effective
    * Allow to specify per-machine prefixes

--- benchmarks/io/cache_utils.py
+++ benchmarks/io/cache_utils.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 
 def warmup_cache(flist):
@@ -8,7 +9,23 @@ def warmup_cache(flist):
                 pass
 
 
-def drop_cache(flist):
-    for fname in flist:
-        with open(fname, "rb") as f:
-            os.posix_fadvise(f.fileno(), 0, 0, os.POSIX_FADV_DONTNEED)
+if platform.system() == "Windows":
+    import win32file
+
+    def drop_cache(flist):
+        for fname in flist:
+            f = win32file.CreateFile(
+                fname,  # fileName
+                win32file.GENERIC_READ,  # desiredAccess
+                win32file.FILE_SHARE_READ | win32file.FILE_SHARE_WRITE | win32file.FILE_SHARE_DELETE,  # shareMode
+                None,  # attributes
+                win32file.OPEN_EXISTING,  # CreationDisposition
+                win32file.FILE_FLAG_NO_BUFFERING,  # flagsAndAttributes
+                0,  # hTemplateFile
+            )
+            f.close()
+else:
+    def drop_cache(flist):
+        for fname in flist:
+            with open(fname, "rb") as f:
+                os.posix_fadvise(f.fileno(), 0, 0, os.POSIX_FADV_DONTNEED)

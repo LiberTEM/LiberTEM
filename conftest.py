8371ae5bc697fb930b276468b8e7b244a91942bd
Alexander Clausen <alex@gc-web.de>
2020-11-26 20:00:28 +0100

    adjust_event_loop_policy even earlier

--- conftest.py
+++ conftest.py
@@ -25,10 +25,6 @@ from libertem.web.state import SharedState
 from libertem.executor.base import AsyncAdapter, sync_to_async
 from libertem.utils.async_utils import adjust_event_loop_policy
 
-# event loop policy needs to be set as early as possible,
-# let's see if this is early enough:
-adjust_event_loop_policy()
-
 # A bit of gymnastics to import the test utilities since this
 # conftest.py file is shared between the doctests and unit tests
 # and this file is outside the package

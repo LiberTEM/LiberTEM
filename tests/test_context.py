f4978c3676a16f985e4567189bcaf77f83c5d4c5
Dieter Weber <d.weber@fz-juelich.de>
2020-12-07 19:01:28 +0100

    Test case for passing bad value for executor

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,3 +1,9 @@
+import pytest
+
+from libertem.executor.inline import InlineJobExecutor
+from libertem.api import Context
+
+
 def test_ctx_load(lt_ctx, default_raw):
     lt_ctx.load(
         "raw",
@@ -6,3 +12,9 @@ def test_ctx_load(lt_ctx, default_raw):
         dtype="float32",
         detector_size=(128, 128),
     )
+
+
+def test_context_arguments():
+    with pytest.raises(ValueError):
+        # refs https://github.com/LiberTEM/LiberTEM/issues/918
+        Context(executor=InlineJobExecutor)

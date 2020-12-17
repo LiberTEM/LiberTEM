8ba64304cc6aa7ee039a4d27d07f78cc9688b1bf
Alexander Clausen <alex@gc-web.de>
2020-12-17 17:56:22 +0100

    Add benchmark for numba warmup
    Note that it doesn't really make sense to run this as a part of the
    whole benchmark suite yet - needs to be properly isolated such that it
    doesn't depend on what benchmarks run before this one.

---
+++ benchmarks/common/test_numba_warmup.py
@@ -0,0 +1,13 @@
+import pytest
+
+from libertem.common.numba import prime_numba_cache
+
+
+@pytest.mark.benchmark()
+def test_numba_prime_cache(lt_ctx, benchmark, large_raw):
+    """
+    Primes the cache for the raw dataset. this only makes sense to be run individually, really,
+    or we need to find a way to dynamically set the numba cache path somehow, and/or run the
+    test in a subprocess.
+    """
+    benchmark.pedantic(prime_numba_cache, kwargs=dict(ds=large_raw), rounds=5, warmup_rounds=0)

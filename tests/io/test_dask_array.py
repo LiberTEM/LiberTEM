c16f1905ee5a91ec3ccb4a7399833ea408c04799
Alexander Clausen <alex@gc-web.de>
2021-01-20 14:28:08 +0100

    Reproducer and partial fix for make_dask_array + roi
    See also #933

--- tests/io/test_dask_array.py
+++ tests/io/test_dask_array.py
@@ -24,3 +24,20 @@ def test_dask_array():
         data.sum()
     )
     assert da.shape == data.shape
+
+
+def test_dask_array_with_roi():
+    data = _mk_random(size=(16, 16, 16, 16))
+    dataset = MemoryDataSet(
+        data=data,
+        tileshape=(16, 16, 16),
+        num_partitions=2,
+    )
+    roi = np.zeros(dataset.shape.nav, dtype=np.bool)
+    roi[0, 0] = True
+    (da, workers) = make_dask_array(dataset, roi=roi)
+    assert np.allclose(
+        da.compute(workers=workers, scheduler='single-threaded'),
+        data[0, 0]
+    )
+    assert da.shape == data[0, 0].shape

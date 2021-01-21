1ca10a1cc1877329743f974f3e2f7dce7043bb68
Alexander Clausen <alex@gc-web.de>
2021-01-21 16:51:10 +0100

    Need at least numba 0.51 for our hacked caching to work!

--- setup.py
+++ setup.py
@@ -146,7 +146,7 @@ setup(
         "psutil",
         # https://github.com/LiberTEM/LiberTEM/issues/763
         # https://github.com/LiberTEM/LiberTEM/issues/783
-        "numba>=0.49.1",
+        "numba>=0.51",
         "ncempy>=1.4",
         'pywin32!=226;platform_system=="Windows"',
         # FIXME pull request #259

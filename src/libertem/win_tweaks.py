640c7d8ad4d5e8936964091cbb9782698915c5e6
Dieter Weber <d.weber@fz-juelich.de>
2021-02-04 20:20:20 +0100

    Handle misconfigured pywin32 gracefully
    Show a warning pointing to the probable fix,
    disable the affected feature and continue running
    instead of failing hard.

--- src/libertem/win_tweaks.py
+++ src/libertem/win_tweaks.py
@@ -3,21 +3,42 @@ import msvcrt
 import ctypes
 from ctypes import windll, wintypes, byref
 import logging
+import warnings
 
-import win32security
-import pywintypes
 
 logger = logging.getLogger(__name__)
 
 
-def get_owner_name(full_path, stat):
-    try:
-        s = win32security.GetFileSecurity(full_path, win32security.OWNER_SECURITY_INFORMATION)
-        sid = s.GetSecurityDescriptorOwner()
-        (name, domain, t) = win32security.LookupAccountSid(None, sid)
-        return "%s\\%s" % (domain, name)
-    except pywintypes.error as e:
-        raise IOError(e)
+def get_owner_name_dummy(full_path, stat):
+    return "Unknown"
+
+
+try:
+    import win32security
+    import pywintypes
+
+    def get_owner_name(full_path, stat):
+        try:
+            s = win32security.GetFileSecurity(full_path, win32security.OWNER_SECURITY_INFORMATION)
+            sid = s.GetSecurityDescriptorOwner()
+            (name, domain, t) = win32security.LookupAccountSid(None, sid)
+            return "%s\\%s" % (domain, name)
+        except pywintypes.error as e:
+            raise IOError(e)
+
+except ModuleNotFoundError:
+    warnings.warn("Install package pywin32 to enable detecting file owners on Windows.")
+
+    get_owner_name = get_owner_name_dummy
+
+except ImportError as e:
+    warnings.warn(
+        f"Importing modules from pywin32 failed. {e} "
+        "Possibly the post installation script of pywin32 has to be run. More information: "
+        "https://github.com/mhammond/pywin32#installing-via-pip"
+    )
+
+    get_owner_name = get_owner_name_dummy
 
 
 ENABLE_QUICK_EDIT = 0x0040

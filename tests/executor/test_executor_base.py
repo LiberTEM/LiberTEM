8f675b9d4161b5aab6803ade2a699bce9e77c4e4
Alexander Clausen <alex@gc-web.de>
2021-03-11 10:53:42 +0100

    Use side-effect to check that async_generator_eager is actually eager

--- tests/executor/test_executor_base.py
+++ tests/executor/test_executor_base.py
@@ -1,5 +1,4 @@
 import time
-import asyncio
 import concurrent
 
 import pytest
@@ -17,53 +16,46 @@ def thread_pool():
 
 @pytest.mark.asyncio
 async def test_async_generator_eager(thread_pool):
-    def sync_generator():
-        t0 = time.time()
-        for i in range(2):
-            time.sleep(0.1)
-            yield 'result'
-        print(time.time() - t0)
+    results = []
 
-    t0 = time.time()
+    def sync_generator():
+        for i in range(20):
+            results.append('result')
+            yield 'result'
 
     gen = sync_generator()
     async_gen = async_generator_eager(gen, pool=thread_pool)
 
+    await async_gen.__anext__()
+    time.sleep(0.1)
+
+    # the generator has run to completion already:
+    assert len(results) == 20
+
     async for value in async_gen:
         assert value == 'result'
-        await asyncio.sleep(0.1)  # simulate something else happening in the main thread
 
-    t1 = time.time()
-    delta = t1 - t0
-
-    assert 0.3 <= delta < 0.4
-
-    t0 = time.time()
+    results = []
 
     gen = sync_generator()
     async_gen = async_generator(gen, pool=thread_pool)
 
+    await async_gen.__anext__()
+    time.sleep(0.1)
+
+    # the generator has only run a single iteration:
+    assert len(results) == 1
+
     async for value in async_gen:
         assert value == 'result'
-        await asyncio.sleep(0.1)  # simulate something else happening in the main thread
-
-    t1 = time.time()
-    delta_lazy = t1 - t0
-
-    assert 0.4 <= delta_lazy < 0.5
-
-    assert delta < delta_lazy
 
 
 @pytest.mark.asyncio
 async def test_async_generator_err_handling(thread_pool):
     def sync_generator():
-        t0 = time.time()
         for i in range(2):
-            time.sleep(0.01)
             yield 'result'
         raise RuntimeError("something unexpected happened")
-        print(time.time() - t0)
 
     gen = sync_generator()
     async_gen = async_generator_eager(gen, pool=thread_pool)

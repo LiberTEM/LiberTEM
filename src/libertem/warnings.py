a87e2823b68b224e3c0fbc722b826ad948464f7e
Alexander Clausen <alex@gc-web.de>
2021-03-25 11:48:12 +0100

    Remove unneccesary conversion to np.array

--- src/libertem/warnings.py
+++ src/libertem/warnings.py
@@ -1,3 +1,12 @@
+"""
+Custom warning subclasses. Useful for selectively filtering. For example, to
+cause an exception for all `UseDiscouragedWarning` that are thrown in the test
+suite, one can run:
+
+$ pytest -Werror:libertem.warnings.UseDiscouragedWarning tests/
+"""
+
+
 class UseDiscouragedWarning(FutureWarning):
     """
     This warning is thrown when a problematic feature of an API is used,

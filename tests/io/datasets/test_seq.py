08ad498515034feb3fa82a3212a69e41ca8c3466
DYVZ8X <puskaslevi99@gmail.com>
2021-07-22 11:30:57 +0200

    testing the tester

--- tests/io/datasets/test_seq.py
+++ tests/io/datasets/test_seq.py
@@ -22,20 +22,9 @@ except ModuleNotFoundError:
 SEQ_TESTDATA_PATH = os.path.join(get_testdata_path(), 'default.seq')
 HAVE_SEQ_TESTDATA = os.path.exists(SEQ_TESTDATA_PATH)
 
-#pytestmark = pytest.mark.skipif(not HAVE_SEQ_TESTDATA, reason="need .seq testdata")
+pytestmark = pytest.mark.skipif(not HAVE_SEQ_TESTDATA, reason="need .seq testdata")
 
-def test_xml_excluded_pixels_loading_unbind():
-    path = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' \
-           '<Configuration><PixelSize></PixelSize><DiffPixelSize></DiffPixelSize><BadPixels><BadPixelMap Rows="4096" ' \
-           'Columns="4096"><Defect Rows="2311-2312"/><Defect Rows="3413-3414"/></BadPixelMap><BadPixelMap Binning="2" ' \
-           'Rows="2048" Columns="2048"><Defect Rows="1155-1156"/><Defect Rows="1706-1707"/></BadPixelMap></BadPixels>' \
-           '</Configuration>'
-    ds = SEQDataSet(path=SEQ_TESTDATA_PATH, nav_shape=(8, 8), sig_shape=(1024, 1024))
-    test_arr = np.zeros((1024, 1024))
-    test_arr[775] = 1
-    test_arr[777] = 1
-    expected_res = ds.get_excluded_pixels()
-    assert not np.array_equal(test_arr, expected_res)
+
 @pytest.fixture
 def default_seq(lt_ctx):
     nav_shape = (8, 8)
@@ -138,6 +127,18 @@ def test_positive_sync_offset(default_seq, lt_ctx):
 
     assert np.allclose(result, result_with_offset)
 
+def test_xml_excluded_pixels_loading_unbind():
+    path = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' \
+           '<Configuration><PixelSize></PixelSize><DiffPixelSize></DiffPixelSize><BadPixels><BadPixelMap Rows="4096" ' \
+           'Columns="4096"><Defect Rows="2311-2312"/><Defect Rows="3413-3414"/></BadPixelMap><BadPixelMap Binning="2" ' \
+           'Rows="2048" Columns="2048"><Defect Rows="1155-1156"/><Defect Rows="1706-1707"/></BadPixelMap></BadPixels>' \
+           '</Configuration>'
+    ds = SEQDataSet(path=SEQ_TESTDATA_PATH, nav_shape=(8, 8), sig_shape=(1024, 1024))
+    test_arr = np.zeros((1024, 1024))
+    test_arr[775] = 1
+    test_arr[777] = 1
+    expected_res = ds.get_excluded_pixels()
+    assert not np.array_equal(test_arr, expected_res)
 
 def test_negative_sync_offset(default_seq, lt_ctx):
     udf = SumSigUDF()

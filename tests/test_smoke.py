197c1f278afc5af0d86c3acd7040a0e58530d5a9
Dieter Weber <d.weber@fz-juelich.de>
2021-09-03 14:39:29 +0200

    Test imports from old locations for backwards compatibility

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -16,6 +16,15 @@ import libertem.preload
 import libertem.udf
 import libertem.analysis.gridmatching   # NOQA: F401
 
+# Ensure imports still work for public or very common items after license review #1099
+from libertem.corrections import CorrectionSet, corrset, detector  # NOQA: F401
+from libertem.analysis.base import AnalysisResult, AnalysisResultSet, Analysis  # NOQA: F401
+from libertem.web.messages import MessageConverter  # NOQA: F401
+from libertem.viz.base import encode_image  # NOQA: F401
+from libertem.viz import encode_image as encode_image_2  # NOQA: F401
+from libertem.masks import to_dense, to_sparse, is_sparse  # NOQA: F401
+from libertem.utils.async_utils import sync_to_async  # NOQA: F401
+
 
 def test_stuff():
     print("stuff")

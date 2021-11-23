aaf770b70003e4c7fb3db2a6c91ce38bd0b79807
Dieter Weber <d.weber@fz-juelich.de>
2021-11-23 11:14:52 +0100

    Test for empty damage

--- tests/viz/test_gms.py
+++ tests/viz/test_gms.py
@@ -1,10 +1,12 @@
 from unittest import mock
 import sys
 
+import numpy as np
 import pytest
 
 from libertem.viz.gms import GMSLive2DPlot
 from libertem.udf.sum import SumUDF
+from libertem.udf.sumsigudf import SumSigUDF
 
 
 def test_gms_smoke(monkeypatch, lt_ctx, default_raw):
@@ -21,3 +23,17 @@ def test_gms_nodisplay(monkeypatch, lt_ctx, default_raw):
     plots = [GMSLive2DPlot(dataset=default_raw, udf=udf)]
     with pytest.warns(UserWarning, match="Plot is not displayed, not plotting."):
         lt_ctx.run_udf(dataset=default_raw, udf=udf, plots=plots)
+
+
+def test_empty(monkeypatch, lt_ctx, default_raw):
+    pytest.importorskip("bqplot")
+    udf = SumSigUDF()
+    monkeypatch.setitem(sys.modules, 'DigitalMicrograph', mock.MagicMock())
+    monkeypatch.setattr(lt_ctx, 'plot_class', GMSLive2DPlot)
+
+    def extract(udf_result, damage):
+        return udf_result['intensity'].data, np.zeros_like(damage)
+
+    plot = GMSLive2DPlot(dataset=default_raw, udf=udf, channel=extract)
+
+    lt_ctx.run_udf(dataset=default_raw, udf=udf, plots=[plot])

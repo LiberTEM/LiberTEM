bae10e3719aa0aa3e0ceb127861963bc4bd0ae9a
Dieter Weber <d.weber@fz-juelich.de>
2021-11-29 12:32:49 +0100

    Improved integration with other Dask workflows
    * Start matching Executor by querying Dask which scheduler it is using
    * Option to set the dask.distributed Client that the DaskJobExecutor
      starts in `make_local()` or `connect()` as the default Dask scheduler if desired.
    * Pass string as `executor` argument to `Context` to make a number
      of default cases easier to use, in particular the integration features.
      This also saves a lot of boilerplate when switching between default executor
      and inline executor for testing.
    
    Tests to confirm the assumptions. In particular, confirm that we don't set the
    Dask scheduler unless explicitly requested, and that we always clean it up
    after tests.

---
+++ src/libertem/executor/integration.py
@@ -0,0 +1,38 @@
+import types
+
+import dask
+import dask.delayed
+import dask.distributed as dd
+
+from .dask import DaskJobExecutor
+from .concurrent import ConcurrentJobExecutor
+from .inline import InlineJobExecutor
+
+
+def get_dask_integration_executor():
+    item = dask.delayed(1)
+    dask_scheduler = dask.base.get_scheduler(collections=[item])
+    # We first test for circumstances where we know how to return an adapted
+    # JobExecutor instance.
+    if isinstance(dask_scheduler, types.MethodType):
+        if isinstance(dask_scheduler.__self__, dd.Client):
+            return DaskJobExecutor(client=dask_scheduler.__self__)
+    elif dask_scheduler is dask.threaded.get:
+        if dask.threaded.default_pool:
+            return ConcurrentJobExecutor(client=dask.threaded.default_pool)
+    # ConcurrentJobExecutor is currently incompatible with ProcessPoolExecutor
+    # since it can't pickle local functions.
+    # Therefore, fall through to default case for now.
+    # elif dask_scheduler is dask.multiprocessing.get:
+    #     # FIXME more research needed if the pool is cached and if
+    #     # one can get hold of it.
+    #     return ConcurrentJobExecutor(
+    #         client=concurrent.futures.ProcessPoolExecutor(),
+    #         is_local=True
+    #     )
+    elif dask_scheduler is dask.local.get_sync:
+        return InlineJobExecutor()
+
+    # If we didn't return yet,
+    # we fall through to the default case.
+    return ConcurrentJobExecutor.make_local()

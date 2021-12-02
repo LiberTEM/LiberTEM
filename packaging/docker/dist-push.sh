52f73a19c5a0a8bb250d71d0cd1572c532401641
Dieter Weber <d.weber@fz-juelich.de>
2021-12-02 19:25:14 +0100

    Debugging why it failed

--- packaging/docker/dist-push.sh
+++ packaging/docker/dist-push.sh
@@ -3,6 +3,7 @@ if [ -n "$DOCKER_ACCESS_TOKEN" ] && [ -n "$DOCKER_USER" ]
 then
     cd packaging/docker/ || exit
     export DOCKER_BUILDKIT=1
+    echo "Pushing as $DOCKER_USER"
     docker login -u "$DOCKER_USER" --password-stdin <<< "$DOCKER_ACCESS_TOKEN"
     docker push libertem/libertem-dev
     docker push libertem/libertem:continuous

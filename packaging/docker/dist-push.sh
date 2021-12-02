e8d996f5c767d8dc55b0c987b5392791c931dcf8
Dieter Weber <d.weber@fz-juelich.de>
2021-12-02 17:16:31 +0100

    Review @sk1p: Restructure
    * Use single Docker file and ARG since the difference is minimal
    * Parameters to make the cache work
    * Stand-alone scripts instead of just in the azure-pipelines.yml
    * Div. tweaks
    
    Still TODO is user documentation
    
    Co-authored-by: Alexander Clausen <a.clausen@fz-juelich.de>

---
+++ packaging/docker/dist-push.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+if [ -n "$DOCKER_ACCESS_TOKEN" ] && [ -n "$DOCKER_USER" ]
+then
+    cd packaging/docker/ || exit
+    export DOCKER_BUILDKIT=1
+    docker login -u "$DOCKER_USER" --password-stdin <<< "$DOCKER_ACCESS_TOKEN"
+    docker push libertem/libertem-dev
+    docker push libertem/libertem:continuous
+else
+    echo "not authenticated, skipping"
+fi

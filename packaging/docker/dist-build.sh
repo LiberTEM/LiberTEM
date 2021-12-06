fec4f06ea0d498be70831f68ceb1c67254b39143
Dieter Weber <d.weber@fz-juelich.de>
2021-12-06 15:27:22 +0100

    Make sure to use the most recent base image
    This should prevent cache busting when building locally
    since CI likely uses the most recent base image.

--- packaging/docker/dist-build.sh
+++ packaging/docker/dist-build.sh
@@ -8,6 +8,7 @@ TRIAGE=libertem/libertem:triage
 
 ./update_reqs.sh
 
+docker pull python:3.9-slim
 docker pull $TRIAGE || true
 docker pull $DEV || true
 docker build --cache-from=$TRIAGE --cache-from=$DEV -t $TRIAGE --build-arg BUILDKIT_INLINE_CACHE=1 ../../ -f Dockerfile

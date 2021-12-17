617aeed57b0087b35e1ff6ac81754dfe49067c3b
Dieter Weber <d.weber@fz-juelich.de>
2021-12-17 13:12:43 +0100

    Include StdDevUDF in test lineup for delayed result

--- benchmarks/executor/test_delayed.py
+++ benchmarks/executor/test_delayed.py
@@ -1,10 +1,57 @@
 import numpy as np
-import dask.array as da
 import pytest
 
 from libertem.udf.base import UDF
+from libertem.udf.stddev import StdDevUDF
 from libertem.api import Context
 from libertem.executor.delayed import DelayedJobExecutor
+
+
+class MyStdDevUDF(StdDevUDF):
+    pass
+
+
+# FIXME check if renamed?
+if hasattr(MyStdDevUDF, 'dask_merge'):
+    delattr(MyStdDevUDF, 'dask_merge')
+
+
+class MyStdDevMergeUDF(MyStdDevUDF):
+    # Copied and adapted from tests/executor/test_delayed
+    # FIXME import instead?
+    def dask_merge(self, ordered_results):
+        n_frames = np.concatenate([[b.num_frames[0] for b in ordered_results.values()]])
+        pixel_sums = np.concatenate([[b.sum for b in ordered_results.values()]])
+        pixel_varsums = np.concatenate([[b.varsum for b in ordered_results.values()]])
+
+        # Expand n_frames to be broadcastable
+        extra_dims = pixel_sums.ndim - n_frames.ndim
+        n_frames = n_frames.reshape(n_frames.shape + (1,) * extra_dims)
+
+        cumulative_frames = np.cumsum(n_frames, axis=0)
+        cumulative_sum = np.cumsum(pixel_sums, axis=0)
+        sumsum = cumulative_sum[-1, ...]
+        total_frames = cumulative_frames[-1, 0]
+
+        mean_0 = cumulative_sum / cumulative_frames
+        # Handle the fact that mean_0 is indexed to results from
+        # up-to the partition before. We shift everything one to
+        # the right, and we don't care about result 0 because it
+        # is by definiition replaced with varsum[0, ...]
+        mean_0 = np.roll(mean_0, 1, axis=0)
+
+        mean_1 = pixel_sums / n_frames
+        delta = mean_1 - mean_0
+        mean = mean_0 + (n_frames * delta) / cumulative_frames
+        partial_delta = mean_1 - mean
+        varsum = pixel_varsums + (n_frames * delta * partial_delta)
+        varsum[0, ...] = pixel_varsums[0, ...]
+        varsum_cumulative = np.cumsum(varsum, axis=0)
+        varsum_total = varsum_cumulative[-1, ...]
+
+        self.results.get_buffer('sum').update_data(sumsum)
+        self.results.get_buffer('varsum').update_data(varsum_total)
+        self.results.get_buffer('num_frames').update_data(total_frames)
 
 
 class MySumUDF(UDF):
@@ -22,7 +69,7 @@ class MySumUDF(UDF):
 
 class MySumMergeUDF(MySumUDF):
     def dask_merge(self, ordered_results):
-        intensity = da.stack([b.intensity for b in ordered_results.values()]).sum(axis=0)
+        intensity = np.stack([b.intensity for b in ordered_results.values()]).sum(axis=0)
         self.results.get_buffer('intensity').update_data(intensity)
 
 
@@ -63,6 +110,50 @@ class EchoMergeUDF(EchoUDF):
 
 
 class Test:
+    @pytest.mark.benchmark(
+        group="std",
+    )
+    def test_std_baseline(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+        ctx = shared_dist_ctx_globaldask
+        ds = medium_raw_float32
+        udf = MyStdDevUDF()
+        benchmark(
+            ctx.run_udf,
+            dataset=ds,
+            udf=udf
+        )
+
+    @pytest.mark.benchmark(
+        group="std",
+    )
+    def test_std_delayed(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+        ctx = Context(executor=DelayedJobExecutor())
+        ds = medium_raw_float32
+        udf = MyStdDevUDF()
+        resources = DelayedJobExecutor.get_resources_from_udfs(udf)
+
+        def doit():
+            result = ctx.run_udf(dataset=ds, udf=udf)
+            # Make sure we run on the same number of workers
+            return result['intensity'].raw_data.compute(resources=resources)
+
+        benchmark(doit)
+
+    @pytest.mark.benchmark(
+        group="std",
+    )
+    def test_std_delayed_merge(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+        ctx = Context(executor=DelayedJobExecutor())
+        ds = medium_raw_float32
+        udf = MyStdDevMergeUDF()
+        resources = DelayedJobExecutor.get_resources_from_udfs(udf)
+
+        def doit():
+            result = ctx.run_udf(dataset=ds, udf=udf)
+            return result['intensity'].raw_data.compute(resources=resources)
+
+        benchmark(doit)
+
     @pytest.mark.benchmark(
         group="sum",
     )

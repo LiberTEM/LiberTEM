5ee97109a15e94304ce5121d46f7b526370d76ec
Dieter Weber <d.weber@fz-juelich.de>
2021-12-20 12:02:37 +0100

    Test also uint16, compare EchoUDF with `make_dask_array()`, bugfix for tiling

--- benchmarks/executor/test_delayed.py
+++ benchmarks/executor/test_delayed.py
@@ -5,6 +5,17 @@ from libertem.udf.base import UDF
 from libertem.udf.stddev import StdDevUDF
 from libertem.api import Context
 from libertem.executor.delayed import DelayedJobExecutor
+from libertem.contrib.daskadapter import make_dask_array
+
+
+@pytest.fixture(scope="module", params=["float32", "uint16"])
+def my_ds(request, medium_raw, medium_raw_float32):
+    if request.param == 'float32':
+        return medium_raw_float32
+    elif request.param == 'uint16':
+        return medium_raw
+    else:
+        raise ValueError()
 
 
 class MyStdDevUDF(StdDevUDF):
@@ -80,6 +91,10 @@ class MySumSigUDF(UDF):
             ),
         }
 
+    # Make sure we don't have default merge so that there is no default dask_merge
+    def merge(self, dest, src):
+        return super().merge(dest, src)
+
     def process_tile(self, tile):
         self.results.intensity[:] += np.sum(tile, axis=tuple(range(1, len(tile.shape))))
 
@@ -99,7 +114,12 @@ class EchoUDF(UDF):
         }
 
     def process_tile(self, tile):
-        self.results.intensity[:] = tile
+        sl = (..., *self.meta.sig_slice.get())
+        self.results.intensity[sl] = tile
+
+    # Make sure we don't have default merge so that there is nod efault dask_merge
+    def merge(self, dest, src):
+        return super().merge(dest, src)
 
 
 class EchoMergeUDF(EchoUDF):
@@ -112,27 +132,25 @@ class Test:
     @pytest.mark.benchmark(
         group="std",
     )
-    def test_std_baseline(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_std_baseline(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = shared_dist_ctx_globaldask
-        ds = medium_raw_float32
         udf = MyStdDevUDF()
         benchmark(
             ctx.run_udf,
-            dataset=ds,
+            dataset=my_ds,
             udf=udf
         )
 
     @pytest.mark.benchmark(
         group="std",
     )
-    def test_std_delayed(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_std_delayed(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = MyStdDevUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             # Make sure we run on the same number of workers
             return result['std'].raw_data.compute(resources=resources)
 
@@ -141,14 +159,13 @@ class Test:
     @pytest.mark.benchmark(
         group="std",
     )
-    def test_std_delayed_merge(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_std_delayed_merge(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = MyStdDevMergeUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['std'].raw_data.compute(resources=resources)
 
         benchmark(doit)
@@ -156,27 +173,25 @@ class Test:
     @pytest.mark.benchmark(
         group="sum",
     )
-    def test_sum_baseline(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_sum_baseline(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = shared_dist_ctx_globaldask
-        ds = medium_raw_float32
         udf = MySumUDF()
         benchmark(
             ctx.run_udf,
-            dataset=ds,
+            dataset=my_ds,
             udf=udf
         )
 
     @pytest.mark.benchmark(
         group="sum",
     )
-    def test_sum_delayed(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_sum_delayed(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = MySumUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             # Make sure we run on the same number of workers
             return result['intensity'].raw_data.compute(resources=resources)
 
@@ -185,14 +200,13 @@ class Test:
     @pytest.mark.benchmark(
         group="sum",
     )
-    def test_sum_delayed_merge(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_sum_delayed_merge(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = MySumMergeUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['intensity'].raw_data.compute(resources=resources)
 
         benchmark(doit)
@@ -200,27 +214,25 @@ class Test:
     @pytest.mark.benchmark(
         group="sumsig",
     )
-    def test_sumsig_baseline(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_sumsig_baseline(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = shared_dist_ctx_globaldask
-        ds = medium_raw_float32
         udf = MySumSigUDF()
         benchmark(
             ctx.run_udf,
-            dataset=ds,
+            dataset=my_ds,
             udf=udf
         )
 
     @pytest.mark.benchmark(
         group="sumsig",
     )
-    def test_sumsig_delayed(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_sumsig_delayed(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = MySumSigUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['intensity'].raw_data.compute(resources=resources)
 
         benchmark(doit)
@@ -228,14 +240,13 @@ class Test:
     @pytest.mark.benchmark(
         group="sumsig",
     )
-    def test_sumsig_delayed_merge(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_sumsig_delayed_merge(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = MySumSigMergeUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['intensity'].raw_data.compute(resources=resources)
 
         benchmark(doit)
@@ -243,13 +254,12 @@ class Test:
     @pytest.mark.benchmark(
         group="large",
     )
-    def test_large_baseline(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_large_baseline(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = shared_dist_ctx_globaldask
-        ds = medium_raw_float32
         udf = EchoUDF()
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['intensity'].raw_data.sum(axis=0)
 
         benchmark(doit)
@@ -257,14 +267,13 @@ class Test:
     @pytest.mark.benchmark(
         group="large",
     )
-    def test_large_delayed(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_large_delayed(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = EchoUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['intensity'].raw_data.sum(axis=0).compute(resources=resources)
 
         benchmark(doit)
@@ -272,14 +281,28 @@ class Test:
     @pytest.mark.benchmark(
         group="large",
     )
-    def test_large_delayed_merge(self, shared_dist_ctx_globaldask, medium_raw_float32, benchmark):
+    def test_large_delayed_merge(self, shared_dist_ctx_globaldask, my_ds, benchmark):
         ctx = Context(executor=DelayedJobExecutor())
-        ds = medium_raw_float32
         udf = EchoMergeUDF()
         resources = DelayedJobExecutor.get_resources_from_udfs(udf)
 
         def doit():
-            result = ctx.run_udf(dataset=ds, udf=udf)
+            result = ctx.run_udf(dataset=my_ds, udf=udf)
             return result['intensity'].raw_data.sum(axis=0).compute(resources=resources)
+
+        benchmark(doit)
+
+    @pytest.mark.benchmark(
+        group="large",
+    )
+    def test_large_dsdirect(self, shared_dist_ctx_globaldask, my_ds, benchmark):
+        resources = DelayedJobExecutor.get_resources_from_udfs(EchoUDF())
+
+        def doit():
+            # There seems to be some form of caching if the sum is calculated
+            # repeatedly on the same dask arrays
+            dask_array, workers = make_dask_array(my_ds, dtype=my_ds.dtype)
+            assert len(dask_array.shape) == 4
+            return dask_array.sum(axis=(0, 1)).compute(resources=resources)
 
         benchmark(doit)

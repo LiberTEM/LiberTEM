b49f5ebf39b3408206939e8ff72901716af0a73a
Matthew Bryan <matthewbryan52@gmail.com>
2022-06-20 10:28:25 +0200

    Add tests, doc and adjust kwarg handling

--- src/libertem/io/dataset/npy.py
+++ src/libertem/io/dataset/npy.py
@@ -2,6 +2,8 @@ import io
 import os
 import sys
 import typing
+from typing import Optional, Tuple
+import logging
 
 import numpy as np
 from numpy.lib.utils import safe_eval
@@ -14,6 +16,8 @@ from libertem.io.dataset.base import (
 )
 from libertem.common import Shape
 from libertem.common.math import prod
+
+log = logging.getLogger(__name__)
 
 
 class NPYDatasetParams(MessageConverter):
@@ -205,21 +209,65 @@ def read_npy_info(path: str) -> NPYInfo:
 
 
 class NPYDataSet(DataSet):
+    """
+    Read data stored in a numpy .npy binary file. Dataset shape
+    and dtype are inferred from the file header unless overridden
+    by the arguments to this class.
+
+    Parameters
+    ----------
+    path : str
+        The path to the .npy file
+    sig_dims : Optional[int], optional
+        The number of dimensions from the end of the full shape
+        to interpret as signal dimensions, by default 2. If None
+        will be inferred from the sig_shape argument when present.
+    nav_shape : Optional[Tuple[int, int]], optional
+        A nav_shape to apply to the dataset overriding the shape
+        value read from the .npy header, by default None. This can
+        be used to read a subset of the .npy file, or reshape the
+        contained data. Frames are read in C-order from the beginning
+        of the file.
+    sig_shape : Optional[Tuple[int, int]], optional
+        A sig_shape to apply to the dataset overriding the shape
+        value read from the .npy header, by default None. Pixels are
+        read in C-order from the beginning of the file.
+    sync_offset : int, optional
+        If positive, number of frames to skip from start
+        If negative, number of blank frames to insert at start
+    io_backend : Optional[IOBackend], optional
+        _description_, by default None
+
+    Raises
+    ------
+    DataSetException
+        If sig_dims is not an integer and cannot be inferred from sig_shape
+    DataSetException
+        If the supplied nav_shape + sig_shape describe an array larger
+        than the contents of the .npy file
+    """
     def __init__(
         self,
         path: str,
-        sig_dims=2,
-        nav_shape=None,
-        sig_shape=None,
-        sync_offset=0,
-        io_backend=None,
+        sig_dims: Optional[int] = 2,
+        nav_shape: Optional[Tuple[int, int]] = None,
+        sig_shape: Optional[Tuple[int, int]] = None,
+        sync_offset: int = 0,
+        io_backend: Optional[IOBackend] = None,
     ):
         super().__init__(io_backend=io_backend)
         self._meta = None
         self._nav_shape = tuple(nav_shape) if nav_shape else nav_shape
         self._sig_shape = tuple(sig_shape) if sig_shape else sig_shape
         self._sig_dims = sig_dims
-        # self._sync_offset = sync_offset
+        if self._sig_shape is not None:
+            if self._sig_dims is None:
+                self._sig_dims = len(self._sig_shape)
+            if len(self._sig_shape) != self._sig_dims:
+                raise DataSetException(f'Mismatching sig_dims (= {self._sig_dims}) '
+                                       f'and sig_shape {self._sig_shape} arguments')
+        if self._sig_dims is None or not isinstance(self._sig_dims, int):
+            raise DataSetException('Must supply one of sig_dims or sig_shape to NPYDataSet')
         self._path = path
         self._sync_offset = sync_offset
         self._npy_info: typing.Optional[NPYInfo] = None
@@ -231,15 +279,27 @@ class NPYDataSet(DataSet):
         self._filesize = executor.run_function(self._get_filesize)
         npyinfo = executor.run_function(read_npy_info, self._path)
         self._npy_info = npyinfo
-        shape = Shape(npyinfo.shape, sig_dims=self._sig_dims)
-        self._image_count = shape.nav.size
-        self._nav_shape_product = int(prod(self._nav_shape))
+        np_shape = Shape(npyinfo.shape, sig_dims=self._sig_dims)
+        sig_shape = self._sig_shape if self._sig_shape else np_shape.sig
+        nav_shape = self._nav_shape if self._nav_shape else np_shape.nav
+        shape = Shape(tuple(nav_shape) + tuple(sig_shape), sig_dims=self._sig_dims)
+        if shape.size > np_shape.size:
+            raise DataSetException('Shape parameters incompatible with .npy contents')
+        elif shape.size < np_shape.size:
+            log.info(f'Contents of {self._path} not fully accessed by read shape, '
+                     f'{shape} read vs. {np_shape} in file.')
+        # Trying to follow implementation of RawFileDataSet i.e. the _image_count
+        # is the whole block of data interpreted as N frames of sig_shape, noting that
+        # here sig_shape can be either user-supplied or from the npy metadata
+        # if prod(sig_shape) is not a factor then bytes will be dropped at the end
+        self._image_count = np_shape.size // prod(sig_shape)
+        self._nav_shape_product = shape.nav.size
         self._sync_offset_info = self.get_sync_offset_info()
         self._meta = DataSetMeta(
             shape=shape,
             raw_dtype=np.dtype(npyinfo.dtype),
             sync_offset=self._sync_offset or 0,
-            image_count=shape.nav.size,
+            image_count=self._image_count,
         )
         return self
 
---
+++ tests/io/datasets/test_npy.py
@@ -0,0 +1,509 @@
+import os
+import sys
+import json
+import pickle
+
+import numpy as np
+import pytest
+
+import libertem.api as lt
+
+from libertem.analysis.raw import PickFrameAnalysis
+from libertem.io.dataset.npy import NPYDatasetParams, NPYDataSet, read_npy_info
+from libertem.io.dataset.base import (
+    TilingScheme, BufferedBackend, MMapBackend, DirectBackend, DataSetException
+)
+from libertem.common import Shape
+from libertem.common.math import prod
+from libertem.common.buffers import reshaped_view
+from libertem.udf.sumsigudf import SumSigUDF
+
+from utils import dataset_correction_verification, ValidationUDF, _mk_random
+
+
+@pytest.fixture(scope='session')
+def npy_datadir(tmpdir_factory):
+    yield tmpdir_factory.mktemp('data_npy')
+
+
+@pytest.fixture(scope='session')
+def default_npy_filepath(npy_datadir):
+    yield str(npy_datadir + '/test_default.npy')
+
+
+@pytest.fixture()
+def npy_random_array(npy_datadir):
+    random_filename = str(npy_datadir + f'/array{np.random.randint(0, 1000)}.npy')
+    ndim = np.random.randint(1, 6)
+    shape = tuple(np.random.randint(1, 10) for _ in range(ndim))
+    dtype = np.random.choice([np.float32, np.uint8, np.int64, np.complex128])
+    array = np.empty(shape, dtype=dtype)
+    np.save(random_filename, array)
+    return random_filename, array
+
+
+@pytest.fixture()
+def npy_fortran_array(npy_datadir):
+    random_filename = str(npy_datadir + f'/array{np.random.randint(0, 1000)}.npy')
+    array = np.ones((55, 55), order='F')
+    np.save(random_filename, array)
+    return random_filename, array
+
+
+@pytest.fixture(scope='session')
+def npy_8x8x8x8_path(npy_datadir):
+    filename = npy_datadir + '/8x8x8x8.npy'
+    data = _mk_random(size=(8, 8, 8, 8), dtype='float32')
+    np.save(str(filename), data)
+    del data
+    yield str(filename)
+
+
+@pytest.fixture(scope='session')
+def npy_8x8x8x8_ds(npy_8x8x8x8_path):
+    lt_ctx = lt.Context.make_with('inline')
+    ds = lt_ctx.load(
+            'npy',
+            path=npy_8x8x8x8_path,
+        )
+    ds.set_num_cores(2)
+    yield ds
+
+
+@pytest.fixture(scope='session')
+def default_npy(default_npy_filepath, default_raw_data):
+    lt_ctx = lt.Context.make_with('inline')
+    filename = default_npy_filepath
+    np.save(filename, default_raw_data)
+    ds = lt_ctx.load(
+        "npy",
+        path=filename,
+        sig_dims=2,
+        io_backend=MMapBackend(),
+    )
+    ds.set_num_cores(2)
+    del default_raw_data
+    yield ds
+
+
+@pytest.mark.parametrize(
+    "repeat", [*range(50)]
+)
+def test_read_npy_info(npy_random_array, repeat):
+    npy_filepath, array = npy_random_array
+
+    info = read_npy_info(npy_filepath)
+    assert info.dtype == array.dtype
+    assert info.shape == array.shape
+    assert info.count == array.size
+    # compute the header offset from the other direction !
+    offset = os.stat(npy_filepath).st_size - array.nbytes
+    assert info.offset == offset
+
+
+def test_read_npy_info_fortran(npy_fortran_array):
+    npy_filepath, array = npy_fortran_array
+    assert array.flags.f_contiguous
+    with pytest.raises(AssertionError):
+        read_npy_info(npy_filepath)
+
+
+def test_repr(default_npy):
+    assert 'NPYDataSet' in repr(default_npy)
+
+
+def test_get_fileset(default_npy):
+    fileset = default_npy._get_fileset()
+    assert len(fileset) == 1
+    assert fileset[0]._path == default_npy._path
+
+
+def test_detect_params(default_npy, default_raw_data, lt_ctx):
+    params = NPYDataSet.detect_params(default_npy._path, lt_ctx.executor)
+    assert (params['parameters']['nav_shape']
+        + params['parameters']['sig_shape'])\
+        == default_raw_data.shape
+
+
+def test_detect_params_fail(lt_ctx):
+    params = NPYDataSet.detect_params(555, lt_ctx.executor)
+    assert not params
+
+
+def test_apply_mask_analysis(default_npy, lt_ctx):
+    mask = np.ones((128, 128))
+    analysis = lt_ctx.create_mask_analysis(factories=[lambda: mask], dataset=default_npy)
+    results = lt_ctx.run(analysis)
+    assert results[0].raw_data.shape == (16, 16)
+
+
+def test_sum_analysis(default_npy, lt_ctx):
+    analysis = lt_ctx.create_sum_analysis(dataset=default_npy)
+    results = lt_ctx.run(analysis)
+    assert results[0].raw_data.shape == (128, 128)
+
+
+def test_pick_analysis(default_npy, lt_ctx):
+    analysis = PickFrameAnalysis(dataset=default_npy, parameters={"x": 15, "y": 15})
+    results = lt_ctx.run(analysis)
+    assert results[0].raw_data.shape == (128, 128)
+    assert np.count_nonzero(results[0].raw_data) > 0
+
+
+def test_comparison(default_npy, default_raw_data, lt_ctx_fast):
+    udf = ValidationUDF(
+        reference=reshaped_view(default_raw_data, (-1, *tuple(default_npy.shape.sig)))
+    )
+    lt_ctx_fast.run_udf(udf=udf, dataset=default_npy)
+
+
+def test_comparison_roi(default_npy, default_raw_data, lt_ctx_fast):
+    roi = np.random.choice(
+        [True, False],
+        size=tuple(default_npy.shape.nav),
+        p=[0.5, 0.5]
+    )
+    udf = ValidationUDF(reference=default_raw_data[roi])
+    lt_ctx_fast.run_udf(udf=udf, dataset=default_npy, roi=roi)
+
+
+def test_pickle_is_small(default_npy):
+    pickled = pickle.dumps(default_npy)
+    pickle.loads(pickled)
+
+    # let's keep the pickled dataset size small-ish:
+    assert len(pickled) < 2 * 1024
+
+
+def test_cache_key_json_serializable(default_npy):
+    json.dumps(default_npy.get_cache_key())
+
+
+def test_message_converter_direct():
+    src = {
+        "type": "NPY",
+        "path": "p",
+        "nav_shape": [16, 16],
+        "sig_shape": [8, 8],
+    }
+    converted = NPYDatasetParams().convert_to_python(src)
+    assert converted == {
+        "path": "p",
+        "nav_shape": (16, 16),
+        "sig_shape": (8, 8),
+    }
+
+
+def test_bad_sig_dims(lt_ctx):
+    with pytest.raises(DataSetException):
+        lt_ctx.load(
+            "npy",
+            path='anything.npy',
+            sig_shape=(1, 2, 3),
+            sig_dims=1,
+        )
+
+
+def test_bad_sig_dims2(lt_ctx):
+    with pytest.raises(DataSetException):
+        lt_ctx.load(
+            "npy",
+            path='anything.npy',
+            sig_dims=None,
+        )
+
+
+def test_auto_sig_dims(lt_ctx):
+    ds = NPYDataSet(path='anything.npy',
+                    sig_shape=(1, 2, 3),
+                    sig_dims=None)
+    assert ds._sig_dims == 3
+
+
+def test_shape_override_toobig(default_npy_filepath, default_raw_data, lt_ctx):
+    nav_shape, sig_shape = default_raw_data.shape[:2], default_raw_data.shape[2:]
+    with pytest.raises(DataSetException):
+        lt_ctx.load(
+            "npy",
+            path=default_npy_filepath,
+            sig_dims=2,
+            nav_shape=tuple(n + 1 for n in nav_shape),
+            sig_shape=tuple(n + 1 for n in sig_shape),
+        )
+
+
+def test_shape_arg_smallernav(default_npy_filepath, default_raw_data, lt_ctx):
+    nav_shape, sig_shape = default_raw_data.shape[:2], default_raw_data.shape[2:]
+    smaller_nav_shape = tuple(n - 1 for n in nav_shape)
+    ds = lt_ctx.load(
+            "npy",
+            path=default_npy_filepath,
+            sig_dims=2,
+            nav_shape=smaller_nav_shape,
+        )
+    assert tuple(ds.shape.sig) == sig_shape
+    assert tuple(ds.shape.nav) == smaller_nav_shape
+    lt_ctx: lt.Context
+    result = lt_ctx.map(ds, lambda x: True)
+    assert result.data.shape == smaller_nav_shape
+
+
+def test_shape_arg_flatnav(default_npy_filepath, default_raw_data, lt_ctx):
+    nav_shape, sig_shape = default_raw_data.shape[:2], default_raw_data.shape[2:]
+    flat_nav_shape = (prod(nav_shape),)
+    ds = lt_ctx.load(
+            "npy",
+            path=default_npy_filepath,
+            sig_dims=2,
+            nav_shape=flat_nav_shape,
+        )
+    assert tuple(ds.shape.sig) == sig_shape
+    assert tuple(ds.shape.nav) == flat_nav_shape
+    lt_ctx: lt.Context
+    result = lt_ctx.map(ds, lambda x: True)
+    assert result.data.shape == flat_nav_shape
+
+
+@pytest.mark.with_numba
+def test_roi_1(default_npy, lt_ctx):
+    p = next(default_npy.get_partitions())
+    roi = np.zeros(p.meta.shape.flatten_nav().nav, dtype=bool)
+    roi[0] = 1
+    tiles = []
+    tiling_scheme = TilingScheme.make_for_shape(
+        tileshape=Shape((1, 128, 128), sig_dims=2),
+        dataset_shape=default_npy.shape,
+    )
+    for tile in p.get_tiles(tiling_scheme=tiling_scheme, dest_dtype="float32", roi=roi):
+        print("tile:", tile)
+        tiles.append(tile)
+    assert len(tiles) == 1
+    assert tiles[0].tile_slice.origin == (0, 0, 0)
+    assert tuple(tiles[0].tile_slice.shape) == (1, 128, 128)
+
+
+@pytest.mark.with_numba
+def test_roi_2(default_npy, lt_ctx):
+    p = next(default_npy.get_partitions())
+    roi = np.zeros(p.meta.shape.flatten_nav(), dtype=bool)
+    stackheight = 4
+    tiling_scheme = TilingScheme.make_for_shape(
+        tileshape=Shape((stackheight, 128, 128), sig_dims=2),
+        dataset_shape=default_npy.shape,
+    )
+    roi[0:stackheight + 2] = 1
+    tiles = p.get_tiles(tiling_scheme=tiling_scheme, dest_dtype="float32", roi=roi)
+    tiles = list(tiles)
+
+
+@pytest.mark.parametrize(
+    "io_backend", (
+        BufferedBackend(),
+        MMapBackend(),
+    ),
+)
+def test_positive_sync_offset(lt_ctx, npy_8x8x8x8_ds, npy_8x8x8x8_path, io_backend):
+    udf = SumSigUDF()
+    sync_offset = 2
+
+    ds_with_offset = NPYDataSet(
+        path=npy_8x8x8x8_path,
+        sync_offset=sync_offset,
+        io_backend=io_backend,
+    )
+    ds_with_offset.set_num_cores(4)
+    ds_with_offset = ds_with_offset.initialize(lt_ctx.executor)
+    ds_with_offset.check_valid()
+
+    p0 = next(ds_with_offset.get_partitions())
+    assert p0._start_frame == 2
+    assert p0.slice.origin == (0, 0, 0)
+
+    tileshape = Shape(
+        (4,) + tuple(ds_with_offset.shape.sig),
+        sig_dims=ds_with_offset.shape.sig.dims
+    )
+    tiling_scheme = TilingScheme.make_for_shape(
+        tileshape=tileshape,
+        dataset_shape=ds_with_offset.shape,
+    )
+
+    t0 = next(p0.get_tiles(tiling_scheme))
+    assert tuple(t0.tile_slice.origin) == (0, 0, 0)
+
+    for p in ds_with_offset.get_partitions():
+        for t in p.get_tiles(tiling_scheme=tiling_scheme):
+            pass
+
+    assert p.slice.origin == (48, 0, 0)
+    assert p.slice.shape[0] == 16
+
+    result = lt_ctx.run_udf(dataset=npy_8x8x8x8_ds, udf=udf)
+    result = result['intensity'].raw_data[sync_offset:]
+
+    result_with_offset = lt_ctx.run_udf(dataset=ds_with_offset, udf=udf)
+    result_with_offset = result_with_offset['intensity'].raw_data[
+        :ds_with_offset._meta.image_count - sync_offset
+    ]
+
+    assert np.allclose(result, result_with_offset)
+
+
+@pytest.mark.parametrize(
+    "io_backend", (
+        BufferedBackend(),
+        MMapBackend(),
+    ),
+)
+def test_negative_sync_offset(lt_ctx, npy_8x8x8x8_ds, npy_8x8x8x8_path, io_backend):
+    udf = SumSigUDF()
+    sync_offset = -2
+
+    ds_with_offset = NPYDataSet(
+        path=npy_8x8x8x8_path,
+        sync_offset=sync_offset,
+        io_backend=io_backend,
+    )
+    ds_with_offset.set_num_cores(4)
+    ds_with_offset = ds_with_offset.initialize(lt_ctx.executor)
+    ds_with_offset.check_valid()
+
+    p0 = next(ds_with_offset.get_partitions())
+    assert p0._start_frame == -2
+    assert p0.slice.origin == (0, 0, 0)
+
+    tileshape = Shape(
+        (4,) + tuple(ds_with_offset.shape.sig),
+        sig_dims=ds_with_offset.shape.sig.dims
+    )
+    tiling_scheme = TilingScheme.make_for_shape(
+        tileshape=tileshape,
+        dataset_shape=ds_with_offset.shape,
+    )
+
+    t0 = next(p0.get_tiles(tiling_scheme))
+    assert tuple(t0.tile_slice.origin) == (2, 0, 0)
+
+    for p in ds_with_offset.get_partitions():
+        for t in p.get_tiles(tiling_scheme=tiling_scheme):
+            pass
+
+    assert p.slice.origin == (48, 0, 0)
+    assert p.slice.shape[0] == 16
+
+    result = lt_ctx.run_udf(dataset=npy_8x8x8x8_ds, udf=udf)
+    result = result['intensity'].raw_data[:npy_8x8x8x8_ds._meta.image_count - abs(sync_offset)]
+
+    result_with_offset = lt_ctx.run_udf(dataset=ds_with_offset, udf=udf)
+    result_with_offset = result_with_offset['intensity'].raw_data[abs(sync_offset):]
+
+    assert np.allclose(result, result_with_offset)
+
+
+@pytest.mark.skipif(
+    sys.platform.startswith("darwin"),
+    reason="No support for direct I/O on Mac OS X"
+)
+def test_load_direct(lt_ctx, npy_8x8x8x8_path):
+    ds_direct = lt_ctx.load(
+        "npy",
+        path=npy_8x8x8x8_path,
+        io_backend=DirectBackend(),
+    )
+    analysis = lt_ctx.create_sum_analysis(dataset=ds_direct)
+    lt_ctx.run(analysis)
+
+
+@pytest.mark.parametrize(
+    "io_backend", (
+        BufferedBackend(),
+        MMapBackend(),
+    ),
+)
+def test_reshape_nav(lt_ctx, npy_8x8x8x8_ds, npy_8x8x8x8_path, io_backend):
+    udf = SumSigUDF()
+
+    ds_with_1d_nav = lt_ctx.load(
+        "npy",
+        path=npy_8x8x8x8_path,
+        nav_shape=(64,),
+        io_backend=io_backend,
+    )
+    result_with_1d_nav = lt_ctx.run_udf(dataset=ds_with_1d_nav, udf=udf)
+    result_with_1d_nav = result_with_1d_nav['intensity'].raw_data
+
+    result_with_2d_nav = lt_ctx.run_udf(dataset=npy_8x8x8x8_ds, udf=udf)
+    result_with_2d_nav = result_with_2d_nav['intensity'].raw_data
+
+    ds_with_3d_nav = lt_ctx.load(
+        "npy",
+        path=npy_8x8x8x8_path,
+        nav_shape=(2, 4, 8),
+    )
+    result_with_3d_nav = lt_ctx.run_udf(dataset=ds_with_3d_nav, udf=udf)
+    result_with_3d_nav = result_with_3d_nav['intensity'].raw_data
+
+    assert np.allclose(result_with_1d_nav, result_with_2d_nav, result_with_3d_nav)
+
+
+def test_different_sig_shape(lt_ctx, npy_8x8x8x8_path):
+    sig_shape = (4, 4)
+
+    ds = lt_ctx.load(
+        "npy",
+        path=npy_8x8x8x8_path,
+        sig_shape=sig_shape,
+    )
+
+    assert ds._meta.image_count == 256
+
+
+@pytest.mark.parametrize(
+    "io_backend", (
+        BufferedBackend(),
+        MMapBackend(),
+    ),
+)
+def test_extra_data_at_the_end(lt_ctx, npy_8x8x8x8_path, io_backend):
+    """
+    If there is extra data at the end of the file, make sure it is cut off
+    """
+    sig_shape = (3, 3)
+
+    ds = lt_ctx.load(
+        "npy",
+        path=npy_8x8x8x8_path,
+        sig_shape=sig_shape,
+        io_backend=io_backend,
+    )
+
+    assert ds._meta.image_count == 455
+
+
+def test_offset_greater_than_image_count(lt_ctx, npy_8x8x8x8_path):
+    sync_offset = 70
+
+    with pytest.raises(Exception) as e:
+        lt_ctx.load(
+            "npy",
+            path=npy_8x8x8x8_path,
+            sync_offset=sync_offset,
+        )
+    assert e.match(
+        r"offset should be in \(-64, 64\), which is \(-image_count, image_count\)"
+    )
+
+
+@pytest.mark.parametrize(
+    "with_roi", (True, False)
+)
+def test_correction_default(default_npy, lt_ctx, with_roi):
+    ds = default_npy
+
+    if with_roi:
+        roi = np.zeros(ds.shape.nav, dtype=bool)
+        roi[:1] = True
+    else:
+        roi = None
+
+    dataset_correction_verification(ds=ds, roi=roi, lt_ctx=lt_ctx)

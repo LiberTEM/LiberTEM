21e310fe3f2bbda66929c8d1a6be6531b9721b07
Dieter Weber <d.weber@fz-juelich.de>
2022-08-15 20:49:15 +0200

    Use sparse input data when sparse; test sum UDF with sparse

--- tests/udf/test_sum.py
+++ tests/udf/test_sum.py
@@ -1,14 +1,22 @@
 import numpy as np
+import sparse
+import pytest
 
 from libertem.udf.sum import SumUDF
-
 from libertem.io.dataset.memory import MemoryDataSet
+from libertem.common.sparse import as_format, NUMPY
 
 from utils import _mk_random
 
 
-def test_sum(lt_ctx, delayed_ctx):
-    data = _mk_random((16, 8, 32, 64))
+@pytest.mark.parametrize(
+    "sparse_input", [True, False]
+)
+def test_sum(lt_ctx, delayed_ctx, sparse_input):
+    if sparse_input:
+        data = sparse.random((16, 8, 32, 64))
+    else:
+        data = _mk_random((16, 8, 32, 64))
 
     dataset = MemoryDataSet(data=data, tileshape=(8, 32, 64),
                             num_partitions=2, sig_dims=2)
@@ -16,7 +24,7 @@ def test_sum(lt_ctx, delayed_ctx):
     sum_result = lt_ctx.run_udf(udf=SumUDF(), dataset=dataset)
     sum_delayed = delayed_ctx.run_udf(udf=SumUDF(), dataset=dataset)
 
-    naive_result = data.sum(axis=(0, 1))
+    naive_result = as_format(data.sum(axis=(0, 1)), NUMPY)
 
     assert np.allclose(sum_result['intensity'].data, naive_result)
     assert np.allclose(sum_delayed['intensity'].data, naive_result)

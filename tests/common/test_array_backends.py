d80f5ded1e37f70916aaa329ae09cb9d97511f1c
Dieter Weber <d.weber@fz-juelich.de>
2022-09-02 14:46:59 +0200

    Mitigation for https://github.com/cupy/cupy/issues/7035

--- tests/common/test_array_backends.py
+++ tests/common/test_array_backends.py
@@ -4,8 +4,9 @@ import numpy as np
 from libertem.utils.devices import detect
 from libertem.common.array_backends import (
     CUDA, CUPY_SCIPY_COO, CUPY_SCIPY_CSC, CUPY_SCIPY_CSR, NUMPY,
-    BACKENDS, CUDA_BACKENDS, ND_BACKENDS, for_backend, get_backend
+    BACKENDS, CUDA_BACKENDS, ND_BACKENDS, check_shape, for_backend, get_backend
 )
+from libertem.common.math import prod
 
 from utils import _mk_random
 
@@ -27,7 +28,7 @@ has_cupy = d['cudas'] and d['has_cupy']
         np.float32, np.float64, np.complex64, np.complex128,
     ]
 )
-def test_as_format(left, right, dtype):
+def test_for_backend(left, right, dtype):
     CUPY_SPARSE_DTYPES = {
         np.float32, np.float64, np.complex64, np.complex128
     }
@@ -39,17 +40,15 @@ def test_as_format(left, right, dtype):
         pytest.skip("No CUDA device or no CuPy, skipping CuPy test")
     if left in CUPY_SPARSE_FORMATS and dtype not in CUPY_SPARSE_DTYPES:
         pytest.skip(f"Dtype {dtype} not supported for left format {left}")
-    if left in ND_BACKENDS:
-        shape = (7, 11, 13, 17)
-    else:
-        shape = (7, 11)
-    data = _mk_random(shape, dtype=dtype, array_backend=left)
+    shape = (7, 11, 13, 17)
+    left_ref = _mk_random(shape, dtype=dtype, array_backend=NUMPY)
+    assert isinstance(left_ref, np.ndarray)
+    data = for_backend(left_ref, left)
     if left == CUDA:
         assert get_backend(data) == NUMPY
     else:
         assert get_backend(data) == left
-    left_ref = for_backend(data, NUMPY)
-    assert isinstance(left_ref, np.ndarray)
+
     converted = for_backend(data, right)
     if right == CUDA:
         assert get_backend(converted) == NUMPY
@@ -59,7 +58,12 @@ def test_as_format(left, right, dtype):
     converted_back = for_backend(converted, NUMPY)
     assert isinstance(converted_back, np.ndarray)
 
-    if right not in ND_BACKENDS:
-        left_ref = left_ref.reshape((left_ref.shape[0], -1))
+    if left in ND_BACKENDS and right in ND_BACKENDS:
+        target_shape = shape
+    else:
+        target_shape = (shape[0], prod(shape[1:]))
 
-    assert np.allclose(left_ref, converted_back)
+    check_shape(converted)
+    assert converted_back.shape == target_shape
+
+    assert np.allclose(left_ref.reshape(target_shape), converted_back)

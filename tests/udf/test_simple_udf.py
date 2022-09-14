0a287ea4d8e747360b4f2e30c7f255f78af4b370
Matthew Bryan <matthewbryan52@gmail.com>
2022-09-14 14:35:10 +0200

    remove extra assert

--- tests/udf/test_simple_udf.py
+++ tests/udf/test_simple_udf.py
@@ -27,7 +27,6 @@ def test_validation(lt_ctx):
     udf = ValidationUDF(reference=data.reshape((-1, 16, 16)))
     res = lt_ctx.run_udf(dataset=dataset, udf=udf)
     assert res['seen'].data.shape == (16, 16)
-    assert np.sum(res['seen'].data) == np.prod((16, 16))
 
     with pytest.raises(AssertionError):
         data2 = data.copy()

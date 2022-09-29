d74e754abc4c44f30d538a0ebdff05778f6a165f
Matthew Bryan <matthewbryan52@gmail.com>
2022-09-29 14:50:27 +0200

    Add more comments/docstrings

--- src/libertem/io/dataset/base/backend_fortran.py
+++ src/libertem/io/dataset/base/backend_fortran.py
@@ -1,6 +1,7 @@
 import os
 import operator
 import typing
+from typing import Tuple, Union, Generator, Iterable, List, Set, Dict, Any
 import concurrent.futures
 import itertools
 import functools
@@ -22,6 +23,11 @@ class FortranReader:
     Splits the file into chunks spanning flat_sig
     in a way which corresponds to the desired tiling
     scheme and parallellises the reads to improve performance
+
+    The parameter sig_order determines if the data read should
+    be reshaped from flat_sig using 'C' or 'F' ordering. In the case
+    of DM4 files the data is (flat_sig, flat_nav) but the signal and nav
+    dimensions were individually unrolled using C-order
     """
     MIN_MEMMAP_SIZE = 512 * 2 ** 20  # 512 MB
     MAX_NUM_MEMMAP: int = 16
@@ -153,7 +159,7 @@ class FortranReader:
         self._scheme_map_combination = {}
         combination_tiles = np.flatnonzero(chunks_per_tile > 1)
         for tile_idx in combination_tiles:
-            chunks_for_tile = tuple(np.flatnonzero(chunk_tile_map[:, tile_idx]))
+            chunks_for_tile: Tuple[int] = tuple(np.flatnonzero(chunk_tile_map[:, tile_idx]))
             try:
                 self._scheme_map_combination[chunks_for_tile].append(tile_idx)
             except KeyError:
@@ -162,6 +168,7 @@ class FortranReader:
                                         in self._scheme_map_combination.items()}
         self._scheme_map = {**{ci: tis for ci, tis in enumerate(_scheme_map_independent)},
                             **self._scheme_map_combination}
+        self._scheme_map: Dict[Union[int, Tuple[int]], Set[int]]
 
     def create_memmaps(self):
         # Always memmap in C-order (slice_of_flat_sig), whole_flat_nav)
@@ -177,16 +184,24 @@ class FortranReader:
                                            offset=offset,
                                            shape=chunkshape))
 
-    def _byte_size(self, shape):
+    def _byte_size(self, shape: Iterable) -> int:
         return np.dtype(self._dtype).itemsize * np.prod(shape, dtype=np.int64)
 
     def reset_memmaps(self):
-        # This is necessary to call occasionally to prevent memory issues
+        # This may be necessary to call occasionally to prevent memory issues
         self._memmaps.clear()
         self.create_memmaps()
 
     @staticmethod
-    def _load_data(memmap, slices, out_buf, idx):
+    def _load_data(memmap: np.ndarray,
+                   slices: Tuple[Iterable, slice],
+                   out_buf: np.ndarray,
+                   idx: int) -> int:
+        """
+        Read slices/values from memmap into out_buf sequentially in the last dimension
+
+        Return idx to allow tracking which memmap chunk(s) just completed
+        """
         slice_start = 0
         for sl in slices:
             # sl can be a slice or index array
@@ -198,11 +213,11 @@ class FortranReader:
             slice_start += length
         return idx
 
-    def generate_tiles(self, *index_or_slice)\
-            -> typing.Generator[tuple[tuple[int], int, np.ndarray], None, None]:
+    def generate_tiles(self, *index_or_slice: Union[int, slice, Iterable])\
+            -> Generator[tuple[tuple[int], int, np.ndarray], None, None]:
         """
         Yields tiles from dataset according to tiling_scheme (potentially unordered)
-        *index_or_slice can be any/mix of [int, slice, iterable]
+        *index_or_slice can be any/mix of [int, slice, iterable] (strictly ordered)
         of flat_nav, full (no-ROI) frame indices to yield to the caller (the partition)
 
         The backend optimises the reads to override the tiling_scheme depth
@@ -237,7 +252,7 @@ class FortranReader:
                 tiles_completed = set()
                 for complete in concurrent.futures.as_completed(raw_futures + combined_futures):
                     chunks_completed = complete.result()
-                    tiles_ready: set = self._scheme_map[chunks_completed]
+                    tiles_ready = self._scheme_map[chunks_completed]
                     tiles_to_yield = tiles_ready.difference(tiles_completed)
                     if not tiles_to_yield:
                         continue
@@ -249,32 +264,59 @@ class FortranReader:
                                                    buffer_nav_slice]
                             tile_shape = self._tile_shapes[scheme_index] + flat_tile.shape[-1:]
                             tile = flat_tile.reshape(tile_shape, order=self._sig_order)
+                            # must roll final axis to provide shape == (nav, *sig)
                             tile = np.moveaxis(tile, -1, 0)
                             yield idcs_in_flat_nav, scheme_index, tile
 
-    def _combine_raw_futures(self, futures, pool):
+    def _combine_raw_futures(self,
+                             futures: Iterable[concurrent.futures.Future],
+                             pool: concurrent.futures.Executor) -> List[concurrent.futures.Future]:
+        """
+        Combines futures according to self._scheme_map_combination,
+        which was created during __init__, to allow us to wait for multiple
+        chunks to complete before yielding tiles which depend on them
+        """
         return [pool.submit(self._combine, *tuple(futures[i] for i in combination))
                 for combination in self._scheme_map_combination.keys()]
 
     @staticmethod
-    def _combine(*futures):
+    def _combine(*futures: concurrent.futures.Future) -> tuple[int]:
+        """
+        Wait on all provided futures to complete before returning
+        """
         return tuple(f.result() for f in futures)
 
     @classmethod
-    def _plan_reads(cls, ts_depth, *index_or_slice):
+    def _plan_reads(cls, ts_depth: int, *index_or_slice: Union[int, slice]) -> Tuple[int, List]:
         """
-        Performs a similar function to get_read_ranges but works on slices instead
+        For a given scheme depth and frame indices to read, try to find a good
+        sequence of reads to perform which minimises the number of passes through the file
+
+        Constraint: must yield tiles/frames with ts_depth at the end to be consistent
+
+        Steps are:
+         - Compute a good buffer_length which is multiple of ts_depth
+         - Split big slices into chunks <= buffer_length
+         - Combine slices sequentially up to buffer_length
+
+        Each time we fill the buffer should ideally represent one pass through the
+        the file, though in practice it depends on how the low-level file access is
+        coded, especially when using an ROI and the reads for a single buffer fill
+        are sparse/separated
+
+        This function performs a similar role to get_read_ranges as it also specifies
+        how to unpack the filled buffers into the data to emit as tiles
         """
-        # Must yield tiles/frames with ts_depth at the end to be consistent
-        # Combine any sequential indices or slices into contiguous slices
+        # First combine any sequential indices or slices into contiguous slices
         # will cast any int in index_or_slice to slice(int, int + 1) (or combine it)
         index_or_slice = cls._combine_sequential(*index_or_slice)
         # Compute number of frames to read
         slice_lengths = tuple(cls._length_slice(s) for s in index_or_slice)
         to_read = sum(slice_lengths)
 
-        if to_read == ts_depth:
-            # Best to do in a single pass
+        # Compute the size of the buffer (num_frames) we will read into
+        if to_read <= ts_depth:
+            # In this case we can complete in a single pass
             # This should cover pick_frame, i.e. no wasted buffer/reading
             # Should probably also cover process_partition
             # NOTE check how ROI + process_partition + depth works
@@ -294,18 +336,13 @@ class FortranReader:
                                           tuple(cls._split_slice(sl, buffer_length)
                                                 for sl in index_or_slice))
 
-        # Collect slice info in one pass so we can do recombination
+        # Pre-compute the slice lengths
         slice_info = []
         for element in index_or_slice:
             read_length = (element.stop - element.start)
             first_idx = element.start
             last_idx = element.stop - 1
-            try:
-                # distance should always be > 1 as adjacent slices are combined
-                distance = slice_info[-1][-1] - last_idx
-            except IndexError:
-                distance = 0
-            slice_info.append((read_length, distance, first_idx, last_idx))
+            slice_info.append((read_length, first_idx, last_idx))
 
         # During slice combination we're constrained that even in the ROI case
         # we need to emit frames in the right order, as the tile_slice attribute
@@ -318,25 +355,19 @@ class FortranReader:
         # and indices matching the roi into the appropriately sized buffer
         # A further optimisation is to read contiguous blocks even if one or two
         # frames in the block are roi == false, and just discard this data after
+
+        # Build combinations of sequential slices until we exceed buffer_length
+        # Working set is in _combining, completed sets are in combinations
         idx = 0
         combinations = []
         _combining = []
         num_slices = len(slice_info)
         while idx < num_slices:
-            read_length, distance, first_idx, last_idx = slice_info[idx]
+            read_length, first_idx, last_idx = slice_info[idx]
             # start of loop or new potential combination
             # optimistically add it and move on
             if not _combining:
                 _combining.append(idx)
-                idx += 1
-                continue
-            # Found a full-length read, push current working
-            # combination onto stack and then push this one
-            # read_length should never be > buffer_length
-            if read_length == buffer_length:
-                combinations.append(tuple(_combining))
-                _combining = []
-                combinations.append((idx,))
                 idx += 1
                 continue
             # current length of the data from _combining
@@ -351,21 +382,23 @@ class FortranReader:
             _combining.append(idx)
             idx += 1
 
+        # Handle any hanging combination
         if _combining:
             combinations.append(tuple(_combining))
         _combining.clear()
 
+        # Now apply the combinations and convert them into groups of slice objects
+        # which specify how to pack into and upack from the read buffer
         reads = []
-        # returns reads:
-        #     list of (tuple(slice,...)_for_memmap), slice_for_buffer_alloc, list(unpacks))
-        #     unpacks == (slice_in_buffer, tuple(int, ...)_flat_frame_idcs)
-        #     union(slice_in_buffer) does not necessarily fully cover
-        #     all read data, to allow ROI gaps with contig reads
+        # The return value reads has the following structure
+        #     list of (tuple(slice,...) for_memmap), slice_for_buffer_alloc, list(unpacks))
+        # where:
+        #     unpacks == (slice_in_buffer, tuple(int, ...) of flat_frame_idcs)
         for combination in combinations:
             mmap_slices = tuple(index_or_slice[i] for i in combination)
             # If we are just handling a single contig combination can optimise here by not
             # returning a frame index array but rather just a single nav slice object
-            frame_idcs = itertools.chain(*tuple(range(slice_info[i][2], slice_info[i][3] + 1)
+            frame_idcs = itertools.chain(*tuple(range(slice_info[i][1], slice_info[i][2] + 1)
                                                 for i in combination))
             frame_idcs = tuple(frame_idcs)
             read_length = sum(slice_info[i][0] for i in combination)
@@ -380,7 +413,11 @@ class FortranReader:
         return buffer_length, reads
 
     @staticmethod
-    def _gen_slices_for_depth(length, depth):
+    def _gen_slices_for_depth(length: int, depth: int) -> Generator[tuple[int, int], None, None]:
+        """
+        Generate (lower, upper) index integers which split length
+        into chunks of size depth, including a final chunk <= depth
+        """
         assert length
         boundaries = [*range(0, length, depth)]
         if boundaries[-1] != length:
@@ -388,14 +425,22 @@ class FortranReader:
         yield from zip(boundaries[:-1], boundaries[1:])
 
     @staticmethod
-    def _length_slice(sl_or_int):
+    def _length_slice(sl_or_int: Union[slice, int]) -> int:
+        """
+        Get the length of a slice object. Returns 1 for integers
+        on the assumption that they represent slice(i, i + 1)
+        """
         try:
             return sl_or_int.stop - sl_or_int.start
         except AttributeError:
             return 1
 
     @classmethod
-    def _split_slice(cls, sl, target_length):
+    def _split_slice(cls, sl: slice, target_length: int) -> Tuple[slice]:
+        """
+        Split a slice into tuple[subslices] of maximum length target_length
+        The final slice is <= target_length and the whole slice is covered
+        """
         sl_length = cls._length_slice(sl)
         if sl_length <= target_length:
             return (sl,)
@@ -405,7 +450,14 @@ class FortranReader:
                      in cls._gen_slices_for_depth(sl_length, target_length))
 
     @staticmethod
-    def _splat_iterables(*index_or_slice):
+    def _splat_iterables(*index_or_slice: Union[Iterable, Any]):
+        """
+        Any elements of index_or_slice which are iterable
+        are inserted elementwise into the returned list,
+        along with any non-iterable elements,
+        preserving the order of items in index_or_slice
+        e.g. (0, (1, 2, 3), 4) => (0, 1, 2, 3, 4)
+        """
         slices = []
         for sl in index_or_slice:
             try:
@@ -415,7 +467,13 @@ class FortranReader:
         return slices
 
     @staticmethod
-    def _combine_sequential(*index_or_slice):
+    def _combine_sequential(*index_or_slice: Union[slice, int]) -> Tuple[slice]:
+        """
+        Combines an iterable of slice or integer indices
+        into an iterable of slices. Any items in index_or_slice
+        which are sequential (slice0.stop == slice1.start) or (slice0.stop == int1)
+        are combined into a new slice slice(slice0.start, slice2.stop)
+        """
         slices = []
         for sl in index_or_slice:
             if isinstance(sl, (int, np.integer)):
@@ -430,7 +488,17 @@ class FortranReader:
         return tuple(slices)
 
     @staticmethod
-    def _slice_combine_array(*slices, threshold_combine: int = 8):
+    def _slice_combine_array(*slices: slice,
+                             threshold_combine: int = 8) -> List[Union[slice, List[int]]]:
+        """
+        Iterate over slices to find any which are shorter than threshold_combine
+        and if so convert them into an integer array rather than a slice.
+        If any slices are adjacent and both shorter than threshold_combine
+        then the integer arrays are subsequently combined
+
+        Used to combine read operations for sparsely filled ROIs where we'd
+        like to read the data for many ROI points in one pass
+        """
         _slices = []
         for sl in slices:
             if (sl.stop - sl.start) <= threshold_combine:

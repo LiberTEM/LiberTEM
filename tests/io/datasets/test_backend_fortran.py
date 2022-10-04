dbcba585e4cd7ffa5d41a614281f189c96cfb305
Matthew Bryan <matthewbryan52@gmail.com>
2022-10-04 16:40:17 +0200

    Add tests to _plan_reads

--- tests/io/datasets/test_backend_fortran.py
+++ tests/io/datasets/test_backend_fortran.py
@@ -1,4 +1,5 @@
 import itertools
+import numpy as np
 
 import pytest
 
@@ -99,3 +100,84 @@ def test_combine_sequential(seq, result):
 def test_slice_combine_array(seq, result):
     out = tuple(FortranReader._slice_combine_array(*seq))
     assert out == result
+
+
+@pytest.mark.parametrize(
+    "ideal_depth, ts_depth, sl",
+    [
+        (1, 3, slice(5, 100)),
+        (16, 1, slice(5, 6)),
+        (16, 8, slice(5, 6)),
+    ],
+)
+def test_plan_reads_buffer_length(ideal_depth, ts_depth, sl):
+    buffer_length, _ = FortranReader._plan_reads(ideal_depth,
+                                                 ts_depth,
+                                                 sl)
+    if (sl.stop - sl.start) <= ts_depth:
+        assert buffer_length == (sl.stop - sl.start)
+    else:
+        assert buffer_length >= ts_depth
+        assert (buffer_length % ts_depth) == 0
+
+
+def _generate_random_slices():
+    start = np.random.randint(0, 5)
+    length = np.random.choice([1, 3, 37, 54, 8, 113, 64, 511])
+    end = start + length
+    contig = np.random.choice([False, True])
+    if contig:
+        yield slice(start, end)
+        return
+    _upto = start
+    while _upto < end:
+        yield_length = np.random.choice([1,
+                                         np.random.randint(2, max(3, end - _upto))],
+                                        p=(0.4, 0.6))
+        if yield_length == 1:
+            yield _upto
+        else:
+            yield slice(_upto, min(_upto + yield_length, end))
+        _upto += yield_length
+        _upto += np.random.choice([0,
+                                   np.random.randint(2, max(3, end - _upto))],
+                                  p=(0.1, 0.9))
+
+
+def _expand_slices(*slices):
+    indices = tuple(i if isinstance(i, (np.integer, int))
+                    else range(i.start, i.stop)
+                    for i in slices)
+    return tuple(FortranReader._splat_iterables(*indices))
+
+
+@pytest.mark.parametrize('repeat', tuple(range(30)))
+def test_plan_reads(repeat):
+    ideal_depth = np.random.choice([1, 3, 8, 64])
+    ts_depth = np.random.choice([1, 3, 8, 16, 128])
+    slices = tuple(_generate_random_slices())
+
+    buffer_length, reads = FortranReader._plan_reads(ideal_depth,
+                                                     ts_depth,
+                                                     *slices)
+
+    # The return value reads has the following structure
+    #     list of (tuple(slice,...) for_memmap), slice_for_buffer_alloc, list(unpacks))
+    # where:
+    #     unpacks == (slice_in_buffer, tuple(int, ...) of flat_frame_idcs)
+
+    # verify sum of all reads covers all idcs from input slices
+    assert _expand_slices(*itertools.chain(*tuple(r[0] for r in reads))) == _expand_slices(*slices)
+    # verify each read group fits into a single buffer
+    assert all(len(_expand_slices(*r[0])) <= buffer_length for r in reads)
+    # verify the length of each read is placed into the correct slice size of output buffer
+    assert all(len(_expand_slices(*r[0])) == (r[1].stop - r[1].start) for r in reads)
+    # verify that each read group is unpacked to the right frame indices
+    assert all(_expand_slices(*r[0]) == tuple(itertools.chain(*tuple(rr[1]
+                                                                     for rr
+                                                                     in r[2])))
+               for r in reads)
+    # verify all unpacks from buffer are nonzero and ts_depth or less long
+    assert all(all(0 < (rr[0].stop - rr[0].start) <= ts_depth for rr in r[2]) for r in reads)
+    # verify each unpack unpacks to the correct number of frame indices
+    assert all((rr[0].stop - rr[0].start) == len(rr[1]) for r in reads for rr in r[2])

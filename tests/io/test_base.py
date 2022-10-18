4cb4958ef31507637bceae915b484d9988399909
Matthew Bryan <matthewbryan52@gmail.com>
2022-10-18 14:42:40 +0200

    Also test sync_offset

--- tests/io/test_base.py
+++ tests/io/test_base.py
@@ -45,15 +45,18 @@ def test_num_part_larger_than_num_frames():
     'repeat', range(30)
 )
 def test_make_slices(repeat):
+    sync_offset = np.random.randint(-5, 5)
     num_part = np.random.randint(1, 41)
     num_frames = np.random.randint(num_part, 333)
     shape = Shape((num_frames,), 0)
-    slices = tuple(Partition.make_slices(shape=shape, num_partitions=num_part))
+    slices = tuple(Partition.make_slices(shape=shape,
+                                         num_partitions=num_part,
+                                         sync_offset=sync_offset))
     assert len(slices) == num_part
     assert slices[0][0].origin[0] == 0
     assert slices[-1][0].origin[0] + slices[-1][0].shape[0] == num_frames
-    assert slices[0][1] == 0
-    assert slices[-1][-1] == num_frames
+    assert slices[0][1] == 0 + sync_offset
+    assert slices[-1][-1] == num_frames + sync_offset
     assert all(s0[-1] == s1[1] for s0, s1 in zip(slices[:-1], slices[1:]))
 
 

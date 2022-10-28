ce445403911c648ba595ffa78c6c66743a7176b0
Matthew Bryan <matthewbryan52@gmail.com>
2022-10-28 13:10:32 +0200

    Remove comment

--- src/libertem/executor/dask.py
+++ src/libertem/executor/dask.py
@@ -27,7 +27,6 @@ class DaskWorkerContext(WorkerContext):
     @property
     def dask_worker(self):
         try:
-            # Might be a problem if a task changes worker somehow ?
             return self._worker
         except AttributeError:
             self._worker = dd.get_worker()

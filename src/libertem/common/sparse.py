6e5c6281570734d10fbd4e7b4270e369fbf58755
Matthew Bryan <matthewbryan52@gmail.com>
2022-11-18 14:38:51 +0100

    Add conversion to COO from other formats

--- src/libertem/common/sparse.py
+++ src/libertem/common/sparse.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy.sparse as sp
 import sparse
+from typing import Union, TYPE_CHECKING, Optional, Tuple
+
+if TYPE_CHECKING:
+    from libertem.common.shape import Shape
 
 
 def to_dense(a):
@@ -12,15 +16,29 @@ def to_dense(a):
         return np.array(a)
 
 
-def to_sparse(a):
+def to_sparse(a, shape: Optional[Union['Shape', Tuple[int]]] = None):
     if isinstance(a, sparse.COO):
         return a
     elif isinstance(a, sparse.SparseArray):
         return sparse.COO(a)
     elif sp.issparse(a):
         return sparse.COO.from_scipy_sparse(a)
+    elif isinstance(a, (tuple, list)):
+        unique = {aa[-1] for aa in a}
+        if len(unique) != 1:
+            raise ValueError('Cannot cast iterable roi coords with '
+                             f'more than one truth value {unique}')
+        roi_val = bool(tuple(unique)[0])
+        fill_val = not roi_val
+        return sparse.COO.from_iter(a, shape=tuple(shape), fill_value=fill_val, dtype=bool)
     else:
         return sparse.COO.from_numpy(np.array(a))
+
+
+def sparse_to_coo(a, shape: Optional[Union['Shape', Tuple[int]]] = None):
+    if a is None or isinstance(a, np.ndarray):
+        return a
+    return to_sparse(a, shape=shape)
 
 
 def is_sparse(a):

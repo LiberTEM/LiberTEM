d7da3d6c0b88696704706ed473ab8cd4365847f8
Matthew Bryan <matthewbryan52@gmail.com>
2022-12-05 08:43:40 +0100

    flake8

--- prototypes/hdf5_reshape/make_data.py
+++ prototypes/hdf5_reshape/make_data.py
@@ -9,13 +9,13 @@ from libertem.udf.sumsigudf import SumSigUDF
 
 
 if __name__ == '__main__':
-    data_root = pathlib.Path('./data')
+    data_root = pathlib.Path('.')
 
     true_shape = (12, 15)
     nav_prod = np.prod(true_shape)
     sig_shape = (6, 9)
     dtype = np.float32
-    
+
     # row = np.arange(true_shape[1], dtype=dtype)[:, np.newaxis, np.newaxis]
     # row = np.repeat(row, sig_shape[-1], axis=-1)
     # row = np.repeat(row, sig_shape[-2], axis=-2)
@@ -26,23 +26,23 @@ if __name__ == '__main__':
     filepath = data_root / 'data.hdf5'
     ds_path = 'data'
     roi = np.random.choice([True, False], size=true_shape).astype(bool)
-    
+
     sum_result = data.sum(axis=(-2, -1))
     if roi is not None:
         sum_result[np.logical_not(roi)] = np.nan
 
-    with h5py.File(filepath,'w') as fp:
+    with h5py.File(filepath, 'w') as fp:
         dset = fp.create_dataset(ds_path, data=flat_data, chunks=chunks)
 
     ctx = lt.Context.make_with('inline')
     udf = SumSigUDF()
     ds = ctx.load(
-            'hdf5',
-            path=filepath,
-            ds_path=ds_path,
-            nav_shape=true_shape,
-            target_size=np.dtype(dtype).itemsize * true_shape[1] * 2 * np.prod(sig_shape),
-        )
+        'hdf5',
+        path=filepath,
+        ds_path=ds_path,
+        nav_shape=true_shape,
+        target_size=np.dtype(dtype).itemsize * true_shape[1] * 2 * np.prod(sig_shape),
+    )
     res = ctx.run_udf(ds, udf=udf, roi=roi)
     result = res['intensity'].data.copy()
 

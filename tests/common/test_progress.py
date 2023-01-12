0038a92980efab9d73d5e6db868589c69d27589c
Matthew Bryan <matthewbryan52@gmail.com>
2023-01-12 09:34:32 +0100

    modify in_progress count, reduce strictness

--- tests/common/test_progress.py
+++ tests/common/test_progress.py
@@ -46,7 +46,6 @@ class FastTime:
 
     def __call__(self):
         self._now += self._increment
-        print(self._now)
         return self._now
 
 
@@ -93,29 +92,8 @@ def test_progress_inline_fasttime(lt_ctx, monkeypatch):
     assert reporter._bar.n == reporter._bar.total
 
     states = reporter._history
-    # Check we received some specific messages
     start_progress = ProgressState(0., 16, 0, 0, 2)
     assert start_progress in states
-    # part0_start_progress = ProgressState(0., 16, 0, 1, 2)
-    # NOTE Sometimes missing ? Maybe a warmup / busy problem
-    # Can only reproduce this sometimes when running the full
-    # test suite. In the end it's not critical that we don't
-    # get every message because the update methods are robust
-    # to missing messages so the bar should always complete correctly
-    # assert part0_start_progress in states
-    part0_end_progress = ProgressState(8., 16, 1, 0, 2)
-    assert part0_end_progress in states
-    part1_start_progress = ProgressState(8., 16, 1, 1, 2)
-    assert part1_start_progress in states
-    part1_end_progress = ProgressState(16., 16, 2, 0, 2)
-    assert part1_end_progress in states
-
-    # Becase every tile is 'slow' we can expect
-    # to recieve some mid-partition updates
-    part0_mid_progress = ProgressState(4., 16, 0, 1, 2)
-    assert part0_mid_progress in states
-    part1_mid_progress = ProgressState(12., 16, 1, 1, 2)
-    assert part1_mid_progress in states
 
     # Check for errant values
     assert {s.num_part_in_progress for s in states} == {0, 1}

9865affd6be6785c903d151ebd7146f3bfee866e
Matthew Bryan <matthewbryan52@gmail.com>
2023-02-09 14:57:44 +0100

    adapt test cases

--- tests/io/test_detect.py
+++ tests/io/test_detect.py
@@ -5,7 +5,7 @@ from libertem.executor.inline import InlineJobExecutor
 def test_detection_empty_hdf5(empty_hdf5):
     executor = InlineJobExecutor()
     fn = empty_hdf5.filename
-    params = detect(fn, executor=executor)["parameters"]
+    filetype, params = detect(fn, executor=executor)["parameters"]
     assert params != {}
     assert list(params.keys()) == ["path"]
 
@@ -13,10 +13,10 @@ def test_detection_empty_hdf5(empty_hdf5):
 def test_detection_nonempty_hdf5(hdf5_ds_1):
     executor = InlineJobExecutor()
     fn = hdf5_ds_1.path
-    params = detect(fn, executor=executor)
+    filetype, params = detect(fn, executor=executor)
     parameters = params["parameters"]
     assert parameters != {}
     assert parameters["ds_path"] == "data"
     assert parameters["path"] == fn
-    assert params["type"] == "hdf5"
+    assert filetype == "hdf5"
     assert list(parameters.keys()) == ["path", "ds_path", "nav_shape", "sig_shape"]

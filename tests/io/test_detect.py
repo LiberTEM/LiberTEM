144b86cd19de7c8d87ae7dd88f59569736aa732d
Matthew Bryan <matthewbryan52@gmail.com>
2023-02-09 15:25:49 +0100

    revert return change for web use

--- tests/io/test_detect.py
+++ tests/io/test_detect.py
@@ -5,9 +5,7 @@ from libertem.executor.inline import InlineJobExecutor
 def test_detection_empty_hdf5(empty_hdf5):
     executor = InlineJobExecutor()
     fn = empty_hdf5.filename
-    filetype, params = detect(fn, executor=executor)
-    assert filetype == "hdf5"
-    params = params["parameters"]
+    params = detect(fn, executor=executor)["parameters"]
     assert params != {}
     assert list(params.keys()) == ["path"]
 
@@ -15,10 +13,10 @@ def test_detection_empty_hdf5(empty_hdf5):
 def test_detection_nonempty_hdf5(hdf5_ds_1):
     executor = InlineJobExecutor()
     fn = hdf5_ds_1.path
-    filetype, params = detect(fn, executor=executor)
+    params = detect(fn, executor=executor)
     parameters = params["parameters"]
     assert parameters != {}
     assert parameters["ds_path"] == "data"
     assert parameters["path"] == fn
-    assert filetype == "hdf5"
+    assert params["type"] == "hdf5"
     assert list(parameters.keys()) == ["path", "ds_path", "nav_shape", "sig_shape"]

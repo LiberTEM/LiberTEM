e3a1a3904cf12c5516afb9795791c53b004fac84
Matthew Bryan <matthewbryan52@gmail.com>
2023-04-21 15:58:02 +0200

    Much simpler basic_example.py

--- examples/basic.py
+++ examples/basic.py
@@ -8,20 +8,17 @@ if __name__ == '__main__':
     dataset_path = './path_to_dataset.hdr'
 
     # Create a Context object to load data and run analyses
-    # Here we specify we want to use 4 CPU workers for parallel jobs
-    with lt.Context.make_with(cpus=4) as ctx:
-        # Next we define a dataset, at this time no data is loaded
-        # into memory, we only specify where the files are
-        # The key 'mib' tells LiberTEM which format to load
-        # it is possible to supply 'auto' and the Context will
-        # try to auto-detect the correct dataset format
-        ds = ctx.load('mib', path=dataset_path)
+    ctx = lt.Context()
+    # The key 'mib' tells LiberTEM which format to load
+    # it is possible to supply 'auto' and the Context will
+    # try to auto-detect the correct dataset format
+    ds = ctx.load('mib', path=dataset_path)
 
-        # Create a sum-over-disk analysis, i.e. brightfield image
-        # Values for disk centre x/y and radius in pixels
-        disk_sum_analysis = ctx.create_disk_analysis(ds, cx=32, cy=32, r=8)
-        disk_sum_result = ctx.run(disk_sum_analysis, progress=True)
+    # Create a sum-over-disk analysis, i.e. brightfield image
+    # Values for disk centre x/y and radius in pixels
+    disk_sum_analysis = ctx.create_disk_analysis(ds, cx=32, cy=32, r=8)
+    disk_sum_result = ctx.run(disk_sum_analysis, progress=True)
 
-        # Plot the resulting brightfield image
-        plt.imshow(disk_sum_result.intensity.raw_data)
-        plt.show()
+    # Plot the resulting brightfield image
+    plt.imshow(disk_sum_result.intensity.raw_data)
+    plt.show()

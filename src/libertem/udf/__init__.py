50088ee318267b78eabbe8cf6ab86d48539ca8f8
Alexander Clausen <alex@gc-web.de>
2023-06-23 10:30:00 +0200

    Export `UDFRunCancelled` from `libertem.udf`

--- src/libertem/udf/__init__.py
+++ src/libertem/udf/__init__.py
@@ -1,9 +1,11 @@
-from .base import UDF, UDFMeta, UDFData, UDFFrameMixin, UDFTileMixin, UDFPartitionMixin,\
-    UDFPostprocessMixin, UDFPreprocessMixin, check_cast
+from .base import (
+    UDF, UDFMeta, UDFData, UDFFrameMixin, UDFTileMixin, UDFPartitionMixin,
+    UDFPostprocessMixin, UDFPreprocessMixin, check_cast, UDFRunCancelled,
+)
 from .auto import AutoUDF
 
 
 __all__ = [
     'UDF', 'UDFFrameMixin', 'UDFTileMixin', 'UDFPartitionMixin', 'UDFPostprocessMixin',
-    'UDFPreprocessMixin', 'UDFMeta', 'UDFData', 'check_cast', 'AutoUDF',
+    'UDFPreprocessMixin', 'UDFMeta', 'UDFData', 'check_cast', 'AutoUDF', 'UDFRunCancelled',
 ]

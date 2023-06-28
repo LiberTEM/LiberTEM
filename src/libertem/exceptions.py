043cd79763a33696bd59423397c92b1bd8f685ae
Matthew Bryan <matthewbryan52@gmail.com>
2023-06-28 08:45:58 +0200

    Add ExecutorSpecException

--- src/libertem/exceptions.py
+++ src/libertem/exceptions.py
@@ -3,3 +3,11 @@ class UDFException(Exception):
     Raised when the UDF interface is somehow misused
     """
     pass
+
+
+class ExecutorSpecException(Exception):
+    """
+    Raised when there is an error specifying an
+    Executor or its resources / workers
+    """
+    pass

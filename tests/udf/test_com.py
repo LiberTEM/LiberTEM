de1baad8cd43222bfbf1f59f0ab6259c7fd5aa91
Matthew Bryan <matthewbryan52@gmail.com>
2023-07-04 14:48:50 +0200

    Doc and lift defaults to namedtuple

--- tests/udf/test_com.py
+++ tests/udf/test_com.py
@@ -85,35 +85,41 @@ def test_com_params(lt_ctx, repeat):
 
     cy = random.choice((None, 2.1, 17))
     cx = random.choice((None, 3.7, 23))
-    r = random.choice((None, 10.2, 19))
-    ri = random.choice((None, 3.1, 9))
-    scan_rotation = random.choice((None, 13.3, 233))
-    flip_y = random.choice((None, True, False))
+    ro = random.choice((float('inf'), 10.2, 19))
+    ri = random.choice((0., 3.1, 9))
+    if np.isinf(ro):
+        # Non-zero ri not supported by analysis API
+        ri = 0.
+    scan_rotation = random.choice((0., 13.3, 233))
+    flip_y = random.choice((True, False))
 
-    com_params = com.COMParams(
+    com_udf = com.COMUDF.with_params(
         cy=cy,
         cx=cx,
-        r=r,
+        r=ro,
         ri=ri,
         scan_rotation=scan_rotation,
         flip_y=flip_y,
     )
-    com_dict = {
-        p: getattr(com_params, p) for p in (
-            'cy', 'cx', 'scan_rotation', 'flip_y'
-        ) if getattr(com_params, p) is not None
-    }
+    com_params = com_udf.params.com_params
 
-    if com_params.r is not None:
-        com_dict['mask_radius'] = com_params.r
-    if com_params.ri is not None:
-        com_dict['mask_radius_inner'] = com_params.ri
+    com_dict = {}
+    com_dict['cy'] = com_params.cy
+    com_dict['cx'] = com_params.cx
+    com_dict['scan_rotation'] = com_params.scan_rotation
+    com_dict['flip_y'] = com_params.flip_y
+    com_dict['mask_radius'] = com_params.r
+    com_dict['mask_radius_inner'] = com_params.ri
 
-    if com_params.r is None and com_params.ri is not None:
-        return  # not supported by COMAnalysis
+    # Test the analysis default None parameters
+    if np.isinf(com_dict['mask_radius']) and random.choice((True, False)):
+        com_dict['mask_radius'] = None
+
+    if (com_dict['mask_radius'] is None
+       or (np.isclose(com_dict['mask_radius_inner'], 0.) and random.choice((True, False)))):
+        com_dict['mask_radius_inner'] = None
 
     com_result = lt_ctx.run_udf(udf=com.COMUDF(com_params), dataset=dataset)
-
     com_analysis = lt_ctx.create_com_analysis(dataset=dataset, **com_dict)
     analysis_result = lt_ctx.run(com_analysis)
 

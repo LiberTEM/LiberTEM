1e7c28b8125e9024227d3e75f635ff29f2410aa0
Dieter Weber <d.weber@fz-juelich.de>
2023-07-12 09:48:15 +0200

    Require current sparseconverter

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ setup(
         'opentelemetry-api',
         'tblib',
         'tomli',
-        'sparseconverter>=0.3.0',
+        'sparseconverter>=0.3.2',
     ],
     extras_require={
         # NumPy interfacing issue on Win 11, Python 3.10

68f3817089662d1a80dcbd05d27bdb114258cfa8
Alexander Clausen <alex@gc-web.de>
2023-07-24 19:50:17 +0200

    Update codecov uploader binary version
    This should fix inconsistencies in the coverage reports, where some jobs
    would be attributed to a merge commit instead of the HEAD of the PR.

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -3,8 +3,8 @@
 set -e
 
 # pin the version so we can compare sha
-curl -Os https://uploader.codecov.io/v0.1.17/linux/codecov
-echo 'ca88335829e3a5b589674a200fdd1dae8f2ef27775647bc3aef6677266a6fda2 codecov' | sha256sum -c
+curl -Os https://uploader.codecov.io/v0.6.1/linux/codecov
+echo '0c9b79119b0d8dbe7aaf460dc3bd7c3094ceda06e5ae32b0d11a8ff56e2cc5c5 codecov' | sha256sum -c
 
 chmod +x codecov
 ./codecov $*

28b60a91c5c4952875441cb825868200d921366c
Alexander Clausen <alex@gc-web.de>
2023-07-26 18:19:42 +0200

    Update docker build infra
    - Use github registry for now (easier auth and IIRC not as rate limited)
    - Build using the official docker github actions
    - Don't keep the requirements.txt around; generate from scratch
    - Update base image (Python 3.11)

--- packaging/docker/dist-build.sh
+++ packaging/docker/dist-build.sh
@@ -11,7 +11,7 @@ TRIAGE=libertem/libertem:triage
 
 ./update_reqs.sh
 
-docker pull python:3.9-slim
+docker pull python:3.11-slim
 docker pull $TRIAGE || true
 docker pull $DEV || true
 docker build --cache-from=$TRIAGE --cache-from=$DEV -t $TRIAGE --build-arg BUILDKIT_INLINE_CACHE=1 ../../ -f Dockerfile

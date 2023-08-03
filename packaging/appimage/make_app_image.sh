e38e48457d213556b795ab79f1357b8344cbfed1
Alexander Clausen <alex@gc-web.de>
2023-08-03 16:40:21 +0200

    Need to install `build` to use it

--- packaging/appimage/make_app_image.sh
+++ packaging/appimage/make_app_image.sh
@@ -19,7 +19,7 @@ conda create -n libertem python=3.11 -y || exit 1
 # FIXME: install specific version (for example from pypi, or continuous build, ...)
 
 # Build wheel & sdist
-( cd "$BASE_DIR" && python -m build )
+( cd "$BASE_DIR" && python -m pip install build && python -m build )
 
 pip install "$BASE_DIR"/dist/*.whl || exit 1
 

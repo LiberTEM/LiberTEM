afbe6a3e51ccf20ffb9829a53c1ca49a377d2578
Alexander Clausen <alex@gc-web.de>
2023-08-15 15:59:01 +0200

    Switch from jest to vitest

--- client/src/config/__tests__/helpers.test.ts
+++ client/src/config/__tests__/helpers.test.ts
@@ -1,8 +1,10 @@
+import { test, expect, describe } from 'vitest';
+
 import { joinPaths, makeUnique } from '../helpers';
 import { initialConfigState } from '../reducers';
 
 describe('joinPaths', () => {
-    it('properly joins paths', () => {
+    test('properly joins paths', () => {
         expect(joinPaths(initialConfigState, '/', 'home')).toBe("/home");
         expect(joinPaths(initialConfigState, '/home', 'something')).toBe("/home/something");
         expect(joinPaths(initialConfigState, '/home', '..')).toBe("/home/..");
@@ -10,7 +12,7 @@ describe('joinPaths', () => {
 });
 
 describe('makeUnique', () => {
-    it('keep the order', () => {
+    test('keep the order', () => {
         expect(makeUnique([1, 1, 2, 7, 4, 5, 1, 9, 3])).toEqual([1, 2, 7, 4, 5, 9, 3]);
     });
 });
--- client/src/config/__tests__/reducers.test.ts
+++ client/src/config/__tests__/reducers.test.ts
@@ -1,14 +1,16 @@
+import { test, expect, describe } from 'vitest';
+
 import { Actions } from "../actions";
 import { configReducer, initialConfigState } from "../reducers";
 
 const somePath = "/some/path/"
 
 describe('browser reducer', () => {
-    it('should allow to add a favorite', () => {
+    test('should allow to add a favorite', () => {
         const newState = configReducer(initialConfigState, Actions.toggleStar(somePath));
         expect(newState.starred).toEqual(["/some/path/"]);
     });
-    it('should allow to remove a favorite', () => {
+    test('should allow to remove a favorite', () => {
         const intermediateState = configReducer(initialConfigState, Actions.toggleStar(somePath));
         const newState = configReducer(intermediateState, Actions.toggleStar(somePath));
         expect(newState.starred).toEqual([]);

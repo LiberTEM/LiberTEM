47bcc43e401ed907585b249ca609a1278c17d6db
Alexander Clausen <alex@gc-web.de>
2023-08-15 18:50:08 +0200

    vite: allow to control API host/port
    Via environment variables API_HOST, API_PORT

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const API_PORT = process.env.API_PORT ? process.env.API_PORT : 9000;
+const API_HOST = process.env.API_HOST ? process.env.API_HOST : 'localhost';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api/events/': {
-        target: 'ws://localhost:9000',
+        target: `ws://${API_HOST}:${API_PORT}`,
         ws: true,
       },
       '/api': {
-        target: 'http://localhost:9000',
+        target: `http://${API_HOST}:${API_PORT}`,
       }
     }
   }

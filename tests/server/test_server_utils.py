27111568b08d0c12fad230832717317b3306f2d1
Alexander Clausen <alex@gc-web.de>
2023-08-17 14:47:58 +0200

    Testcases for `_convert_device_map`

---
+++ tests/server/test_server_utils.py
@@ -0,0 +1,7 @@
+from libertem.web.connect import _convert_device_map
+
+
+def test_convert_device_map():
+    assert _convert_device_map({0: 3, 1: 0}) == [0, 0, 0]
+    assert _convert_device_map({0: 1, 1: 1}) == [0, 1]
+    assert _convert_device_map({0: 0, 1: 1, 2: 2}) == [1, 2, 2]

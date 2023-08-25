51a325ea0f2289b6fa31a95a64861b8d2d6d4246
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-25 17:40:01 +0200

    Update ApplyShiftedMasksUDF to ApplyMasksEngine

--- src/libertem/udf/masks.py
+++ src/libertem/udf/masks.py
@@ -25,37 +25,16 @@ class ApplyMasksEngine:
             or self.masks.use_sparse
         )
 
-        self.use_torch = use_torch and (not torch_incompatible)
-
-        if self.use_torch:
-
-            def process_flat(flat_tile):
-                import torch
-                masks = self.masks.get_for_sig_slice(
-                    self.meta.sig_slice, transpose=True
-                )
-                # CuPy back-end disables torch in get_task_data
-                # FIXME use GPU torch with CuPy array?
-                result = torch.mm(
-                    torch.from_numpy(flat_tile),
-                    torch.from_numpy(masks),
-                ).numpy()
-                return result
-
-        # Required due to https://github.com/scipy/scipy/issues/13211
+        self.needs_transpose = True
+        if use_torch and (not torch_incompatible):
+            self.process_flat = self._process_flat_torch
         elif (
             self.meta.array_backend == UDF.BACKEND_NUMPY
             and self.masks.use_sparse
             and 'scipy.sparse' in self.masks.use_sparse
         ):
-
-            def process_flat(flat_tile):
-                masks = self.masks.get_for_sig_slice(
-                    self.meta.sig_slice, transpose=True
-                )
-                result = rmatmul(flat_tile, masks)
-                return result
-
+            # Due to https://github.com/scipy/scipy/issues/13211
+            self.process_flat = self._process_flat_spsp
         elif (
             self.meta.array_backend in (
                 UDF.BACKEND_SCIPY_COO,
@@ -64,37 +43,52 @@ class ApplyMasksEngine:
             ) and self.masks.use_sparse
             and 'sparse.pydata' in self.masks.use_sparse
         ):
-
-            def process_flat(flat_tile):
-                masks = self.masks.get_for_sig_slice(
-                    self.meta.sig_slice, transpose=False
-                )
-                # Make sure the sparse.pydata mask comes first
-                # to choose the right multiplication method
-                result = (masks @ flat_tile.T).T
-                return result
-
+            self.process_flat = self._process_flat_sparsepyd
+            self.needs_transpose = False
         else:
+            self.process_flat = self._process_flat_standard
 
-            def process_flat(flat_tile):
-                masks = self.masks.get_for_sig_slice(
-                    self.meta.sig_slice, transpose=True
-                )
+    def _get_masks(self):
+        return self.masks.get_for_sig_slice(
+            self.meta.sig_slice, transpose=self.needs_transpose
+        )
 
-                result = flat_tile @ masks
-                return result
+    def _process_flat_torch(self, flat_tile, masks):
+        import torch
+        if masks is None:
+            masks = self._get_masks()
+        # CuPy back-end disables torch in get_task_data
+        # FIXME use GPU torch with CuPy array?
+        return torch.mm(
+            torch.from_numpy(flat_tile, masks),
+            torch.from_numpy(masks),
+        ).numpy()
 
-        self.process_flat = process_flat
+    def _process_flat_spsp(self, flat_tile, masks):
+        if masks is None:
+            masks = self._get_masks()
+        return rmatmul(flat_tile, masks)
 
-    def process_tile(self, tile):
+    def _process_flat_sparsepyd(self, flat_tile, masks):
+        if masks is None:
+            masks = self._get_masks()
+        # Make sure the sparse.pydata mask comes first
+        # to choose the right multiplication method
+        return (masks @ flat_tile.T).T
+
+    def _process_flat_standard(self, flat_tile, masks):
+        if masks is None:
+            masks = self._get_masks()
+        return flat_tile @ masks
+
+    def process_tile(self, tile, masks: np.ndarray | None = None):
         flat_shape = (tile.shape[0], prod(tile.shape[1:]))
         # Avoid reshape since older versions of scipy.sparse don't support it
         flat_data = tile.reshape(flat_shape) if tile.shape != flat_shape else tile
+        return self.process_flat(flat_data, masks)
 
-        return self.process_flat(flat_data)
 
-
-class ApplyMasksUDF(UDF):
+class BaseMasksUDF(UDF):
     '''
     Apply masks to signals/frames in the dataset. This can not only be used to integrate
     over regions with a binary mask - the integration can be weighted by using
@@ -344,3 +338,147 @@ class ApplyMasksUDF(BaseMasksUDF):
             self.task_data.engine.process_tile(tile),
             self.results.intensity,
         )
+
+
+class ApplyShiftedMasksUDF(BaseMasksUDF):
+    '''
+    Apply masks to signals/frames in the dataset, with a shift that can be specified per-frame
+    through an aux data parameter.
+
+    Regions without overlap between masks and frame due to shift will be ignored.
+
+    This UDF can only compensate integer shifts. The residual of float shifts
+    will be returned in the nav-shaped buffer called residual.
+    Its shape will be :code:`(*nav_shape, dataset.shape.sig.dims)`.
+
+    The result will be returned in a nav-shaped buffer called intensity.
+    Its shape will be :code:`(*nav_shape, len(masks))`.
+
+    Parameters
+    ----------
+
+    mask_factories : Union[Callable[[], array_like], Iterable[Callable[[], array_like]]]
+        Function or list of functions that take no arguments and create masks. The returned
+        masks can be
+        numpy arrays, scipy.sparse or sparse https://sparse.pydata.org/ matrices. The mask
+        factories should not reference large objects because they can create significant
+        overheads when they are pickled and unpickled. Each factory function should, when
+        called, return a numpy array with the same shape as frames in the dataset
+        (so dataset.shape.sig).
+    shifts : AUXBufferWrapper
+        Shift per frame, kind nav, extra_shape (dataset.shape.sig.dims, )
+    use_torch : bool, optional
+        Use pytorch back-end if available. Default True
+    use_sparse : Union[None, False, True, 'sparse.pydata'], optional
+        Which sparse back-end to use.
+        * None (default): Use sparse matrix multiplication if all factory functions return a \
+            sparse mask, otherwise convert all masks to dense matrices and use dense matrix \
+            multiplication
+        * True: Convert all masks to sparse matrices and use default sparse back-end.
+        * False: Convert all masks to dense matrices.
+        * 'sparse.pydata': Use sparse.pydata COO matrix (default sparse)
+    mask_count : int, optional
+        Specify the number of masks if a single factory function is used so that the
+        number of masks can be determined without calling the factory function.
+    mask_dtype : numpy.dtype, optional
+        Specify the dtype of the masks so that mask dtype
+        can be determined without calling the mask factory functions. This can be used to
+        override the mask dtype in the result dtype determination. As an example, setting
+        this to np.float32 means that masks of type float64 will not switch the calculation
+        and result dtype to float64 or complex128.
+    preferred_dtype : numpy.dtype, optional
+        Let :meth:`get_preferred_input_dtype` return the specified type instead of the
+        default `float32`. This can perform the calculation with integer types if both input
+        data and mask data are compatible with this.
+    backends : Iterable containing strings "numpy" and/or "cupy", or None
+        Control which back-ends are used. Default is numpy and cupy
+
+    Examples
+    --------
+
+    >>> dataset.shape
+    (16, 16, 32, 32)
+    >>> def my_masks():
+    ...     return [np.ones((32, 32)), np.zeros((32, 32))]
+    >>> assert False   # FIXME proper example
+    >>> udf = ApplyShiftedMasksUDF(mask_factories=my_masks)
+    >>> res = ctx.run_udf(dataset=dataset, udf=udf)['intensity']
+    >>> res.data.shape
+    (16, 16, 2)
+    >>> np.allclose(res.data[..., 1], 0)  # same order as in the mask factory
+    True
+
+    Mask factories can also return all masks as a single array, stacked on the first axis:
+
+    >>> def my_masks_2():
+    ...     masks = np.zeros((2, 32, 32))
+    ...     masks[1, ...] = 1
+    ...     return masks
+    >>> assert False   # FIXME proper example
+    >>> udf = ApplyShiftedMasksUDF(mask_factories=my_masks_2)
+    >>> res_2 = ctx.run_udf(dataset=dataset, udf=udf)['intensity']
+    >>> np.allclose(res_2.data, res.data)
+    True
+
+    .. versionadded:: 0.11.0
+    '''
+    def __init__(self, mask_factories, shifts=None, use_torch=True, use_sparse=None,
+                mask_count=None, mask_dtype=None, preferred_dtype=None, backends=None):
+        if use_sparse is True:
+            use_sparse = 'sparse.pydata'
+        if isinstance(use_sparse, str) and use_sparse.startswith('scipy.sparse'):
+            raise ValueError(
+                f'Sparse backend {use_sparse} not supported for shifts, use sparse.pydata instead.'
+            )
+        super().__init__(
+            mask_factories=mask_factories,
+            shifts=shifts,
+            use_torch=use_torch,
+            use_sparse=use_sparse,
+            mask_count=mask_count,
+            mask_dtype=mask_dtype,
+            preferred_dtype=preferred_dtype,
+            backends=backends,
+        )
+
+    @property
+    def shift(self):
+        if self.params.shifts is None:
+            return ((0, ) * self.meta.dataset_shape.sig.dims, ) * 2
+        else:
+            int_shifts = np.round(self.params.shifts).astype(int)
+            return int_shifts, self.params.shifts - int_shifts
+
+    def get_result_buffers(self):
+        super_buffers = super().get_result_buffers()
+        super_buffers['residual'] = self.buffer(
+            kind='nav', extra_shape=(self.meta.dataset_shape.sig.dims, )
+        )
+        return super_buffers
+
+    def process_frame(self, frame):
+        ''
+        masks = self.task_data.engine._get_masks()
+        mask_transposed = self.task_data.engine.needs_transpose
+        shift, residual = self.shift
+        shifted_slice = self.meta.sig_slice.shift_by(shift)
+        left, right = self.meta.sig_slice.intersection_pair(shifted_slice)
+        data = left.get(frame)
+        if mask_transposed:
+            mask_slice = right.get() + (slice(None), )
+            num_masks = masks.shape[-1]
+            masks = masks.reshape((*frame.shape[::-1], masks.shape[-1]))
+            masks = masks[mask_slice].reshape((-1, num_masks))
+        else:
+            mask_slice = (slice(None), ) + right.get()
+            num_masks = masks.shape[0]
+            masks = masks.reshape((masks.shape[0], *frame.shape))
+            masks = masks[mask_slice].reshape((num_masks, -1))
+
+        # Fake tile processing to use ApplyMasksEngine
+        tile_data = data.reshape(1, *data.shape)
+        self.results.intensity[:] += self.forbuf(
+            self.task_data.engine.process_tile(tile_data, masks=masks)[0],
+            self.results.intensity,
+        )
+        self.results.residual = residual

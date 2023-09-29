61fdfceb65f372e8ae59b6f6fc71e5147455419d
Matthew Bryan <matthewbryan52@gmail.com>
2023-09-29 17:46:41 +0200

    Add function to convert transposed dm4

--- src/libertem/contrib/convert_transposed.py
+++ src/libertem/contrib/convert_transposed.py
@@ -1,7 +1,14 @@
-from typing import Tuple
+import os
+from typing import Tuple, Optional, TYPE_CHECKING
 
+import libertem.api as lt
+from libertem.io.dataset.base import DataSetException
+from libertem.io.dataset.dm_single import SingleDMDataSet
 from libertem.common import Shape
 from libertem.udf.record import RecordUDF
+
+if TYPE_CHECKING:
+    from libertem.api import Context, DataSet
 
 
 class ConvertTransposedDatasetUDF(RecordUDF):
@@ -31,3 +38,37 @@ class ConvertTransposedDatasetUDF(RecordUDF):
         self.task_data.memmap[
             :, flat_sig_origin:flat_sig_origin + n_sig_px
         ] = partition
+
+
+def _convert_transposed_ds(
+    ctx: 'Context',
+    ds: 'DataSet',
+    out_path: os.PathLike,
+    **run_kwargs,
+):
+    ctx.run_udf(
+        ds,
+        ConvertTransposedDatasetUDF(
+            out_path,
+        ),
+        **run_kwargs,
+    )
+
+
+def convert_dm4_transposed(
+    dm4_path: os.PathLike,
+    out_path: os.PathLike,
+    ctx: Optional['Context'] = None,
+    num_cpus: Optional[int] = None,
+    dataset_index: Optional[int] = None,
+    progress: bool = False,
+):
+    if ctx is not None and num_cpus is not None:
+        raise ValueError('Either supply a Context or number of cpus to use in conversion')
+    elif ctx is None:
+        ctx = lt.Context.make_with('dask', cpus=num_cpus)
+    ds_meta = SingleDMDataSet._read_metadata(dm4_path, use_ds=dataset_index)
+    if ds_meta['c_order']:
+        raise DataSetException('The DM4 data is not transposed')
+    ds = ctx.load('dm', dm4_path, force_c_order=True, dataset_index=dataset_index)
+    return _convert_transposed_ds(ctx, ds, out_path, progress=progress)

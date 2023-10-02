ba3ac47797915fc7d8b0ddc2f3384b8d00339dd2
Matthew Bryan <matthewbryan52@gmail.com>
2023-10-02 08:37:32 +0200

    Test case covers 1d sig/nav

--- tests/io/test_convert_transposed.py
+++ tests/io/test_convert_transposed.py
@@ -1,5 +1,6 @@
 import pathlib
 import numpy as np
+import pytest
 
 from libertem.udf.raw import PickUDF
 from libertem.udf.sum import SumUDF
@@ -8,27 +9,58 @@ from libertem.contrib.convert_transposed import _convert_transposed_ds
 from utils import _mk_random
 
 
-def test_functional(lt_ctx, tmpdir_factory):
+@pytest.mark.parametrize(
+    'nav_shape',
+    [
+        (6,),
+        (3, 4),
+    ]
+)
+@pytest.mark.parametrize(
+    'sig_shape',
+    [
+        (8,),
+        (7, 5),
+    ]
+)
+def test_functional(lt_ctx, nav_shape, sig_shape, tmpdir_factory):
+    nav_dims = len(nav_shape)
+    sig_dims = len(sig_shape)
+
     dir = pathlib.Path(tmpdir_factory.mktemp('convert_transposed'))
-    data = _mk_random(size=(7, 5, 3, 4), dtype='float32')
-    converted_data = np.moveaxis(data, (0, 1, 2, 3), (2, 3, 0, 1))
-    ds = lt_ctx.load('memory', data=data, num_partitions=2)
+    data = _mk_random(size=sig_shape + nav_shape, dtype='float32')
+    # Transpose to put the nav dims before the sig dims, but retaining
+    # the order of the dimensions within each group
+    # e.g. (0, 1, 2, 3) => (2, 3, 0, 1)
+    converted_data = np.moveaxis(
+        data,
+        tuple(range(data.ndim)),
+        tuple((*range(nav_dims, data.ndim), *range(nav_dims))),
+    )
+    ds = lt_ctx.load(
+        'memory',
+        data=data,
+        # sig_dims=nav_dims because nav_dims are at the end!
+        # needed so that we correctly partition the array
+        sig_dims=nav_dims,
+        num_partitions=2,
+    )
 
     convert_path = dir / 'out.npy'
     _convert_transposed_ds(lt_ctx, ds, convert_path)
 
-    ds_npy = lt_ctx.load('npy', path=convert_path)
+    # Now that we are transposed sig_dims=sig_dims
+    ds_npy = lt_ctx.load('npy', path=convert_path, sig_dims=sig_dims)
     assert ds_npy.shape.to_tuple() == converted_data.shape
 
-    for check_slice in (
-        np.s_[1, 3],
-        np.s_[2, 0],
-        np.s_[0, 1],
-    ):
+    for _ in range(3):
+        check_slice = tuple(
+            np.random.randint(0, n) for n in nav_shape
+        )
         roi = np.zeros(ds_npy.shape.nav, dtype=bool)
         roi[check_slice] = True
         frame = lt_ctx.run_udf(ds_npy, PickUDF(), roi=roi)['intensity'].data.squeeze()
         assert np.allclose(frame, converted_data[check_slice])
 
     sum_res = lt_ctx.run_udf(ds_npy, SumUDF())['intensity'].data
-    assert np.allclose(sum_res, converted_data.sum(axis=(0, 1)))
+    assert np.allclose(sum_res, converted_data.sum(axis=tuple(range(nav_dims))))

03cb106b61f1ae5163d7a2512ed5f4cc117cbad7
Matthew Bryan <matthewbryan52@gmail.com>
2023-10-25 13:20:07 +0200

    Add testcase for preloading an executor

--- tests/server/test_state.py
+++ tests/server/test_state.py
@@ -1,7 +1,9 @@
 import uuid
 import pytest
 
-from libertem.web.state import JobState, ExecutorState
+from libertem.executor.base import AsyncAdapter
+from libertem.executor.dask import DaskJobExecutor
+from libertem.web.state import JobState, ExecutorState, SharedState
 
 
 pytestmark = []
@@ -37,3 +39,29 @@ async def test_job_state_remove(async_executor):
     assert job_state.get_for_dataset_id(dataset_id) == set()
     assert job_state.get_for_analysis_id(analysis_id) == set()
     assert len(job_state.jobs) == 0
+
+
+@pytest.mark.slow
+def test_preload_executor(tmpdir_factory):
+    workdir = tmpdir_factory.mktemp('preload_workdir')
+    state = SharedState()
+    state.set_local_directory(workdir)
+    state.set_preload(())
+    state.add_executor(
+        {
+            'cpus': 2,
+            'cudas': 0,
+        },
+    )
+    assert isinstance(state.executor_state.executor, AsyncAdapter)
+    sync_exec = state.executor_state.executor.ensure_sync()
+    assert isinstance(sync_exec, DaskJobExecutor)
+    resources = sync_exec.get_resource_details()
+    assert resources[0]['cpu'] == 2
+    assert resources[0]['cuda'] == 0
+    assert resources[0]['service'] == 1
+
+    def test_fn():
+        return 42
+
+    assert 42 == sync_exec.run_function(test_fn)

d86fdf7bc4274faba9a902e7fad861037df31df5
Dieter Weber <d.weber@fz-juelich.de>
2023-11-08 11:35:35 +0100

    Update tox.ini and add Powershell script for release tests
    * Include cuda12x
    * Include Python 3.11
    * Allow to run notebook tests with CUDA (to be tested more)

---
+++ scripts/releasetests.ps1
@@ -0,0 +1,24 @@
+param (
+    [switch]$rebuild = $false,
+    [string]$testdata = "C:\Users\weber\Nextcloud3\ER-C-Data\adhoc\libertem\libertem-test-data\",
+    [string]$cuda = ""
+)
+
+if ($rebuild) {
+    $rebuildflag = "-r"
+} else {
+    $rebuildflag = ""
+}
+
+if ($cuda) {
+    $cudaflag = "-$cuda"
+} else {
+    $cudaflag = ""
+}
+
+#tox -r -e py39 -- -m "not dist" tests | tee tests.log
+tox $rebuildflag -e py37$cudaflag,py38$cudaflag,py39$cudaflag,py310$cudaflag,py311$cudaflag -- -m "not dist" tests | tee tests.log
+$Env:TESTDATA_BASE_PATH = $testdata
+#tox -r -e py39-data -- -m "not dist" | tee data-tests.log
+tox $rebuildflag -e py37-data$cudaflag,py38-data$cudaflag,py39-data$cudaflag,py310-data$cudaflag,py311-data$cudaflag -- -m "not dist" | tee data-tests.log
+tox $rebuildflag -e notebooks,notebooks$cudaflag | tee notebook-tests.log

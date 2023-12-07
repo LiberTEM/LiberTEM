04ea758b46090a83d43489e4e289b73d30e5f665
Matthew Bryan <matthewbryan52@gmail.com>
2023-12-07 08:11:29 +0100

    Convert to fixture

--- tests/io/test_many_files.py
+++ tests/io/test_many_files.py
@@ -4,25 +4,19 @@ import numpy as np
 
 from libertem.io.dataset.raw import RawFileDataSet, RawFileSet, RawFile
 
-from utils import get_testdata_path
 
-MANYRAW_TESTDATA_PATH = os.path.join(get_testdata_path(), 'many_raw')
-
-
-def _generate_many_files():
+@pytest.fixture(scope='session')
+def generate_many_files(tmpdir_factory):
     # Generates 16k 8x8 uint8 frames
     # Combined with the below 2-partition dataset and
     # the two-cpu local_cluster_ctx this is likely to trigger file
     # limits on a system where this is not set to the hard limit globally
+    datadir = tmpdir_factory.mktemp('many_raw_files')
     frame = np.arange(64).astype(np.uint8)
     nav_shape = (128, 128)
-    os.mkdir(MANYRAW_TESTDATA_PATH)
     for idx in range(np.prod(nav_shape)):
-        frame.tofile(os.path.join(MANYRAW_TESTDATA_PATH, f'f{idx:>05d}.raw'))
-
-
-HAVE_TESTDATA = os.path.exists(MANYRAW_TESTDATA_PATH)
-pytestmark = pytest.mark.skipif(not HAVE_TESTDATA, reason="need testdata")  # NOQA
+        frame.tofile(os.path.join(datadir, f'f{idx:>05d}.raw'))
+    yield datadir
 
 
 class ManyRawFileDataSetMock(RawFileDataSet):
@@ -55,14 +49,14 @@ class ManyRawFileDataSetMock(RawFileDataSet):
         return 2
 
 
-def test_many_files_read(local_cluster_ctx):
+def test_many_files_read(local_cluster_ctx, generate_many_files):
     """
     Tests if we can load 8k files per process in a Dask cluster
     on this system. Depending on the system configuration
     this test might be a no-op.
     """
     ds = ManyRawFileDataSetMock(
-        path=MANYRAW_TESTDATA_PATH,
+        path=generate_many_files,
         nav_shape=(128, 128),
         sig_shape=(8, 8),
         dtype=np.uint8,

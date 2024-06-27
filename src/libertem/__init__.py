d9f7a46516d02f494ae4be4d844a3cb9981e4590
Matthew Bryan <matthewbryan52@gmail.com>
2024-06-27 11:10:37 +0200

    Version dynamically in __init__.py

--- src/libertem/__init__.py
+++ src/libertem/__init__.py
@@ -1,5 +1,11 @@
-from .__version__ import __version__, __version_tuple__
-revision = __version_tuple__[-1]
+try:
+    from .__version__ import __version__, __version_tuple__
+    revision = __version_tuple__[-1]
+except ModuleNotFoundError:
+    __version__ = "0.0.0.dev0"
+    from .versioning import get_git_rev
+    revision = get_git_rev()
+
 
 __all__ = [
     "revision", "__version__"

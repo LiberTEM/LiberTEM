76b9beadc2074d361b6af88da13c8b1f6d8edb2c
Matthew Bryan <matthewbryan52@gmail.com>
2024-06-27 16:24:43 +0200

    Remove references to setup.py

--- src/libertem/versioning.py
+++ src/libertem/versioning.py
@@ -3,7 +3,6 @@ import subprocess
 
 
 def get_git_rev():
-    # NOTE: there is a copy of this code in setup.py!
     try:
         new_cwd = os.path.abspath(os.path.dirname(__file__))
         rev_raw = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=new_cwd)

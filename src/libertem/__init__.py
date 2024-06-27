8d2656af652123bdedcbc24f21ca7972fa63ff6b
Matthew Bryan <matthewbryan52@gmail.com>
2024-06-27 10:44:52 +0200

    Revision from __version_tuple__

--- src/libertem/__init__.py
+++ src/libertem/__init__.py
@@ -1,13 +1,5 @@
-# keep version and revision in separate files such that we can
-# generate them without any regex magic
-
-from .__version__ import __version__
-
-try:
-    from ._baked_revision import revision
-except ImportError:
-    from .versioning import get_git_rev
-    revision = get_git_rev()
+from .__version__ import __version__, __version_tuple__
+revision = __version_tuple__[-1]
 
 __all__ = [
     "revision", "__version__"

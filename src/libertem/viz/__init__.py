191ff7a52cc9a63a299f915146affa184d0ef971
Dieter Weber <d.weber@fz-juelich.de>
2024-10-23 20:27:23 +0200

    ...forgot to remove code here

--- src/libertem/viz/__init__.py
+++ src/libertem/viz/__init__.py
@@ -1,21 +1,6 @@
-from empyre.vis.colors import ColormapCubehelix, ColormapPerception, ColormapHLS, ColormapClassic
-
 from .base import visualize_simple, rgb_from_2dvector, get_plottable_2D_channels
 
 from libertem.common.viz import encode_image
 
 
-__all__ = ['ColormapCubehelix', 'ColormapPerception', 'ColormapHLS',
-           'ColormapClassic', 'cmaps', 'CMAP_CIRCULAR_DEFAULT',
-           'visualize_simple', 'encode_image', 'get_plottable_2D_channels', 'rgb_from_2dvector']
-
-
-cmaps = {'cubehelix_standard': ColormapCubehelix(),
-         'cubehelix_reverse': ColormapCubehelix(reverse=True),
-         'cubehelix_circular': ColormapCubehelix(start=1, rot=1,
-                                                 minLight=0.5, maxLight=0.5, sat=2),
-         'perception_circular': ColormapPerception(),
-         'hls_circular': ColormapHLS(),
-         'classic_circular': ColormapClassic()}
-
-CMAP_CIRCULAR_DEFAULT = cmaps['cubehelix_circular']
+__all__ = ['visualize_simple', 'encode_image', 'get_plottable_2D_channels', 'rgb_from_2dvector']

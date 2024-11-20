15ab6bf7ce1154f8a821f709a0849f585375f6db
Dieter Weber <d.weber@fz-juelich.de>
2024-11-20 12:55:40 +0100

    Importing the holography code throws an exception

---
+++ src/libertem/udf/holography.py
@@ -0,0 +1,11 @@
+raise ImportError(
+    '''
+    The holography implementation is removed. Development has moved to
+    https://github.com/LiberTEM/LiberTEM-holo. Users who depend on the previous code
+    can use an older LiberTEM version (&lt;= 0.14.1) for the time being and migrate
+    to the new holography sub-project when appropriate.
+
+    More information:
+    - https://github.com/LiberTEM/LiberTEM/pull/1689
+    '''
+)

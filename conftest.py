Fix numerical instability
Closes #880

The random data from np.random.randn is centered around 0, meaning we amplify numerical errors when we sum up.
Switching to np.random.random and setting usual error bounds fixes the issue.

Switching to float64 also fixed the instability in tests.

conftest.py
@@ -125,7 +125,7 @@ def empty_hdf5(tmpdir_factory):
 
 @pytest.fixture(scope='session')
 def hdf5_4d_data():
-    data = np.random.randn(2, 10, 26, 26).astype("float32")
+    data = np.random.random((2, 10, 26, 26)).astype("float32")
     yield data
 
 

tests/analysis/test_analysis_disk.py
@@ -1,9 +1,7 @@
-import numpy as np
+from numpy.testing import assert_allclose
 from libertem.analysis.disk import DiskMaskAnalysis
-import pytest
 
 
-@pytest.mark.xfail
 def test_compare_hdf5_result_with_raw_result(
     lt_ctx, hdf5_same_data_4d, raw_same_dataset_4d
 ):
@@ -24,4 +22,4 @@ def test_compare_hdf5_result_with_raw_result(
     result_raw = lt_ctx.run(analysis_raw)
     result_raw = result_raw['intensity'].raw_data
 
-    assert not np.any(np.abs(result_hdf5 - result_raw))
+    assert_allclose(result_hdf5, result_raw, rtol=1e-6, atol=1e-6)
3388689c98a7d7ce7271b787cdf7a20563eccabd
Dieter Weber <d.weber@fz-juelich.de>
2025-04-10 14:17:46 +0200

    Support sparse 0.16
    The previous implementation throws an error with sparse 0.16.0.
    
    Replace with custom function that works directly on the COO data structure since I didn't
    find an efficient solution for a dense-sparse product that remains sparse and supports broadcasting
    as required.
    
    Note: This only works with 2D sig dimensions. However, it seems that radial_bins() only supports 2D results anyway.
    
    Drive-by:
    
    - Update tests cases to use assert_allclose()
    - Use complex64 for masks by default. Previously, values were cast to complex128 on the way, likely by accident.

--- tests/analysis/test_analysis_radialfourier.py
+++ tests/analysis/test_analysis_radialfourier.py
@@ -1,6 +1,7 @@
 import pytest
 
 import numpy as np
+from numpy.testing import assert_allclose
 
 from libertem.io.dataset.memory import MemoryDataSet
 from libertem.utils.generate import cbed_frame
@@ -66,6 +67,14 @@ def test_sparse(ds_random, lt_ctx):
     lt_ctx.run(analysis)
 
 
+def test_sparse_multi(ds_random, lt_ctx):
+    analysis = lt_ctx.create_radial_fourier_analysis(
+        dataset=ds_random, use_sparse=True,
+        n_bins=3, max_order=7,
+    )
+    lt_ctx.run(analysis)
+
+
 def test_symmetries(lt_ctx):
     (d1, i1, p1) = cbed_frame(all_equal=True, radius=3, indices=np.array([(1, 0)]))
     (d2, i2, p2) = cbed_frame(all_equal=True, radius=3, indices=np.array([(-1, 0)]))
@@ -94,86 +103,86 @@ def test_symmetries(lt_ctx):
     c_0_0 = results.complex_0_0.raw_data
     c_1_0 = results.complex_1_0.raw_data
 
-    assert np.allclose(np.abs(c_0_0), 0)
-    assert np.allclose(np.abs(c_1_0), data.sum(axis=(2, 3)))
+    assert_allclose(np.abs(c_0_0), 0)
+    assert_allclose(np.abs(c_1_0), data.sum(axis=(2, 3)))
 
     c_0_1 = results.complex_0_1.raw_data
     c_1_1 = results.complex_1_1.raw_data
 
-    assert np.allclose(np.abs(c_0_1), 0)
+    assert_allclose(np.abs(c_0_1), 0, atol=1e-6, rtol=1e-6)
 
-    assert np.allclose(np.abs(c_1_1[1, 0]), 0)
-    assert np.allclose(np.abs(c_1_1[1, 1]), 0)
+    assert_allclose(np.abs(c_1_1[1, 0]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.abs(c_1_1[1, 1]), 0, atol=1e-6, rtol=1e-6)
 
     assert np.all(np.abs(c_1_1[0, 0]) > 0)
     assert np.all(np.abs(c_1_1[0, 1]) > 0)
-    assert np.allclose(np.angle(c_1_1[0, 0]), np.pi/2)
-    assert np.allclose(np.angle(c_1_1[0, 1]), -np.pi/2)
+    assert_allclose(np.angle(c_1_1[0, 0]), np.pi/2)
+    assert_allclose(np.angle(c_1_1[0, 1]), -np.pi/2)
 
     c_0_2 = results.complex_0_2.raw_data
     c_1_2 = results.complex_1_2.raw_data
 
-    assert np.allclose(np.abs(c_0_2), 0)
+    assert_allclose(np.abs(c_0_2), 0, atol=1e-6, rtol=1e-6)
 
     # 2-fold suppressed for 4-fold symmetry
-    assert np.allclose(np.abs(c_1_2[1, 1]), 0)
+    assert_allclose(np.abs(c_1_2[1, 1]), 0, atol=1e-6, rtol=1e-6)
 
     assert np.all(np.abs(c_1_2[0, 0]) > 0)
     assert np.all(np.abs(c_1_2[0, 1]) > 0)
     assert np.all(np.abs(c_1_2[1, 0]) > 0)
     # Discontinuity at this point, can be pi or -pi
-    assert np.allclose(np.abs(np.angle(c_1_2[0, 0])), np.pi)
-    assert np.allclose(np.abs(np.angle(c_1_2[0, 1])), np.pi)
-    assert np.allclose(np.abs(np.angle(c_1_2[1, 0])), np.pi)
+    assert_allclose(np.abs(np.angle(c_1_2[0, 0])), np.pi)
+    assert_allclose(np.abs(np.angle(c_1_2[0, 1])), np.pi)
+    assert_allclose(np.abs(np.angle(c_1_2[1, 0])), np.pi)
 
     c_0_3 = results.complex_0_3.raw_data
     c_1_3 = results.complex_1_3.raw_data
 
-    assert np.allclose(np.abs(c_0_3), 0)
+    assert_allclose(np.abs(c_0_3), 0, atol=1e-6, rtol=1e-6)
     # odd harmonics suppressed in 2-fold symmetry
-    assert np.allclose(np.abs(c_1_3[1]), 0)
+    assert_allclose(np.abs(c_1_3[1]), 0, atol=1e-6, rtol=1e-6)
 
     assert np.all(np.abs(c_1_3[0, 0]) > 0)
     assert np.all(np.abs(c_1_3[0, 1]) > 0)
-    assert np.allclose(np.angle(c_1_3[0, 0]), -np.pi/2)
-    assert np.allclose(np.angle(c_1_3[0, 1]), np.pi/2)
+    assert_allclose(np.angle(c_1_3[0, 0]), -np.pi/2)
+    assert_allclose(np.angle(c_1_3[0, 1]), np.pi/2)
 
     c_0_4 = results.complex_0_4.raw_data
     c_1_4 = results.complex_1_4.raw_data
 
-    assert np.allclose(np.abs(c_0_4), 0)
+    assert_allclose(np.abs(c_0_4), 0, atol=1e-6, rtol=1e-6)
 
     assert np.all(np.abs(c_1_4) > 0)
 
-    assert np.allclose(np.angle(c_1_4[0, 0]), 0)
-    assert np.allclose(np.angle(c_1_4[0, 1]), 0)
-    assert np.allclose(np.angle(c_1_4[1, 0]), 0)
-    assert np.allclose(np.angle(c_1_4[1, 1]), 0)
+    assert_allclose(np.angle(c_1_4[0, 0]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.angle(c_1_4[0, 1]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.angle(c_1_4[1, 0]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.angle(c_1_4[1, 1]), 0, atol=1e-6, rtol=1e-6)
 
     c_0_5 = results.complex_0_5.raw_data
     c_1_5 = results.complex_1_5.raw_data
 
-    assert np.allclose(np.abs(c_0_5), 0)
+    assert_allclose(np.abs(c_0_5), 0, atol=1e-6, rtol=1e-6)
 
     # odd harmonics suppressed in 2-fold symmetry
-    assert np.allclose(np.abs(c_1_5[1]), 0)
+    assert_allclose(np.abs(c_1_5[1]), 0, atol=1e-6, rtol=1e-6)
 
     c_0_7 = results.complex_0_5.raw_data
     c_1_7 = results.complex_1_5.raw_data
 
-    assert np.allclose(np.abs(c_0_7), 0)
+    assert_allclose(np.abs(c_0_7), 0, atol=1e-6, rtol=1e-6)
 
     # odd harmonics suppressed in 2-fold symmetry
-    assert np.allclose(np.abs(c_1_7[1]), 0)
+    assert_allclose(np.abs(c_1_7[1]), 0, atol=1e-6, rtol=1e-6)
 
     c_0_8 = results.complex_0_4.raw_data
     c_1_8 = results.complex_1_4.raw_data
 
-    assert np.allclose(np.abs(c_0_8), 0)
+    assert_allclose(np.abs(c_0_8), 0, atol=1e-6, rtol=1e-6)
 
     assert np.all(np.abs(c_1_8) > 0)
 
-    assert np.allclose(np.angle(c_1_8[0, 0]), 0)
-    assert np.allclose(np.angle(c_1_8[0, 1]), 0)
-    assert np.allclose(np.angle(c_1_8[1, 0]), 0)
-    assert np.allclose(np.angle(c_1_8[1, 1]), 0)
+    assert_allclose(np.angle(c_1_8[0, 0]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.angle(c_1_8[0, 1]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.angle(c_1_8[1, 0]), 0, atol=1e-6, rtol=1e-6)
+    assert_allclose(np.angle(c_1_8[1, 1]), 0, atol=1e-6, rtol=1e-6)

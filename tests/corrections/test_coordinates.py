82fcfc5bc4935ed29903e1943e8bbd425cca904e
Dieter Weber <d.weber@fz-juelich.de>
2025-08-27 12:19:50 +0200

    Include test for correct chaining
    This is more documentation than testing: Info on correctly combining
    transforms was missing.

--- tests/corrections/test_coordinates.py
+++ tests/corrections/test_coordinates.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.testing import assert_allclose
 
 import libertem.corrections.coordinates as c
 from libertem.utils import rotate_deg
@@ -42,3 +43,26 @@ def test_flip_x():
     r_y, r_x = c.flip_x() @ (y, x)
     assert np.allclose(r_y, y)
     assert np.allclose(r_x, -x)
+
+
+def test_chain():
+    y = np.array((17., -23., -3., 7.))
+    x = np.array((-13., 11., 5., -2.))
+    # Reference result: First flip, then rotate
+    r_y1, r_x1 = c.flip_y() @ (y, x)
+    r_y2, r_x2 = c.rotate_deg(29) @ (r_y1, r_x1)
+
+    # Combined correctly from right to left
+    trans_alt = c.rotate_deg(29) @ c.flip_y()
+    r_yalt, r_xalt = trans_alt @ (y, x)
+
+    # Combined incorrectly: reverse order
+    trans_wrong = c.flip_y() @ c.rotate_deg(29)
+    r_ywrong, r_xwrong = trans_wrong @ (y, x)
+
+    assert_allclose(r_y2, r_yalt)
+    assert_allclose(r_x2, r_xalt)
+
+    # Counter test: Make sure test is specific
+    assert not np.allclose(r_y2, r_ywrong)
+    assert not np.allclose(r_x2, r_xwrong)

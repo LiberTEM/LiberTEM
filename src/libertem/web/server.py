2defcb9b9876a23e8e0b760addbe8e5593f31827
Alexander Clausen <a.clausen@fz-juelich.de>
2026-01-06 13:53:25 +0100

    `get_event_loop` no longer creates a new loop
    Must be done explicitly now.

--- src/libertem/web/server.py
+++ src/libertem/web/server.py
@@ -223,7 +223,11 @@ def run(
     )
 
     adjust_event_loop_policy()
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
     # shared state:
     event_bus = EventBus()
